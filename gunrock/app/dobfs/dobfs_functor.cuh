1aa2f515c89b6eb3f110f42256bcd81e6fd0f9e2
yzhwang <yzhwang@ucdavis.edu>
2013-11-16 22:04:00 -0800

    dobfs functors outline

--- gunrock/app/dobfs/dobfs_functor.cuh
+++ gunrock/app/dobfs/dobfs_functor.cuh
@@ -21,6 +21,27 @@ namespace gunrock {
 namespace app {
 namespace dobfs {
 
+// TODO:
+// 
+// Prepare for reverse BFS (first two functor set)
+// 1) prepare unvisited queue
+//   VertexMap for all nodes, select whose label is -1
+// 2) prepare frontier_map_in
+//   Use MemsetKernel to set all frontier_map_in as 0
+//   Vertexmap for all the nodes in current frontier,
+//   set their frontier_map_in value as 1
+// 3) clear all frontier_map_in value as 0
+//
+// During the reverse BFS (third functor set)
+// 1) BackwardEdgeMap
+// 2) Clear frontier_map_in
+// 3) VertexMap
+//
+// Switch back to normal BFS (final functor set)
+// 1) prepare current frontier
+// VertexMap for all nodes, select whose frontier_map_out is 1
+//
+
 } // dobfs
 } // app
 } // gunrock
