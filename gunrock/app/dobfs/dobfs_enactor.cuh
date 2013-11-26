7aa77eb4d7c7d185cd857f5752eb29de1dc3fb66
yzhwang <yzhwang@ucdavis.edu>
2013-11-26 14:59:51 -0800

    prepare for SSSP.

--- gunrock/app/dobfs/dobfs_enactor.cuh
+++ gunrock/app/dobfs/dobfs_enactor.cuh
@@ -401,11 +401,11 @@ class DOBFSEnactor : public EnactorBase
 
             if (retval) break;
             }
-            printf("iter: %lld\n, alpha %f\n", iteration, problem->alpha);
+            //printf("iter: %lld\n, alpha %f\n", iteration, problem->alpha);
               
             // Reverse BFS
             if (done[0] < 0) {
-                printf("in RBFS.\n");
+                //printf("in RBFS.\n");
 
             //util::DisplayDeviceResults(graph_slice->frontier_queues.d_keys[0], graph_slice->nodes);
             SizeT queue_length          = current_frontier_size;
@@ -588,11 +588,11 @@ class DOBFSEnactor : public EnactorBase
 
             }
 
-            printf("iter: %lld\n", iteration);
+            //printf("iter: %lld\n", iteration);
 
             // Normal BFS
             if (done[0] < 0) {
-                printf("back to normal BFS.\n");
+                //printf("back to normal BFS.\n");
             SizeT queue_length          = graph_slice->nodes;
             VertexId queue_index        = 0;        // Work queue index
             int selector                = 0;
---
+++ gunrock/app/sssp/sssp_problem.cuh
@@ -0,0 +1,32 @@
+// ----------------------------------------------------------------
+// Gunrock -- Fast and Efficient GPU Graph Library
+// ----------------------------------------------------------------
+// This source code is distributed under the terms of LICENSE.TXT
+// in the root directory of this source distribution.
+// ----------------------------------------------------------------
+
+/**
+ * @file
+ * sssp_problem.cuh
+ *
+ * @brief GPU Storage management Structure for SSSP Problem Data
+ */
+
+#pragma once
+
+#include <gunrock/app/problem_base.cuh>
+#include <gunrock/util/memset_kernel.cuh>
+
+namespace gunrock {
+namespace app {
+namespace sssp {
+
+} //namespace sssp
+} //namespace app
+} //namespace gunrock
+
+// Leave this at the end of the file
+// Local Variables:
+// mode:c++
+// c-file-style: "NVIDIA"
+// End:
