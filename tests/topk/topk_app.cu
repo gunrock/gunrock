59470e9bcbc6974e2a83da3f97b50503c064afa7
wyd855 <wyd855@gmail.com>
2014-06-25 10:34:20 -0700

    public interface structure draft

--- tests/topk/topk_app.cu
+++ tests/topk/topk_app.cu
@@ -20,6 +20,24 @@
 using namespace gunrock::app::topk;
 
 /**
+ * @brief displays the top K results
+ *
+ */
+template<typename VertexId, typename Value, typename SizeT>
+void DisplaySolution(VertexId *h_node_id,
+		     Value    *h_degrees,
+		     SizeT    num_nodes)
+{
+  // at most display first 100 results
+  if (num_nodes > 100) { num_nodes = 100; }
+  printf("==> Top %d centrality nodes:\n", num_nodes);
+  for (SizeT i = 0; i < num_nodes; ++i)
+  { printf("%d %d\n", h_node_id[i], h_degrees[i]); }
+  printf("\n");
+  fflush(stdout);
+}
+
+/**
  * @brief Run TopK
  *
  * @tparam VertexId
@@ -33,10 +51,10 @@ using namespace gunrock::app::topk;
  *
  */
 template <typename VertexId, typename SizeT, typename Value>
-void topk_run(const SizeT    row_offsets,
-	      const VertexId col_indices,
-	      const SizeT    row_offsets,
-	      const VertexId col_indices,
+void topk_run(const SizeT    row_offsets_i,
+	      const VertexId col_indices_i,
+	      const SizeT    row_offsets_j,
+	      const VertexId col_indices_j,
 	      const SizeT    num_nodes,
 	      const SizeT    num_edges,
 	      const SizeT    top_nodes,
@@ -60,15 +78,16 @@ void topk_run(const SizeT    row_offsets,
   VertexId *h_node_id = (VertexId*)malloc(sizeof(VertexId) * top_nodes);
   Value    *h_degrees = ( Value* )malloc(sizeof( Value ) * top_nodes);
   
+  // TODO: build graph_i and graph_j from csr arrays
+  
+
   // copy data from CPU to GPU, initialize data members in DataSlice for graph
   util::GRError(topk_problem->Init(g_stream_from_host,
-				   row_offsets,
-				   col_indices,
-				   row_offsets,
-				   col_indices,
+				   graph_i,
+				   graph_j,
 				   num_gpus), 
 		"Problem TOPK Initialization Failed", __FILE__, __LINE__);
-    
+  
   // reset values in DataSlice for graph
   util::GRError(topk_problem->Reset(topk_enactor.GetFrontierType()), 
 		"TOPK Problem Data Reset Failed", __FILE__, __LINE__);
@@ -97,39 +116,77 @@ void topk_run(const SizeT    row_offsets,
   cudaDeviceSynchronize();
 }
 
-void topk_dispatch(const void *row_offsets,
-		   const void *col_indices,
-		   const void *row_offsets,
-		   const void *col_indices,
+void topk_dispatch(const void *row_offsets_i,
+		   const void *col_indices_i,
+		   const void *row_offsets_j,
+		   const void *col_indices_j,
 		   size_t     num_nodes,
 		   size_t     num_edges,
 		   size_t     top_nodes,
 		   const int  data_type);
 {
-  switch (data_type)
+  //TODO: add more supportive if necessary
+  switch (VTXID_TYPE)
   {
-  case (value=unsigned int):
-    topk_run<unsigned int, 
-	     unsigned int, 
-	     unsigned int>((const unsigned int*)row_offsets,
-			   (const unsigned int*)col_indices,
-			   (const unsigned int*)row_offsets,
-			   (const unsigned int*)col_indices,
-			   num_nodes, 
-			   num_edges, 
-			   top_nodes,
-			   data_type);
-  case (value=double):
-    topk_run<unsigned int, 
-	     unsigned int, 
-	     double>((const unsigned int*)row_offsets,
-		     (const unsigned int*)col_indices,
-		     (const unsigned int*)row_offsets,
-		     (const unsigned int*)col_indices,
-		     num_nodes, 
-		     num_edges, 
-		     top_nodes,
-		     data_type);
+  case VTXID_UINT:
+    switch (VALUE_TYPE)
+    {
+    case VALUE_UINT:
+      topk_run<unsigned int, unsigned int, unsigned int>
+	((const unsigned int*)row_offsets_i,
+	 (const unsigned int*)col_indices_i,
+	 (const unsigned int*)row_offsets_j,
+	 (const unsigned int*)col_indices_j,
+	 num_nodes,  num_edges,  top_nodes); 
+      break;
+      
+    case VALUE_FLOAT:
+      topk_run<unsigned int, unsigned int, float>
+	((const unsigned int*)row_offsets_i,
+	 (const unsigned int*)col_indices_i,
+	 (const unsigned int*)row_offsets_j,
+	 (const unsigned int*)col_indices_j,
+	 num_nodes,  num_edges,  top_nodes); 
+      break;
+      
+    case VALUE_DOUBLE:
+      topk_run<unsigned int, unsigned int, double>
+	((const unsigned int*)row_offsets_i,
+	 (const unsigned int*)col_indices_i,
+	 (const unsigned int*)row_offsets_j,
+	 (const unsigned int*)col_indices_j,
+	 num_nodes,  num_edges,  top_nodes);
+
+    }
+  case VTXID_LONG:
+    switch (VALUE_TYPE)
+    {
+    case VALUE_UINT:
+      topk_run<long int, long int, unsigned int>
+	((const unsigned int*)row_offsets_i,
+	 (const unsigned int*)col_indices_i,
+	 (const unsigned int*)row_offsets_j,
+	 (const unsigned int*)col_indices_j,
+	 num_nodes,  num_edges,  top_nodes); 
+      break;
+   
+    case VALUE_FLOAT:
+      topk_run<long int, long int, float>
+	((const unsigned int*)row_offsets_i,
+	 (const unsigned int*)col_indices_i,
+	 (const unsigned int*)row_offsets_j,
+	 (const unsigned int*)col_indices_j,
+	 num_nodes,  num_edges,  top_nodes); 
+      break;
+    
+    case VALUE_DOUBLE:
+      topk_run<long int, long int, float>
+	((const unsigned int*)row_offsets_i,
+	 (const unsigned int*)col_indices_i,
+	 (const unsigned int*)row_offsets_j,
+	 (const unsigned int*)col_indices_j,
+	 num_nodes,  num_edges,  top_nodes);
+    }
   }
 }
 
