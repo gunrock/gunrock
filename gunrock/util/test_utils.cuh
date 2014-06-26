55ef053e3db1f39037e1fc1ae35946eedbb074c5
Yangzihao Wang <yzhwang@ucdavis.edu>
2014-06-26 08:02:25 -0700

    update basic_util and random_bits

--- gunrock/util/test_utils.cuh
+++ gunrock/util/test_utils.cuh
@@ -20,40 +20,6 @@
 namespace gunrock {
 namespace util {
 
-/******************************************************************************
- * Device initialization
- ******************************************************************************/
-
-void DeviceInit(CommandLineArgs &args)
-{
-    int deviceCount;
-    cudaGetDeviceCount(&deviceCount);
-    if (deviceCount == 0) {
-        fprintf(stderr, "No devices supporting CUDA.\n");
-        exit(1);
-    }
-    int dev = 0;
-    args.GetCmdLineArgument("device", dev);
-    if (dev < 0) {
-        dev = 0;
-    }
-    if (dev > deviceCount - 1) {
-        dev = deviceCount - 1;
-    }
-    cudaDeviceProp deviceProp;
-    cudaGetDeviceProperties(&deviceProp, dev);
-    if (deviceProp.major < 1) {
-        fprintf(stderr, "Device does not support CUDA.\n");
-        exit(1);
-    }
-    if (!args.CheckCmdLineFlag("quiet")) {
-        printf("Using device %d: %s\n", dev, deviceProp.name);
-    }
-
-    cudaSetDevice(dev);
-}
-
-
 /**
  * Verify the contents of a device array match those
  * of a host array
