85b814747c7e8848745818489ce9e5d0e5240d7e
Yangzihao Wang <yzhwang@ucdavis.edu>
2014-10-28 21:01:43 -0700

    Added wtf, salsa, hits code to master branch

---
+++ gunrock/app/salsa/salsa_functor.cuh
@@ -0,0 +1,224 @@
+// ----------------------------------------------------------------
+// Gunrock -- Fast and Efficient GPU Graph Library
+// ----------------------------------------------------------------
+// This source code is distributed under the terms of LICENSE.TXT
+// in the root directory of this source distribution.
+// ---------------------------------------------------------------- 
+/**
+ * @file
+ * salsa_functor.cuh
+ *
+ * @brief Device functions for SALSA problem.
+ */
+
+#pragma once
+
+#include <gunrock/app/problem_base.cuh>
+#include <gunrock/app/salsa/salsa_problem.cuh>
+
+namespace gunrock {
+namespace app {
+namespace salsa {
+
+/**
+ * @brief Structure contains device functions in SALSA graph traverse.
+ *
+ * @tparam VertexId            Type of signed integer to use as vertex id (e.g., uint32)
+ * @tparam SizeT               Type of unsigned integer to use for array indexing. (e.g., uint32)
+ * @tparam ProblemData         Problem data type which contains data slice for SALSA problem
+ *
+ */
+template<typename VertexId, typename SizeT, typename Value, typename ProblemData>
+struct HFORWARDFunctor
+{
+    typedef typename ProblemData::DataSlice DataSlice;
+
+    /**
+     * @brief Forward Edge Mapping condition function. Check if the destination node
+     * has been claimed as someone else's child.
+     *
+     * @param[in] s_id Vertex Id of the edge source node
+     * @param[in] d_id Vertex Id of the edge destination node
+     * @param[in] problem Data slice object
+     *
+     * \return Whether to load the apply function for the edge and include the destination node in the next frontier.
+     */
+    static __device__ __forceinline__ bool CondEdge(VertexId s_id, VertexId d_id, DataSlice *problem, VertexId e_id = 0, VertexId e_id_in = 0)
+    {
+        return true;
+    }
+
+    /**
+     * @brief Forward Edge Mapping apply function. Now we know the source node
+     * has succeeded in claiming child, so it is safe to set label to its child
+     * node (destination node).
+     *
+     * @param[in] s_id Vertex Id of the edge source node
+     * @param[in] d_id Vertex Id of the edge destination node
+     * @param[in] problem Data slice object
+     *
+     */
+    static __device__ __forceinline__ void ApplyEdge(VertexId s_id, VertexId d_id, DataSlice *problem, VertexId e_id = 0, VertexId e_id_in = 0)
+    {
+        util::io::ModifiedStore<ProblemData::QUEUE_WRITE_MODIFIER>::St(
+            s_id, problem->d_hub_predecessors+e_id);
+    }
+
+};
+
+/**
+ * @brief Structure contains device functions in SALSA graph traverse.
+ *
+ * @tparam VertexId            Type of signed integer to use as vertex id (e.g., uint32)
+ * @tparam SizeT               Type of unsigned integer to use for array indexing. (e.g., uint32)
+ * @tparam ProblemData         Problem data type which contains data slice for SALSA problem
+ *
+ */
+template<typename VertexId, typename SizeT, typename Value, typename ProblemData>
+struct HBACKWARDFunctor
+{
+    typedef typename ProblemData::DataSlice DataSlice;
+
+    /**
+     * @brief Forward Edge Mapping condition function. Check if the destination node
+     * has been claimed as someone else's child.
+     *
+     * @param[in] s_id Vertex Id of the edge source node
+     * @param[in] d_id Vertex Id of the edge destination node
+     * @param[in] problem Data slice object
+     *
+     * \return Whether to load the apply function for the edge and include the destination node in the next frontier.
+     */
+    static __device__ __forceinline__ bool CondEdge(VertexId s_id, VertexId d_id, DataSlice *problem, VertexId e_id = 0, VertexId e_id_in = 0)
+    {
+        VertexId v_id = problem->d_hub_predecessors[e_id_in];
+        bool flag = (problem->d_out_degrees[v_id] != 0);
+        if (!flag) problem->d_hrank_next[v_id] = 0;
+        return flag;
+    }
+
+    /**
+     * @brief Forward Edge Mapping apply function. Now we know the source node
+     * has succeeded in claiming child, so it is safe to set label to its child
+     * node (destination node).
+     *
+     * @param[in] s_id Vertex Id of the edge source node
+     * @param[in] d_id Vertex Id of the edge destination node
+     * @param[in] problem Data slice object
+     *
+     */
+    static __device__ __forceinline__ void ApplyEdge(VertexId s_id, VertexId d_id, DataSlice *problem, VertexId e_id = 0, VertexId e_id_in = 0)
+    {
+        Value hrank_dst = problem->d_hrank_curr[d_id] / (problem->d_in_degrees[s_id] * problem->d_out_degrees[d_id]);
+        VertexId v_id = problem->d_hub_predecessors[e_id_in];
+        //printf("v:%d, s:%d, d:%d in(s):%d, out(d):%d, H(d):%5f\n", v_id, s_id, d_id, problem->d_in_degrees[s_id], problem->d_out_degrees[d_id], problem->d_hrank_curr[d_id]);
+        atomicAdd(&problem->d_hrank_next[v_id], hrank_dst);
+    }
+};
+
+/**
+ * @brief Structure contains device functions in SALSA graph traverse.
+ *
+ * @tparam VertexId            Type of signed integer to use as vertex id (e.g., uint32)
+ * @tparam SizeT               Type of unsigned integer to use for array indexing. (e.g., uint32)
+ * @tparam ProblemData         Problem data type which contains data slice for SALSA problem
+ *
+ */
+template<typename VertexId, typename SizeT, typename Value, typename ProblemData>
+struct AFORWARDFunctor
+{
+    typedef typename ProblemData::DataSlice DataSlice;
+
+    /**
+     * @brief Forward Edge Mapping condition function. Check if the destination node
+     * has been claimed as someone else's child.
+     *
+     * @param[in] s_id Vertex Id of the edge source node
+     * @param[in] d_id Vertex Id of the edge destination node
+     * @param[in] problem Data slice object
+     *
+     * \return Whether to load the apply function for the edge and include the destination node in the next frontier.
+     */
+    static __device__ __forceinline__ bool CondEdge(VertexId s_id, VertexId d_id, DataSlice *problem, VertexId e_id = 0, VertexId e_id_in = 0)
+    {
+        return true;
+    }
+
+    /**
+     * @brief Forward Edge Mapping apply function. Now we know the source node
+     * has succeeded in claiming child, so it is safe to set label to its child
+     * node (destination node).
+     *
+     * @param[in] s_id Vertex Id of the edge source node
+     * @param[in] d_id Vertex Id of the edge destination node
+     * @param[in] problem Data slice object
+     *
+     */
+    static __device__ __forceinline__ void ApplyEdge(VertexId s_id, VertexId d_id, DataSlice *problem, VertexId e_id = 0, VertexId e_id_in = 0)
+    {
+        util::io::ModifiedStore<ProblemData::QUEUE_WRITE_MODIFIER>::St(
+            s_id, problem->d_auth_predecessors+e_id);
+    }
+
+};
+
+/**
+ * @brief Structure contains device functions in SALSA graph traverse.
+ *
+ * @tparam VertexId            Type of signed integer to use as vertex id (e.g., uint32)
+ * @tparam SizeT               Type of unsigned integer to use for array indexing. (e.g., uint32)
+ * @tparam ProblemData         Problem data type which contains data slice for SALSA problem
+ *
+ */
+template<typename VertexId, typename SizeT, typename Value, typename ProblemData>
+struct ABACKWARDFunctor
+{
+    typedef typename ProblemData::DataSlice DataSlice;
+
+    /**
+     * @brief Forward Edge Mapping condition function. Check if the destination node
+     * has been claimed as someone else's child.
+     *
+     * @param[in] s_id Vertex Id of the edge source node
+     * @param[in] d_id Vertex Id of the edge destination node
+     * @param[in] problem Data slice object
+     *
+     * \return Whether to load the apply function for the edge and include the destination node in the next frontier.
+     */
+    static __device__ __forceinline__ bool CondEdge(VertexId s_id, VertexId d_id, DataSlice *problem, VertexId e_id = 0, VertexId e_id_in = 0)
+    {
+        VertexId v_id = problem->d_auth_predecessors[e_id_in];
+        bool flag = (problem->d_in_degrees[v_id] != 0);
+        if (!flag) problem->d_arank_next[v_id] = 0;
+        return flag;
+    }
+
+    /**
+     * @brief Forward Edge Mapping apply function. Now we know the source node
+     * has succeeded in claiming child, so it is safe to set label to its child
+     * node (destination node).
+     *
+     * @param[in] s_id Vertex Id of the edge source node
+     * @param[in] d_id Vertex Id of the edge destination node
+     * @param[in] problem Data slice object
+     *
+     */
+    static __device__ __forceinline__ void ApplyEdge(VertexId s_id, VertexId d_id, DataSlice *problem, VertexId e_id = 0, VertexId e_id_in = 0)
+    {
+        Value arank_dst = problem->d_arank_curr[d_id] / (problem->d_out_degrees[s_id] * problem->d_in_degrees[d_id]);
+        VertexId v_id = problem->d_auth_predecessors[e_id_in];
+        //printf("v:%d, s:%d, d:%d\n",v_id, s_id, d_id);
+        //printf("out(s):%d, in(d):%d, A(d):%5f\n", problem->d_out_degrees[s_id], problem->d_in_degrees[d_id], problem->d_arank_curr[d_id]);
+        atomicAdd(&problem->d_arank_next[v_id], arank_dst);
+    }
+};
+
+} // salsa
+} // app
+} // gunrock
+
+// Leave this at the end of the file
+// Local Variables:
+// mode:c++
+// c-file-style: "NVIDIA"
+// End:
