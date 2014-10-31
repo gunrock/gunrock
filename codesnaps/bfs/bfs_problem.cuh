164865e2e77062cef8b3d8eacd921942530a2b11
John Owens <jowens@ece.ucdavis.edu>
2014-10-30 22:19:10 -0700

    content/grammar edits for rebuttal

--- codesnaps/bfs/bfs_problem.cuh
+++ codesnaps/bfs/bfs_problem.cuh
@@ -1,5 +1,7 @@
-// Problem struct is inherited from ProblemBase struct, which stores graph
-// topology data in CSR format and frontier buffers.
+// This data structure (the "Problem" struct) stores the graph
+// topology in CSR format and the frontier. All Problem structs
+// inherit from the ProblemBase struct. Algorithm-specific data is
+// stored in a "DataSlice".
 
 template<
          typename VertexId,
@@ -11,14 +13,18 @@ template<
          struct BFSProblem : public ProblemBase<VertexId, SizeT, _USE_DOUBLE_BUFFER>
 {
 
-    // MARK_PREDECESSOR would be true when algorithm needs to know the predecessor node IDs
-    // for a newly formed frontier during a certain step.
+    // MARK_PREDECESSORS sets the predecessor node ID during a
+    // traversal for each node in the new frontier.
     static const bool MARK_PREDECESSORS     = _MARK_PREDECESSORS;
-    // ENABLE_IDEMPOTENCE would be true when the operation performed in parallel
-    // for all neighbor nodes/edges is idempotent, meaning data race is benign.
+    // ENABLE_IDEMPOTENCE is an optimization when the operation
+    // performed in parallel for all neighbor nodes/edges is
+    // idempotent, meaning data races are benign.
     static const bool ENABLE_IDEMPOTENCE    = _ENABLE_IDEMPOTENCE;
 
-    // DataSlice sturct stores per-node or per-edge arrays and global variables (if any).
+    // The DataSlice struct stores per-node or per-edge arrays and
+    // global variables (if any) that are specific to this particular
+    // algorithm. Here, we store the depth value and predecessor node
+    // ID for each node.
     struct DataSlice
     {
         VertexId *d_labels; // BFS depth value
@@ -29,9 +35,9 @@ template<
     SizeT edges;
     DataSlice *d_data_slices;
 
-    //Constructor, Destructor ignored here
+    // The constructor and destructor are ignored here.
 
-    // Extract labels and predecessors back to CPU
+    // "Extract" copies labels and predecessors back to the CPU.
     cudaError_t Extract(VertexId *h_labels, VertexId *h_preds)
     {
         cudaError_t retval = cudaSuccess;
@@ -40,9 +46,9 @@ template<
         return retval;
     }
 
-    // Init function takes a CSR graph stored on CPU, and initialize graph
-    // topology data (by loading ProblemBase::Init(graph)) and DataSlice on
-    // GPU.
+    // The Init function initializes this Problem struct with a CSR
+    // graph that's stored on the CPU. It also initializes the
+    // algorithm-specific data, here depth and predecessor.
     cudaError_t Init(
             const Csr<VertexId, Value, SizeT> &graph)
     {
@@ -50,21 +56,23 @@ template<
         if (retval = util::GRError(ProblemBase::Init(graph))) break;
         if (retval = util::GRError(GPUMalloc(data_slices[0]->d_labels, nodes))) break;
         if (retval = util::GRError(GPUMalloc(data_slices[0]->d_preds, nodes))) break;
-        return retval;  
+        return retval;
     }
 
-    // Reset function will be loaded before each BFS to reset problem related data.
+    // The Reset function primes the graph data structure to an
+    // untraversed state.
     cudaError_t Reset(
             const Csr<VertexId, Value, SizeT> &graph, VertexId src)
     {
         cudaError_t retval = cudaSuccess;
         if (retval = util::GRError(ProblemBase::Reset(graph))) break;
-        // Reset depth values and predecessor values to invalid. Only set source node's depth value to 0.
+        // Set all depth and predecessor values to invalid. Set the
+        // source node's depth value to 0.
         util::MemsetKernel<<<BLOCK, THREAD>>>(data_slices[0]->d_labels, INVALID_NODE_VALUE, nodes);
         util::MemsetKernel<<<BLOCK, THREAD>>>(data_slices[0]->d_preds, INVALID_PREDECESSOR_ID, nodes);
         if (retval = util::GRError(CopyGPU2CPU(data_slices[0]->d_labels+src, 0, 1)));
-        // Put source node ID into the initial frontier
+        // Put the source node ID into the initial frontier.
         if (retval = util::GRError(CopyGPU2CPU(g_slices[0]->ping_pong_working_queue, src, 1)));
-        return retval;  
+        return retval;
     }
 };
