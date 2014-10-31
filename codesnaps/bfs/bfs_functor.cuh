87101681c6bbf77d2cb200d77aa97c52f883be03
John Owens <jowens@ece.ucdavis.edu>
2014-10-30 22:34:53 -0700

    content/grammar edits for rebuttal

--- codesnaps/bfs/bfs_functor.cuh
+++ codesnaps/bfs/bfs_functor.cuh
@@ -1,49 +1,45 @@
-// Functor defines user-specific computations with per-edge functors: CondEdge
-// and ApplyEdge, which will be used in Advance operator; and per-node
-// functors: CondVertex and ApplyVertex, which will be used in Filter operator.
+// bfs_functor defines user-specific computations with (1) two
+// per-edge functors, CondEdge and ApplyEdge, which will be used in
+// the Advance operator; and (2) two per-node functors, CondVertex and
+// ApplyVertex, which will be used in the Filter operator.
 
 template<typename VertexId, typename SizeT, typename Value, typename ProblemData>
 struct BFSFunctor {
     typedef typename ProblemData::DataSlice DataSlice;
-    
-    // CondEdge takes edge information (source ID, destination ID) and DataSlice
-    // as input. It returns a boolean value suggesting whether the edge is valid
-    // in the next frontier.
+
     __device__ bool CondEdge(VertexId s_id, VertexId d_id, DataSlice *p)
     {
         if (ProblemData::MARK_PREDECESSORS)
-            // Set predecessor for each destination node.
-            // Both atomic operations here guarantee one edge being visited once
+            // Set predecessor for each destination node. We set the
+            // depth later, because we only want to set the depth for
+            // valid nodes.
             return (atomicCAS(&p->d_preds[d_id], INVALID_PREDECESSOR_ID, s_id) == INVALID_PREDECESSOR_ID)
                 ? true : false;
         else
-            // source ID sent in as depth value, set destination depth
-            // to be current depth value plus one.
+            // If we're not keeping track of predecessors, we can
+            // immediately set the depth of the destination vertex to
+            // one plus the source vertex's depth.
             return (atomicCAS(&p->d_labels[d_id], INVALID_NODE_VALUE, s_id+1) == INVALID_NODE_VALUE)
                 ? true : false;
     }
 
-    // ApplyEdge takes edge information (source ID, destination ID) and DataSlice
-    // as input. It performs user-defined per-edge computations.
+    // ApplyEdge here increments the depth value.
     __device__ void ApplyEdge(VertexId s_id, VertexId d_id, DataSlice *p)
     {
         if (ProblemData::MARK_PREDECESSORS)
-            // When MARK_PREDECESSORS flag is set,
-            // we need to update depth value here.
+            // We know the destination node is valid (from CondEdge),
+            // so here we set its depth to one plus the source
+            // vertex's depth.
             p->d_labels[d_id] = p->d_labels[s_id]+1;
     }
 
-    // CondVertex takes node ID and DataSlice as input. It returns a boolean value
-    // suggesting whether the node is valid in the next frontier.
+    // In BFS, CondVertex checks if the vertex is valid in the next frontier.
     __device__ void CondVertex(VertexId node, DataSlice *p)
     {
-        // This will remove the invalid nodes from the output
-        // frontier.
         return node != INVALID_NODE_ID;
     }
 
-    // ApplyVertex takes node ID and DataSlice as input. It performs user-defined
-    // per-node computations.
+    // In BFS, we don't apply any actions to vertices.
     __device__ void ApplyVertex(VertexId node, DataSlice *p)
     {
     }
