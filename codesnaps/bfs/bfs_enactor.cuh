6103232c5eee9136384683a3555c375eadbede0d
John Owens <jowens@ece.ucdavis.edu>
2014-10-30 22:49:42 -0700

    content/grammar edits for rebuttal

--- codesnaps/bfs/bfs_enactor.cuh
+++ codesnaps/bfs/bfs_enactor.cuh
@@ -1,9 +1,9 @@
-// Enactor defines the running process of a graph primitive using traversal
-// (advance and filter operator) and computation (functors).
+// The enactor defines how a graph primitive runs. It calls traversal
+// (advance and filter operators) and computation (functors).
 
 class BFSEnactor : public EnactorBase {
 
-    //Constructor, Destructor, and Setup functions are ignored
+    // For BFS, Constructor, Destructor, and Setup functions are ignored
 
     template<
     typename AdvancePolicy,
@@ -12,31 +12,32 @@ class BFSEnactor : public EnactorBase {
     cudaError_t EnactBFS(CudaContext &context,BFSProblem *problem,VertexId   src)
     {
 
-        //Define BFSFunctor
         typedef BFSFunctor<
         typename BFSProblem::VertexId,
         typename BFSProblem::SizeT,
         typename BFSProblem::VertexId,
         BFSProblem> BfsFunctor;
 
-        //Load Setup function to initialize kernel running parameters
+        // Start with the Setup function to initialize kernel running parameters
         cudaError_t retval = cudaSuccess;
         if (retval = EnactorBase::Setup(problem)) break;
 
-        //Define graph topology data pointer (g_slice) and problem specific
-        //data pointer (d_slice)
+        // Define the graph topology data pointer (g_slice) and
+        // the problem-specific data pointer (d_slice)
         typename BFSProblem::GraphSlice *g_slice = problem->d_graph_slices;
         typename BFSProblem::DataSlice *d_slice = problem->d_data_slices;
 
-        // queue length is initialized to 1 and get updated within each operator.
+        // Initialize the queue length (frontier size) to 1.
         SizeT queue_length = 1;
-        //ping pong frontier queue selector
-        int selector = 0; 
+        // We ping-pong between old and new frontiers; "selector"
+        // picks which one is the current destination.
+        int selector = 0;
 
-        // Taks source node as the first frontier, visit all neighbors using
-        // Advance, set depth of those that haven't been set using BFSFunctor,
-        // and filter out the invalid nodes to form a new frontier using Filter.
-        // Until the frontier is empty.
+        // Here we sequence our operators and functors. For BFS, we
+        // alternate between advancing to a new frontier
+        // (vertex-to-vertex), calling the BFS functor to set depths
+        // along the way, and then filtering out invalid nodes from
+        // the new frontier. We repeat until the frontier is empty.
         while (queue_length > 0) {
             gunrock::oprtr::advance::Kernel
                 <AdvancePolicy, BFSProblem, BFSFunctor>
@@ -45,10 +46,11 @@ class BFSEnactor : public EnactorBase {
                         g_slice->ping_pong_working_queue[selector],
                         g_slice->ping_pong_working_queue[selector^1],
                         d_slice,
-                        context, 
-                        gunrock::oprtr::advance::V2V); //This is a vertex to vertex advance
+                        context,
+                        // This advance is vertex-to-vertex
+                        gunrock::oprtr::advance::V2V);
 
-            selector ^= 1; //Swap selector 
+            selector ^= 1; // Swap selector
 
             gunrock::oprtr::filter::Kernel
                 <FilterPolicy, BFSProblem, BFSFunctor>
@@ -61,15 +63,15 @@ class BFSEnactor : public EnactorBase {
         return retval;
     }
 
-    // Kernel entry point to be loaded in the driver code
+    // The entry point in the driver code to BFS is this Enact call.
     template <typename BFSProblem>
         cudaError_t Enact(
                 CudaContext &context,
                 BFSProblem *problem, // Problem data sent in
                 typename BFSProblem::VertexId src) // Source node ID for BFS
         {
-            // Gunrock provide a recommend setting for running kernel to users
-            // here. This can be changed by users too.
+            // Gunrock provides recommended settings here for kernel
+            // parameters, but they can be changed by end-users.
             typedef gunrock::oprtr::filter::KernelPolicy<
                 BFSProblem,
                 300, //CUDA_ARCH
