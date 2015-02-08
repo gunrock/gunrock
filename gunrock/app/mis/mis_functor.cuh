7a7c51546a4ac4dce620e49dc15356a8f7092ba2
Yangzihao Wang <yzhwang@ucdavis.edu>
2015-02-07 23:25:24 -0800

    :boom:Start to write MIS primitive.

---
+++ gunrock/app/mis/mis_functor.cuh
@@ -0,0 +1,50 @@
+// ----------------------------------------------------------------
+// Gunrock -- Fast and Efficient GPU Graph Library
+// ----------------------------------------------------------------
+// This source code is distributed under the terms of LICENSE.TXT
+// in the root directory of this source distribution.
+// ---------------------------------------------------------------- 
+/**
+ * @file
+ * mis_functor.cuh
+ *
+ * @brief Device functions for MIS problem.
+ */
+
+// How to generate reduce data should be defined inside each functor by users.
+// TODO: change the interface of edge_map_partitioned
+// TODO: add global identifier for INVALID_VALUE, INVALID_VERTEXID.
+
+#pragma once
+
+#include <gunrock/app/problem_base.cuh>
+#include <gunrock/app/mis/mis_problem.cuh>
+
+namespace gunrock {
+namespace app {
+namespace mis {
+
+/**
+ * @brief Structure contains device functions in PR graph traverse.
+ *
+ * @tparam VertexId            Type of signed integer to use as vertex id (e.g., uint32)
+ * @tparam SizeT               Type of unsigned integer to use for array indexing. (e.g., uint32)
+ * @tparam Value               Type of unsigned integer to use for array indexing. (e.g., uint32)
+ * @tparam ProblemData         Problem data type which contains data slice for PR problem
+ *
+ */
+template<typename VertexId, typename SizeT, typename Value, typename ProblemData>
+struct MISFunctor
+{
+    typedef typename ProblemData::DataSlice DataSlice;
+};
+
+} // pr
+} // app
+} // gunrock
+
+// Leave this at the end of the file
+// Local Variables:
+// mode:c++
+// c-file-style: "NVIDIA"
+// End:
