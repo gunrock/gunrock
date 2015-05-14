bca0f4476f7bdb16ca0246630d3813ada646d78c
Yangzihao Wang <yzhwang@ucdavis.edu>
2015-05-14 00:17:53 -0700

    Added global indicator module. Start writing modularity.

---
+++ gunrock/global_indicator/modularity.cuh
@@ -0,0 +1,44 @@
+// ----------------------------------------------------------------
+// Gunrock -- Fast and Efficient GPU Graph Library
+// ----------------------------------------------------------------
+// This source code is distributed under the terms of LICENSE.TXT
+// in the root directory of this source distribution.
+// ----------------------------------------------------------------
+
+/**
+ * @file
+ * modularity.cuh
+ *
+ * @brief Modularity computation code
+ */
+
+#pragma once
+
+#include <gunrock/util/kernel_runtime_stats.cuh>
+#include <gunrock/util/test_utils.cuh>
+
+#include <gunrock/oprtr/advance/kernel.cuh>
+#include <gunrock/oprtr/advance/kernel_policy.cuh>
+#include <gunrock/oprtr/filter/kernel.cuh>
+#include <gunrock/oprtr/filter/kernel_policy.cuh>
+
+#include <gunrock/app/enactor_base.cuh>
+#include <gunrock/problem_base.cuh>
+
+namespace gunrock {
+namespace global_indicator {
+
+//For now only compute unweighted graph.
+//For each edge in the graph, use filter to find edges whose two end nodes belong
+//to the same cluster, then compute the modularity accoring to the following equation:
+//Q=sum_of_same_cluster_edges(A_ij - k_i*k_i/2m)/2m
+//m:#edges, k_i:out degree of i, A_ij: 1/0
+
+} // namespace global_indicator
+} // namespace gunrock
+
+// Leave this at the end of the file
+// Local Variables:
+// mode:c++
+// c-file-style: "NVIDIA"
+// End:
