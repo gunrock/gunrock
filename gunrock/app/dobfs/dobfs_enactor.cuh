8f9b63d91b796b33413ca0406b09c72c10e06d23
Yangzihao Wang <yzhwang@ucdavis.edu>
2015-08-05 17:18:48 -0700

    Use LB_LIGHT for dobfs.

--- gunrock/app/dobfs/dobfs_enactor.cuh
+++ gunrock/app/dobfs/dobfs_enactor.cuh
@@ -850,7 +850,7 @@ public:
                     32,                                 // WARP_GATHER_THRESHOLD
                     128 * 4,                            // CTA_GATHER_THRESHOLD
                     7,                                  // LOG_SCHEDULE_GRANULARITY
-                    gunrock::oprtr::advance::LB>
+                    gunrock::oprtr::advance::LB_LIGHT>
                         AdvanceKernelPolicy;
 
                 typedef gunrock::oprtr::filter::KernelPolicy<
