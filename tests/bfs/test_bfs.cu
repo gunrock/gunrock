073b084f6216c98c76c43744abf5a30426f1d3e0
Yuechao Pan <panyuechao@gmail.com>
2016-01-26 10:50:16 -0800

    Revert commit 51c7b7c

--- tests/bfs/test_bfs.cu
+++ tests/bfs/test_bfs.cu
@@ -424,11 +424,23 @@ void RunTests(Info<VertexId, Value, SizeT> *info)
                   "BFS Problem Data Extraction Failed", __FILE__, __LINE__);
 
     // verify the result
-    if (reference_check_label != NULL)
+    if ((!quick_mode) && (!quiet_mode))
     {
-        if (!ENABLE_IDEMPOTENCE)
+        printf("Label Validity: ");
+        int num_errors = CompareResults(
+            h_labels, reference_check_label,
+            graph->nodes, true, quiet_mode);
+        if (num_errors > 0)
         {
-            if (!quiet_mode)
+            printf("%d errors occurred.", num_errors);
+        }
+        printf("\n");
+
+        if (MARK_PREDECESSORS)
+        {
+            printf("Predecessor Validity: ");
+            num_errors = 0;
+            for (VertexId v=0; v<graph->nodes; v++)
             {
                 if (h_labels[v] == 
                     (ENABLE_IDEMPOTENCE ? -1 : util::MaxValue<VertexId>() - 1)) 
@@ -437,41 +449,42 @@ void RunTests(Info<VertexId, Value, SizeT> *info)
                 VertexId pred = h_preds[v];
                 if (pred >= graph->nodes || pred < 0)
                 {
-                    if (num_errors == 0)
+                    //if (num_errors == 0)
                         printf("INCORRECT: pred[%d] : %d out of bound\n", v, pred);
                     num_errors ++;
                     continue;
                 }
                 if (h_labels[v] != h_labels[pred] + 1)
                 {
-                    if (num_errors == 0)
+                    //if (num_errors == 0)
                         printf("INCORRECT: label[%d] (%d) != label[%d] (%d) + 1\n", 
                             v, h_labels[v], pred, h_labels[pred]);
                     num_errors ++;
                     continue;
                 }
-            }
-        }
-        else
-        {
-            if (!MARK_PREDECESSORS)
-            {
-                if (!quiet_mode)
+                
+                bool v_found = false;
+                for (SizeT t = graph->row_offsets[pred]; t < graph->row_offsets[pred+1]; t++)
+                if (v == graph->column_indices[t])
                 {
-                    printf("Label Validity: ");
+                    v_found = true;
+                    break;
                 }
-                int error_num = CompareResults(
-                                    h_labels, reference_check_label,
-                                    graph->nodes, true, quiet_mode);
-                if (error_num > 0)
+                if (!v_found)
                 {
-                    if (num_errors == 0)
+                    //if (num_errors == 0)
                         printf("INCORRECT: Vertex %d not in Vertex %d's neighbor list\n",
                             v, pred);
                     num_errors ++;
                     continue;
                 }
             }
+
+            if (num_errors > 0)
+            {
+                printf("%d errors occurred.", num_errors);
+            } else printf("CORRECT");
+            printf("\n");
         }
 
     }
