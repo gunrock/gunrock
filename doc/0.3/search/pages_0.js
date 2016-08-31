f0e548cc8af223dcbffd2e6bd8275f7cd41eea45
John Owens <jowens@ece.ucdavis.edu>
2016-08-31 14:14:36 -0700

    updated docs for 0.3

--- doc/0.3/search/pages_0.js
+++ doc/0.3/search/pages_0.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['building_20gunrock',['Building Gunrock',['../building_gunrock.html',1,'']]]
+  ['building_20gunrock',['Building Gunrock',['../building_gunrock.html',1,'']]],
+  ['bfs_5fchart',['bfs_chart',['../md_bfs_chart.html',1,'']]]
 ];
