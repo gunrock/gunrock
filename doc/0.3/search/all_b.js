b3cfd4a64061a3b614e72325483e7b9bf66fabdd
John Owens <jowens@ece.ucdavis.edu>
2016-08-31 14:41:33 -0700

    updated docs for 0.3

--- doc/0.3/search/all_b.js
+++ doc/0.3/search/all_b.js
@@ -15,11 +15,11 @@ var searchData=
   ['kernel_5fpolicy_2ecuh',['kernel_policy.cuh',['../oprtr_2advance_2kernel__policy_8cuh.html',1,'']]],
   ['kernel_5fruntime_5fstats_2ecuh',['kernel_runtime_stats.cuh',['../kernel__runtime__stats_8cuh.html',1,'']]],
   ['kernelpolicy',['KernelPolicy',['../structgunrock_1_1oprtr_1_1edge__map__forward_1_1_kernel_policy.html',1,'gunrock::oprtr::edge_map_forward']]],
-  ['kernelpolicy',['KernelPolicy',['../structgunrock_1_1oprtr_1_1filter_1_1_kernel_policy.html',1,'gunrock::oprtr::filter']]],
+  ['kernelpolicy',['KernelPolicy',['../structgunrock_1_1oprtr_1_1edge__map__partitioned__backward_1_1_kernel_policy.html',1,'gunrock::oprtr::edge_map_partitioned_backward']]],
   ['kernelpolicy',['KernelPolicy',['../structgunrock_1_1oprtr_1_1advance_1_1_kernel_policy.html',1,'gunrock::oprtr::advance']]],
   ['kernelpolicy',['KernelPolicy',['../structgunrock_1_1oprtr_1_1edge__map__backward_1_1_kernel_policy.html',1,'gunrock::oprtr::edge_map_backward']]],
-  ['kernelpolicy',['KernelPolicy',['../structgunrock_1_1oprtr_1_1edge__map__partitioned__backward_1_1_kernel_policy.html',1,'gunrock::oprtr::edge_map_partitioned_backward']]],
   ['kernelpolicy',['KernelPolicy',['../structgunrock_1_1oprtr_1_1edge__map__partitioned_1_1_kernel_policy.html',1,'gunrock::oprtr::edge_map_partitioned']]],
+  ['kernelpolicy',['KernelPolicy',['../structgunrock_1_1oprtr_1_1filter_1_1_kernel_policy.html',1,'gunrock::oprtr::filter']]],
   ['kernelruntimestats',['KernelRuntimeStats',['../classgunrock_1_1util_1_1_kernel_runtime_stats.html',1,'gunrock::util']]],
   ['kernelruntimestatslifetime',['KernelRuntimeStatsLifetime',['../classgunrock_1_1util_1_1_kernel_runtime_stats_lifetime.html',1,'gunrock::util']]]
 ];
