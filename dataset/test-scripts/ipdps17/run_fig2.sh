577a22aa117ffbf12905b483471ebd81534138bb
Yuechao Pan <panyuechao@gmail.com>
2017-01-27 03:47:52 -0800

    [IPDPS17] more run scripts

--- dataset/test-scripts/ipdps17/run_fig2.sh
+++ dataset/test-scripts/ipdps17/run_fig2.sh
@@ -2,12 +2,12 @@
 
 NVCC="$(which nvcc)"
 NVCC_VERSION=$($NVCC --version | grep release | sed 's/.*release //' |  sed 's/,.*//')
+GPU="k40c"
 
 FIGURE="fig2"
 GUNROCK_DIR="../../.."
 DATADIR="$GUNROCK_DIR/dataset/large"
 PARTITIONERS="random biasrandom metis"
-#PARTITIONERS="metis"
 LOGDIR="eval_$FIGURE"
 BASEOPTION="--undirected --iteration-num=16 --tag=ipdps17_$FIGURE --jsondir=$LOGDIR"
 BASEFLAG=".undir.32bitSizeT"
@@ -17,7 +17,7 @@ OPTION[0]="--idempotence --direction-optimized --src=largestdegree --traversal-m
 FLAG[0]=".skip_pred.idempotence.LB_CULL.do"
 
 ALGO[1]="bfs"
-OPTION[1]="--idempotence --src=largestdegree --taversal-mode=LB_CULL --queue-sizing=6.5 --in-sizing=4"
+OPTION[1]="--idempotence --src=largestdegree --traversal-mode=LB_CULL --queue-sizing=6.5 --in-sizing=4"
 FLAG[1]=".skip_pred.idempotence.LB_CULL.fw"
 
 ALGO[2]="pr"
@@ -43,13 +43,15 @@ mkdir -p $LOGDIR
 for k in {0..2}; do for partitioner in $PARTITIONERS; do for i in {0..2}; do for d in 1 4; do
     DEVICE="0"
     for j in {1..8}
-    do  
+    do
         if [ "$j" -lt "$d" ]; then
             DEVICE=$DEVICE",$j"
-        fi      
+        fi
     done
 
-    echo ${EXECUTION[$k]} ${GRAPH[$i]} $BASEOPTION ${OPTION[$k]} --partition-method=$partitioner --device=$DEVICE --do_a=${DO_A[$i]} --do_b=${DO_B[$i]} " > $LOGDIR/${ALGO[$k]}x$d.${NAME[$i]}${BASEFLAG}${FLAG[$k]}.$partitioner.txt 2>&1"
-         ${EXECUTION[$k]} ${GRAPH[$i]} $BASEOPTION ${OPTION[$k]} --partition-method=$partitioner --device=$DEVICE --do_a=${DO_A[$i]} --do_b=${DO_B[$i]}   > $LOGDIR/${ALGO[$k]}x$d.${NAME[$i]}${BASEFLAG}${FLAG[$k]}.$partitioner.txt 2>&1
+    Comm="${EXECUTION[$k]} ${GRAPH[$i]} $BASEOPTION ${OPTION[$k]} --partition-method=$partitioner --device=$DEVICE --do_a=${DO_A[$i]} --do_b=${DO_B[$i]} "
+    LogFile="$LOGDIR/${ALGO[$k]}.${GPU}x$d.${NAME[$i]}${BASEFLAG}${FLAG[$k]}.$partitioner.txt"
+    echo ${Comm} "> $LogFile 2>&1"
+         ${Comm}  > $LogFile 2>&1
 
 done;done;done;done
