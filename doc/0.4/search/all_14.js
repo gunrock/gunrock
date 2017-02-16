41c2b47703cc494b1584a016e4526a0053495a3e
John Owens <jowens@ece.ucdavis.edu>
2017-02-15 21:06:27 -0800

    updated docs for 0.4

--- doc/0.4/search/all_14.js
+++ doc/0.4/search/all_14.js
@@ -1,6 +1,10 @@
 var searchData=
 [
   ['throughput_20vs_2e_20frontier_20size',['Throughput vs. Frontier Size',['../md_stats_frontier.html',1,'']]],
+  ['tesla_20k40c',['Tesla K40c',['../md_stats_groute__tesla__k40c.html',1,'']]],
+  ['tesla_20k40m',['Tesla K40m',['../md_stats_groute__tesla__k40m.html',1,'']]],
+  ['tesla_20m60',['Tesla M60',['../md_stats_groute__tesla__m60.html',1,'']]],
+  ['tesla_20p100_2dpcie_2d16gb',['Tesla P100-PCIE-16GB',['../md_stats_groute__tesla__p100-_p_c_i_e-16_g_b.html',1,'']]],
   ['temp_5fi',['temp_i',['../structgunrock_1_1app_1_1topk_1_1_t_o_p_k_problem_1_1_data_slice.html#a437974f93525af633b4c90bb38d5c1c2',1,'gunrock::app::topk::TOPKProblem::DataSlice']]],
   ['temp_5fo',['temp_o',['../structgunrock_1_1app_1_1topk_1_1_t_o_p_k_problem_1_1_data_slice.html#aa28cd5ac5ad198bc201e577ea3ed11f5',1,'gunrock::app::topk::TOPKProblem::DataSlice']]],
   ['test_5fbc_2ecu',['test_bc.cu',['../test__bc_8cu.html',1,'']]],
