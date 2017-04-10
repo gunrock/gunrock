ddc22725649ff63529a7a3a9d079540aebfd0613
John Owens <jowens@ece.ucdavis.edu>
2017-04-10 11:47:02 -0700

    updated docs for 0.4

--- doc/0.4/search/pages_3.js
+++ doc/0.4/search/pages_3.js
@@ -2,6 +2,6 @@ var searchData=
 [
   ['gunrock_20faq',['Gunrock FAQ',['../faq.html',1,'']]],
   ['gunrock',['Gunrock',['../index.html',1,'']]],
-  ['groute',['Groute',['../md_stats_groute.html',1,'']]],
+  ['groute_20performance_20vs_2e_20gunrock',['Groute performance vs. Gunrock',['../md_stats_groute.html',1,'']]],
   ['gunrock_20v0_2e4_20release_20notes',['Gunrock v0.4 Release Notes',['../release_notes.html',1,'']]]
 ];
