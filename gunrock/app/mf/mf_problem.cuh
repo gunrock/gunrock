98c321a2a23183eef92321c7f15e63f4bc09738e
Agnieszka Lupinska <lupinska.agnieszka@gmail.com>
2018-09-13 03:38:17 -0700

    templates strange problemm with SizeT not defined in CsrT

--- gunrock/app/mf/mf_problem.cuh
+++ gunrock/app/mf/mf_problem.cuh
@@ -7,23 +7,24 @@
 
 /**
  * @file
- * template_problem.cuh
+ * mf_problem.cuh
  *
- * @brief GPU Storage management Structure for Template Problem Data
+ * @brief GPU Storage management Structure for Max Flow Problem Data
  */
 
 #pragma once
 
 #include <gunrock/app/problem_base.cuh>
+#include <gunrock/oprtr/1D_oprtr/for_all.cuh>
 
 namespace gunrock {
 namespace app {
-// TODO: change the name space
 namespace mf {
 
 /**
- * @brief Speciflying parameters for SSSP Problem
- * @param  parameters  The util::Parameter<...> structure holding all parameter info
+ * @brief Speciflying parameters for MF Problem
+ * @param  parameters  The util::Parameter<...> structure holding all 
+ *			parameter info
  * \return cudaError_t error message(s), if any
  */
 cudaError_t UseParameters_problem(
@@ -34,9 +35,10 @@ cudaError_t UseParameters_problem(
     GUARD_CU(gunrock::app::UseParameters_problem(parameters));
 
     // TODO: Add problem specific command-line parameter usages here, e.g.:
-     GUARD_CU(parameters.Use<bool>(
+    GUARD_CU(parameters.Use<bool>(
         "mark-pred",
-        util::OPTIONAL_ARGUMENT | util::MULTI_VALUE | util::OPTIONAL_PARAMETER,
+        util::OPTIONAL_ARGUMENT | util::MULTI_VALUE | 
+	util::OPTIONAL_PARAMETER,
         false,
         "Whether to mark predecessor info.",
         __FILE__, __LINE__));
@@ -45,57 +47,56 @@ cudaError_t UseParameters_problem(
 }
 
 /**
- * @brief Template Problem structure.
+ * @brief Max Flow Problem structure stores device-side arrays
  * @tparam _GraphT  Type of the graph
+ * @tparam _LabelT  Type of labels used in mf
+ * @tparam _ValueT  Type of signed integer to use as capacity and flow 
+		    of edges and as excess and height values of vertices.
  * @tparam _FLAG    Problem flags
  */
 template <
     typename _GraphT,
-    // TODO: Add problem specific template parameters here, e.g.:
-    // typename _ValueT = typename _GraphT::ValueT,
-    typename _ValueT = typename _GraphT::ValueT,
-    typename _LabelT = typename _GraphT::VertexT,
+    typename _ValueT  = typename _GraphT::ValueT,
     ProblemFlag _FLAG = Problem_None>
 struct Problem : ProblemBase<_GraphT, _FLAG>
 {
-    typedef _GraphT GraphT;
-    static const ProblemFlag FLAG = _FLAG;
-    typedef typename GraphT::VertexT VertexT;
-    typedef typename GraphT::SizeT   SizeT;
-    // TODO: Add algorithm specific types here, e.g.:
-    typedef _ValueT ValueT;
+    typedef	    _GraphT	      GraphT;
+    typedef typename GraphT::VertexT  VertexT;
+    typedef typename GraphT::SizeT    SizeT;
+    typedef typename GraphT::GpT      GpT;
+    typedef	    _ValueT	      ValueT;
 
-    // TODO: Add the graph representation used in the algorithm, e.g.:
-    // typedef typename GraphT::CsrT    CsrT;
-    typedef typename GraphT::GpT     GpT;
-    typedef typename GraphT::CsrT    CsrT;
-    typedef ProblemBase   <GraphT, FLAG> BaseProblem;
-    typedef DataSliceBase <GraphT, FLAG> BaseDataSlice;
+    static const ProblemFlag FLAG =	_FLAG;
+    typedef ProblemBase	 <GraphT, FLAG>	BaseProblem;
+    typedef DataSliceBase<GraphT, FLAG>	BaseDataSlice;
 
     //Helper structures
 
     /**
-     * @brief Data structure containing SSSP-specific data on indivual GPU.
+     * @brief Data structure containing MF-specific data on indivual GPU.
      */
     struct DataSlice : BaseDataSlice
     {
-        // TODO: add problem specific storage arrays, for example:
-        // util::Array1D<SizeT, ValueT>   distances; // distances from source
-        util::Array1D<SizeT, ValueT>     capacity;           /* Used for storing edge capacity */
-        util::Array1D<SizeT, ValueT>     excess;             /* Used for storing vertex excess */
-        util::Array1D<SizeT, ValueT>     flow;               /* Used for storing edge flow */
-        util::Array1D<SizeT, ValueT>     height;             /* Used for storing vertex height */
+        // MF-specific storage arrays:
+        util::Array1D<SizeT, ValueT>  capacity;  // edge capacity
+        util::Array1D<SizeT, ValueT>  flow;      // edge flow 
+        util::Array1D<SizeT, ValueT>  excess;    // vertex excess
+        util::Array1D<SizeT, VertexT> height;    // vertex height
+
+	VertexT	source;	// source vertex
+	VertexT sink;	// sink vertex
+
         /*
          * @brief Default constructor
          */
         DataSlice() : BaseDataSlice()
         {
-            // TODO: Set names of the problem specific arrays, for example:
-            // distances         .SetName("distances"           );
-            capacity        .SetName("capacity"         );
-            excess          .SetName("excess"           );
-            flow            .SetName("flow"             );
-            height          .SetName("height"           );
+	    source = util::PreDefinedValues<VertexT>::InvalidValue;
+	    sink = util::PreDefinedValues<VertexT>::InvalidValue;
+            capacity.SetName("capacity"	);
+            excess  .SetName("excess"	);
+            flow    .SetName("flow"	);
+            height  .SetName("height"	);
         }
 
         /*
@@ -118,19 +119,16 @@ struct Problem : ProblemBase<_GraphT, _FLAG>
                 GUARD_CU(util::SetDevice(this->gpu_idx));
 
             // TODO: Release problem specific data, e.g.:
-            // GUARD_CU(distances      .Release(target));
             GUARD_CU(capacity.Release(target));
             GUARD_CU(excess.Release(target));
             GUARD_CU(flow.Release(target));
             GUARD_CU(height.Release(target));
-
-
             GUARD_CU(BaseDataSlice ::Release(target));
             return retval;
         }
 
         /**
-         * @brief initializing sssp-specific data on each gpu
+         * @brief initializing mf-specific data on each gpu
          * @param     sub_graph   Sub graph on the GPU.
          * @param[in] gpu_idx     GPU device index
          * @param[in] target      Targeting device location
@@ -139,30 +137,27 @@ struct Problem : ProblemBase<_GraphT, _FLAG>
          */
         cudaError_t Init(
             GraphT        &sub_graph,
-	    int		   num_gpus= 1,
+	    int		   num_gpus = 1,
             int            gpu_idx = 0,
             util::Location target  = util::DEVICE,
             ProblemFlag    flag    = Problem_None)
         {
             cudaError_t retval  = cudaSuccess;
+	    SizeT nodes_size = sub_graph.nodes;
+	    SizeT edges_size = sub_graph.edges;
 
             GUARD_CU(BaseDataSlice::Init(
-		    sub_graph, num_gpus, gpu_idx, target, flag));
+				sub_graph, num_gpus, gpu_idx, target, flag));
 
-            // TODO: allocate problem specific data here, e.g.:
-            // GUARD_CU(distances .Allocate(sub_graph.nodes, target));
-            GUARD_CU(capacity.Allocate(sub_graph.edges, target));
-            GUARD_CU(excess.Allocate(sub_graph.nodes, target));
-            GUARD_CU(flow.Allocate(sub_graph.edges, target));
-            GUARD_CU(height.Allocate(sub_graph.nodes, target));
+            // 
+	    // Allocate data on Gpu
+	    //
+            GUARD_CU(capacity .Allocate(edges_size, target));
+            GUARD_CU(flow     .Allocate(edges_size, target));
+            GUARD_CU(excess   .Allocate(nodes_size, target));
+            GUARD_CU(height   .Allocate(nodes_size, target));
 
-
-            /*if (target & util::DEVICE)
-            {
-                // TODO: move sub-graph used by the problem onto GPU, e.g.:
-                // GUARD_CU(sub_graph.CsrT::Move(util::HOST, target, this -> stream));
-            }*/
-            GUARD_CU(sub_graph.Move(util::HOST, target, this -> stream));
+            //GUARD_CU(sub_graph.Move(util::HOST, target, this -> stream));
             return retval;
         } // Init
 
@@ -174,42 +169,38 @@ struct Problem : ProblemBase<_GraphT, _FLAG>
         cudaError_t Reset(util::Location target = util::DEVICE)
         {
             cudaError_t retval = cudaSuccess;
-            SizeT nodes = this -> sub_graph -> nodes;
-            SizeT edges = this -> sub_graph -> edges;
+            SizeT nodes_size = this -> sub_graph -> nodes;
+            SizeT edges_size = this -> sub_graph -> edges;
 
             // Ensure data are allocated
             // TODO: ensure size of problem specific data, e.g.:
             // GUARD_CU(distances.EnsureSize_(nodes, target));
-            GUARD_CU(capacity.EnsureSize_(edges, target));
-            GUARD_CU(excess.EnsureSize_(nodes, target));
-            GUARD_CU(flow.EnsureSize_(edges, target));
-            GUARD_CU(height.EnsureSize_(nodes, target));
+            GUARD_CU(capacity.EnsureSize_(edges_size, target));
+            GUARD_CU(excess.EnsureSize_(nodes_size, target));
+            GUARD_CU(flow.EnsureSize_(edges_size, target));
+            GUARD_CU(height.EnsureSize_(nodes_size, target));
 
             // Reset data
             // TODO: reset problem specific data, e.g.:
-            // GUARD_CU(distances.ForEach([]__host__ __device__
-            // (ValueT &distance){
-            //    distance = util::PreDefinedValues<ValueT>::MaxValue;
-            // }, nodes, target, this -> stream));
             GUARD_CU(capacity.ForEach([]__host__ __device__
             (ValueT &capacity){
               capacity = util::PreDefinedValues<ValueT>::InvalidValue;
-            }, edges, target, this -> stream));
+            }, edges_size, target, this -> stream));
 
             GUARD_CU(excess.ForEach([]__host__ __device__
             (ValueT &excess){
               excess = util::PreDefinedValues<ValueT>::InvalidValue;
-            }, nodes, target, this -> stream));
+            }, nodes_size, target, this -> stream));
 
             GUARD_CU(flow.ForEach([]__host__ __device__
             (ValueT &flow){
               flow = util::PreDefinedValues<ValueT>::InvalidValue;
-            }, edges, target, this -> stream));
+            }, edges_size, target, this -> stream));
 
             GUARD_CU(height.ForEach([]__host__ __device__
             (ValueT &height){
               height = util::PreDefinedValues<ValueT>::InvalidValue;
-            }, nodes, target, this -> stream));
+            }, nodes_size, target, this -> stream));
 
             return retval;
         }
@@ -222,7 +213,7 @@ struct Problem : ProblemBase<_GraphT, _FLAG>
     // Methods
 
     /**
-     * @brief SSSPProblem default constructor
+     * @brief MFProblem default constructor
      */
     Problem(
         util::Parameters &_parameters,
@@ -233,7 +224,7 @@ struct Problem : ProblemBase<_GraphT, _FLAG>
     }
 
     /**
-     * @brief SSSPProblem default destructor
+     * @brief MFProblem default destructor
      */
     virtual ~Problem()
     {
@@ -268,16 +259,17 @@ struct Problem : ProblemBase<_GraphT, _FLAG>
 
     /**
      * @brief Copy result distancess computed on GPUs back to host-side arrays.
-     * @param[out] h_distances Host array to store computed vertex distances from the source.
+     * @param[out] h_distances Host array to store computed vertex distances 
+     *		   from the source.
      * \return     cudaError_t Error message(s), if any
      */
     cudaError_t Extract(
         // TODO: add list of results to extract, e.g.:
-        // ValueT         *h_distances,
-        ValueT *h_excess,
+        ValueT	       **h_flow,
         util::Location  target      = util::DEVICE)
     {
         cudaError_t retval = cudaSuccess;
+	/* aga
         SizeT nodes = this -> org_graph -> nodes;
 
         if (this-> num_gpus == 1)
@@ -290,24 +282,16 @@ struct Problem : ProblemBase<_GraphT, _FLAG>
                 GUARD_CU(util::SetDevice(this->gpu_idx[0]));
 
                 // TODO: extract the results from single GPU, e.g.:
-                // GUARD_CU(data_slice.distances.SetPointer(
-                //    h_distances, nodes, util::HOST));
-                // GUARD_CU(data_slice.distances.Move(util::DEVICE, util::HOST));
                 GUARD_CU(data_slice.excess.SetPointer(
-                  h_excess, nodes, util::HOST));
+					h_excess, nodes, util::HOST));
                 GUARD_CU(data_slice.excess.Move(util::DEVICE, util::HOST));
 
             }
             else if (target == util::HOST)
             {
                 // TODO: extract the results from single CPU, e.g.:
-                // GUARD_CU(data_slice.distances.ForEach(h_distances,
-                //    []__host__ __device__
-                //    (const ValueT &distance, ValueT &h_distance){
-                //        h_distance = distance;
-                //    }, nodes, util::HOST));
-                GUARD_CU(data_slice.excess.ForEach(h_excess,
-                  []__host__ __device__
+		GUARD_CU(data_slice.excess.ForEach(h_excess,
+		  []__host__ __device__
                   (const ValueT &excess, ValueT &h_excess){
                     h_excess = excess;
                   }, nodes, util::HOST));
@@ -317,8 +301,6 @@ struct Problem : ProblemBase<_GraphT, _FLAG>
         { // num_gpus != 1
             // TODO: extract the results from multiple GPUs, e.g.:
             util::Array1D<SizeT, ValueT *> th_excess;
-            // th_distances.SetName("bfs::Problem::Extract::th_distances");
-            // GUARD_CU(th_distances.Allocate(this->num_gpus, util::HOST));
             th_excess.SetName("mf::Problem::Extract::th_excess");
             GUARD_CU(th_excess.Allocate(this->num_gpus, util::HOST));
 
@@ -345,13 +327,13 @@ struct Problem : ProblemBase<_GraphT, _FLAG>
 
             GUARD_CU(th_excess.Release());
         } //end if
-
+	*/
         return retval;
     }
 
     /**
      * @brief initialization function.
-     * @param     graph       The graph that SSSP processes on
+     * @param     graph       The graph that MF processes on
      * @param[in] Location    Memory location to work on
      * \return    cudaError_t Error message(s), if any
      */
@@ -369,7 +351,8 @@ struct Problem : ProblemBase<_GraphT, _FLAG>
 
         for (int gpu = 0; gpu < this->num_gpus; gpu++)
         {
-            data_slices[gpu].SetName("data_slices[" + std::to_string(gpu) + "]");
+            data_slices[gpu].SetName(
+			    "data_slices[" + std::to_string(gpu) + "]");
             if (target & util::DEVICE)
                 GUARD_CU(util::SetDevice(this->gpu_idx[gpu]));
 
@@ -377,7 +360,7 @@ struct Problem : ProblemBase<_GraphT, _FLAG>
 
             auto &data_slice = data_slices[gpu][0];
             GUARD_CU(data_slice.Init(this -> sub_graphs[gpu],
-                this -> gpu_idx[gpu], target, this -> flag));
+				this -> gpu_idx[gpu], target, this -> flag));
         } // end for (gpu)
 
         return retval;
@@ -409,9 +392,11 @@ struct Problem : ProblemBase<_GraphT, _FLAG>
         int gpu;
         VertexT src_;
         if (this->num_gpus <= 1)
-        {
-          gpu = 0; src_=src;
-        } else {
+	{
+	    gpu = 0; 
+	    src_=src;
+        } else 
+	{
             gpu = this -> org_graph -> partition_table[src];
             if (this -> flag & partitioner::Keep_Node_Num)
                 src_ = src;
@@ -422,7 +407,7 @@ struct Problem : ProblemBase<_GraphT, _FLAG>
          GUARD_CU2(cudaDeviceSynchronize(),
             "cudaDeviceSynchronize failed");
 
-         ValueT src_excess = 0;
+         ValueT src_excess = std::numeric_limits<ValueT>::max();
          if (target & util::HOST)
          {
              data_slices[gpu] -> excess[src_] = src_excess;
