1f24cf70debbe1607af763ca43fcfae94275c8ee
Leyuan Wang <laurawly@gmail.com>
2018-10-04 14:22:10 -0700

    tc test added

--- tests/tc/test_tc.cu
+++ tests/tc/test_tc.cu
@@ -12,228 +12,98 @@
  * @brief Simple test driver for computing TC.
  */
 
-#include <stdio.h>
-#include <vector>
-#include <utility>
-#include <iostream>
-#include <cstdlib>
-#include <algorithm>
-
-// Utilities and correctness-checking
-#include <gunrock/util/test_utils.cuh>
-
-// Graph construction utilities
-#include <gunrock/graphio/market.cuh>
-
-// MST includes
-#include <gunrock/global_indicator/tc/tc_enactor.cuh>
-#include <gunrock/global_indicator/tc/tc_problem.cuh>
-
-// Operator includes
-#include <gunrock/oprtr/advance/kernel.cuh>
-#include <gunrock/oprtr/filter/kernel.cuh>
-#include <gunrock/oprtr/intersection/kernel.cuh>
+#include <gunrock/global_indicator/tc/tc_app.cu>
+#include <gunrock/app/test_base.cuh>
 
 using namespace gunrock;
-using namespace gunrock::app;
-using namespace gunrock::util;
-using namespace gunrock::oprtr;
-using namespace gunrock::global_indicator::tc;
 
-///////////////////////////////////////////////////////////////////////////////
-// Housekeeping and utility routines
-///////////////////////////////////////////////////////////////////////////////
-
-void Usage()
-{
-    printf(
-        "test <graph-type> [graph-type-arguments]\n"
-        "Graph type and graph type arguments:\n"
-        "    market <matrix-market-file-name>\n"
-        "        Reads a Matrix-Market coordinate-formatted graph of\n"
-        "        directed/undirected edges from STDIN (or from the\n"
-        "        optionally-specified file).\n"
-        "    rmat (default: rmat_scale = 10, a = 0.57, b = c = 0.19)\n"
-        "        Generate R-MAT graph as input\n"
-        "        --rmat_scale=<vertex-scale>\n"
-        "        --rmat_nodes=<number-nodes>\n"
-        "        --rmat_edgefactor=<edge-factor>\n"
-        "        --rmat_edges=<number-edges>\n"
-        "        --rmat_a=<factor> --rmat_b=<factor> --rmat_c=<factor>\n"
-        "        --rmat_seed=<seed>\n"
-        "    rgg (default: rgg_scale = 10, rgg_thfactor = 0.55)\n"
-        "        Generate Random Geometry Graph as input\n"
-        "        --rgg_scale=<vertex-scale>\n"
-        "        --rgg_nodes=<number-nodes>\n"
-        "        --rgg_thfactor=<threshold-factor>\n"
-        "        --rgg_threshold=<threshold>\n"
-        "        --rgg_vmultipiler=<vmultipiler>\n"
-        "        --rgg_seed=<seed>\n\n"
-        "Optional arguments:\n"
-        "[--device=<device_index>] Set GPU(s) for testing (Default: 0).\n"
-        "[--quick]                 Skip the CPU reference validation process.\n"
-        "[--disable-size-check]    Disable frontier queue size check.\n"
-        "[--v]                     Print verbose per iteration debug info.\n"
-        "[--iteration-num=<num>]   Number of runs to perform the test.\n"
-        "[--quiet]                 No output (unless --json is specified).\n"
-        "[--json]                  Output JSON-format statistics to STDOUT.\n"
-        "[--jsonfile=<name>]       Output JSON-format statistics to file <name>\n"
-        "[--jsondir=<dir>]         Output JSON-format statistics to <dir>/name,\n"
-        "                          where name is auto-generated.\n"
-    );
-}
-
-///////////////////////////////////////////////////////////////////////////////
-// GPU TC test routines
-///////////////////////////////////////////////////////////////////////////////
+/******************************************************************************
+* Main
+******************************************************************************/
 
 /**
- * @brief Test entry
- *
- * @tparam VertexId
- * @tparam SizeT
- * @tparam Value
- * @tparam DEBUG
- * @tparam SIZE_CHECK
- *
- * @param[in] info Pointer to info contains parameters and statistics.
+ * @brief Enclosure to the main function
  */
-template <
-    typename VertexId,
-    typename SizeT,
-    typename Value>
-    //bool     DEBUG,
-    //bool     SIZE_CHECK >
-cudaError_t RunTest(Info<VertexId, Value, SizeT> *info)
+struct main_struct
 {
-    // define the problem data structure for graph primitive
-    typedef TCProblem<VertexId,
-            SizeT,
-            Value>
-            Problem;
-    typedef TCEnactor <Problem>
-            Enactor;
-
-    Csr<VertexId, SizeT, Value>* graph = info->csr_ptr;
-
-    int     num_gpus                = 1; //info->info["num_gpus"         ].get_int ();
-    int     max_grid_size           = info->info["max_grid_size"    ].get_int ();
-    int     iterations              = 1; //force to 1 info->info["num_iteration"].get_int();
-    bool    quiet_mode              = info->info["quiet_mode"       ].get_bool ();
-    bool    quick_mode              = info->info["quick_mode"       ].get_bool ();
-    bool    instrument              = info->info["instrument"       ].get_bool (); 
-    bool    debug                   = info->info["debug_mode"       ].get_bool (); 
-    bool    size_check              = info->info["size_check"       ].get_bool (); 
-    bool    stream_from_host        = info->info["stream_from_host" ].get_bool ();
-    double  max_queue_sizing        = info->info["max_queue_sizing" ].get_real ();
-    double  max_queue_sizing1       = info->info["max_queue_sizing1"].get_real ();
-    double  max_in_sizing           = info->info["max_in_sizing"    ].get_real (); 
-    std::string partition_method    = info->info["partition_method" ].get_str  (); 
-    double   partition_factor       = info->info["partition_factor" ].get_real (); 
-    int      partition_seed         = info->info["partition_seed"   ].get_int  ();
-    CpuTimer cpu_timer; 
-    cudaError_t retval             = cudaSuccess;
-
-    cpu_timer.Start();
-    json_spirit::mArray device_list = info->info["device_list"].get_array();
-    int* gpu_idx = new int[num_gpus];
-    for (int i = 0; i < num_gpus; i++) 
-        gpu_idx[i] = device_list[i].get_int();
-
-    // TODO: remove after merge mgpu-cq
-    ContextPtr   *context = (ContextPtr*  )info->context;
-    cudaStream_t *streams = (cudaStream_t*)info->streams;
-
-    VertexId *h_srcs              = new VertexId[graph->edges/2];
-    VertexId *h_dsts              = new VertexId[graph->edges/2];
-    SizeT    *h_tc                = new SizeT[graph->edges/2];
-
-    printf("size: %d\n", graph->edges/2);
-
-    // allocate problem on GPU create a pointer of the MSTProblem type
-    Problem problem(true);
-
-    // host results spaces
-    if (!quiet_mode) { printf("\nTC TEST\n"); }
-
-    // copy data from CPU to GPU initialize data members in DataSlice
-    util::GRError(problem.Init(stream_from_host,
-                                graph,
-                                NULL,
-                                num_gpus,
-                                gpu_idx,
-                                partition_method,
-                                streams,
-                                max_queue_sizing,
-                                max_in_sizing,
-                                partition_factor,
-                                partition_seed),
-                  "Problem TC Initialization Failed", __FILE__, __LINE__);
-
-    // allocate TC enactor map
-    Enactor enactor(
-    num_gpus, gpu_idx, instrument, debug, size_check);
-    util::GRError(enactor.Init(
-    context, &problem, max_grid_size));
-    cpu_timer.Stop();
-    info->info["preprocess_time"] = cpu_timer.ElapsedMillis();
-
-    // perform calculations
-    //GpuTimer gpu_timer;  // record the kernel running time
-    double elapsed_gpu = 0.0f;  // device elapsed running time
-
-    for (int iter = 0; iter < iterations; ++iter)
+    /**
+     * @brief the actual main function, after type switching
+     * @tparam VertexT    Type of vertex identifier
+     * @tparam SizeT      Type of graph size, i.e. type of edge identifier
+     * @tparam ValueT     Type of edge values
+     * @param  parameters Command line parameters
+     * @param  v,s,val    Place holders for type deduction
+     * \return cudaError_t error message(s), if any
+     */
+    template <
+        typename VertexT, // Use int as the vertex identifier
+        typename SizeT,   // Use int as the graph size type
+        typename ValueT>  // Use int as the value type
+    cudaError_t operator()(util::Parameters &parameters,
+        VertexT v, SizeT s, ValueT val)
     {
-        // reset values in DataSlice
-        util::GRError(problem.Reset(
-                          enactor.GetFrontierType(),
-                          max_queue_sizing,
-                          max_queue_sizing1),
-                      "TC Problem Data Reset Failed", __FILE__, __LINE__);
+        typedef typename app::TestGraph<VertexT, SizeT, ValueT,
+            graph::HAS_EDGE_VALUES | graph::HAS_CSR>
+            GraphT;
+        typedef typename GraphT::CsrT CsrT;
 
-        util::GRError(enactor.Reset(),
-            "TC Enactor Reset failed", __FILE__, __LINE__);
+        cudaError_t retval = cudaSuccess;
+        util::CpuTimer cpu_timer;
+        GraphT graph; // graph we process on
 
         cpu_timer.Start();
-
-        // launch TC enactor
-        util::GRError(enactor.template Enact<30>(),
-                      "TC Problem Enact Failed", __FILE__, __LINE__);
-
+        GUARD_CU(graphio::LoadGraph(parameters, graph));
+        // force edge values to be 1, don't enable this unless you really want to
+        //for (SizeT e=0; e < graph.edges; e++)
+        //    graph.CsrT::edge_values[e] = 1;
         cpu_timer.Stop();
-        elapsed_gpu += cpu_timer.ElapsedMillis();
+        parameters.Set("load-time", cpu_timer.ElapsedMillis());
+        //GUARD_CU(graph.CsrT::edge_values.Print("", 100));
+        //util::PrintMsg("sizeof(VertexT) = " + std::to_string(sizeof(VertexT))
+        //    + ", sizeof(SizeT) = " + std::to_string(sizeof(SizeT))
+        //    + ", sizeof(ValueT) = " + std::to_string(sizeof(ValueT)));
+
+        GUARD_CU(app::Set_Srcs    (parameters, graph));
+        ValueT  **ref_distances = NULL;
+        int num_srcs = 0;
+        bool quick = parameters.Get<bool>("quick");
+        // compute reference CPU TC solution for source-distance
+        if (!quick)
+        {            
+	    bool quiet = parameters.Get<bool>("quiet");
+            std::string validation = parameters.Get<std::string>("validation");
+            util::PrintMsg("Computing reference value ...", !quiet);
+            std::vector<VertexT> srcs
+                = parameters.Get<std::vector<VertexT> >("srcs");
+            num_srcs = srcs.size();
+            SizeT nodes = graph.nodes;
+            ref_distances = new ValueT*[num_srcs];
+            for (int i = 0; i < num_srcs; i++)
+            {
+		int ref_tc_count;
+                VertexT src = srcs[i];
+                util::PrintMsg("__________________________", !quiet);
+                float elapsed = app::tc::CPU_Reference(
+                    graph.csr(), ref_tc_count, NULL,
+                    src, quiet, false);
+                util::PrintMsg("--------------------------\nRun "
+                    + std::to_string(i) + " elapsed: "
+                    + std::to_string(elapsed) + " ms, src = "
+                    + std::to_string(src), !quiet);
+            }
+        }
+
+        std::vector<std::string> switches{"mark-pred", "advance-mode"};
+        GUARD_CU(app::Switch_Parameters(parameters, graph, switches,
+            [ref_tc_count](util::Parameters &parameters, GraphT &graph)
+            {
+                return app::tc::RunTests(parameters, graph, ref_tc_count);
+            }));
+
+        return retval;
     }
+};
 
-    elapsed_gpu /= iterations;
-    cpu_timer.Start();
-
-    if (!quiet_mode)
-    {
-        printf("GPU - Computation Complete in %lf msec.\n", elapsed_gpu);
-    }
-
-    // copy out results
-    util::GRError(problem.Extract(h_srcs, h_dsts, h_tc),
-                  "TC Problem Data Extraction Failed", __FILE__, __LINE__);
-
-    // write to mtx file
-    std::ofstream fout("tc_weight_graph.mtx");
-    if (fout.is_open())
-    {
-        fout << graph->nodes << " " << graph->nodes << " " << graph->edges/2 << std::endl;
-        for (int i = 0; i < graph->edges/2; ++i)
-            fout << h_srcs[i]+1 << " " << h_dsts[i]+1 << " " << h_tc[i] << std::endl;
-        fout.close();
-    }
-
-    info->ComputeCommonStats(enactor.enactor_stats.GetPointer(), elapsed_gpu, (VertexId*)NULL);
-
-    // clean up if necessary
-    cpu_timer.Stop();
-    info->info["postprocess_time"] = cpu_timer.ElapsedMillis();
-    return retval;
-    
-}
 
 ///////////////////////////////////////////////////////////////////////////////
 // Main function
@@ -242,45 +112,23 @@ cudaError_t RunTest(Info<VertexId, Value, SizeT> *info)
 int main(int argc, char** argv)
 {
     cudaError_t retval = cudaSuccess;
-    CommandLineArgs args(argc, argv);
-    int graph_args = argc - args.ParsedArgc() - 1;
-    if (argc < 2 || graph_args < 1 || args.CheckCmdLineFlag("help"))
+    util::Parameters parameters("test tc");
+    GUARD_CU(graphio::UseParameters(parameters));
+    GUARD_CU(app::sssp::UseParameters(parameters));
+    GUARD_CU(app::UseParameters_test(parameters));
+    GUARD_CU(parameters.Parse_CommandLine(argc, argv));
+    if (parameters.Get<bool>("help"))
     {
-        Usage();
-        return 1;
+        parameters.Print_Help();
+        return cudaSuccess;
     }
+    GUARD_CU(parameters.Check_Required());
 
-    typedef int VertexId;  // use int as the vertex identifier
-    typedef int Value;     // use int as the value type
-    typedef int SizeT;     // use int as the graph size type
-
-    CpuTimer cpu_timer, cpu_timer2;
-    cpu_timer.Start();
-
-    Csr<VertexId, Value, SizeT> csr(false);  // graph we process on
-    Info<VertexId, Value, SizeT> *info = new Info<VertexId, SizeT, Value>;
-
-    // graph construction or generation related parameters
-    info->info["undirected"] = true;  // always convert to undirected
-
-    cpu_timer2.Start();
-    info->Init("TC", args, csr);
-    cpu_timer2.Stop();
-    info->info["load_time"] = cpu_timer2.ElapsedMillis();
-
-    retval = RunTest<VertexId, SizeT, Value>(info);
-
-    cpu_timer.Stop();
-    info->info["total_time"] = cpu_timer.ElapsedMillis();
-
-    if (!(info->info["quiet_mode"].get_bool()))
-    {
-        info->DisplayStats();
-    }
-
-    info->CollectInfo();
-    
-    return retval;
+    return app::Switch_Types<
+        app::VERTEXT_U32B | app::VERTEXT_U64B |
+        app::SIZET_U32B | app::SIZET_U64B |
+        app::VALUET_U32B | app::DIRECTED | app::UNDIRECTED>
+        (parameters, main_struct());
 }
 
 // Leave this at the end of the file
