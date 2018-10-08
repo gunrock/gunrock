0281a6d6e1786e38302373bdde49a619605d2db3
Yuechao Pan <panyuechao@gmail.com>
2018-10-08 13:55:20 -0700

    update binary search routines

--- gunrock/util/binary_search.cuh
+++ gunrock/util/binary_search.cuh
@@ -14,8 +14,63 @@
 
 #pragma once
 
+#include <gunrock/util/device_intrinsics.cuh>
+
 namespace gunrock {
 namespace util {
+
+template <typename T1, typename ArrayT, typename SizeT,
+    typename CompareLambda>
+__host__ __device__
+SizeT BinarySearch(
+    const T1& element_to_find,
+    const ArrayT& elements,
+    SizeT left_index,
+    SizeT right_index,
+    CompareLambda comp) // strictly less
+{
+    SizeT center_index = 0;
+    do {
+        if (right_index - left_index <= 1)
+        {
+            if (comp(elements[right_index], element_to_find))
+                return right_index + 1;
+            if (comp(elements[left_index], element_to_find))
+                return right_index;
+            return left_index;
+        } else {
+            center_index = ((long long)left_index + (long long) right_index) >> 1;
+            if (comp(elements[center_index], element_to_find))
+            {
+                left_index = center_index + 1;
+            } else {
+                right_index = center_index - 1;
+            }
+#ifndef __CUDA_ARCH__
+            /*PrintMsg("Find " + std::to_string(element_to_find) +
+                " Pos " + std::to_string(center_index) +
+                " -> [" + std::to_string(left_index) +
+                ", " + std::to_string(right_index) + "]");*/
+#endif
+        }
+    } while (true);
+    //return 0;
+}
+
+template <typename T1, typename ArrayT, typename SizeT>
+__host__ __device__ __forceinline__
+SizeT BinarySearch(
+    const T1     &element_to_find,
+    const ArrayT &elements,
+          SizeT   left_index,
+          SizeT   right_index)
+{
+    return BinarySearch(element_to_find, elements, left_index, right_index,
+        [] (const T1 &a, const T1 &b)
+        {
+            return a < b;
+        });
+}
 
 template <typename T1, typename ArrayT, typename SizeT,
     typename LessOp, typename EqualOp>
@@ -31,11 +86,11 @@ SizeT BinarySearch_LeftMost(
     SizeT org_right_index = right_index;
     SizeT center_index = 0;
 
-    if (!less (elements[left_index], element_to_find) &&
-        !equal(elements[left_index], element_to_find))
-        return left_index - 1;
-    if (less(elements[right_index], element_to_find))
-        return right_index;
+    //if (!less (elements[left_index], element_to_find) &&
+    //    !equal(elements[left_index], element_to_find))
+    //    return left_index - 1;
+    //if (less(elements[right_index], element_to_find))
+    //    return right_index;
 
     while (right_index - left_index > 1)
     {
