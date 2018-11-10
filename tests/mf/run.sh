046a607c12e5f71482a255869ac9e3f391dc416b
neoblizz <osama94@gmail.com>
2018-11-09 23:07:41 -0800

    Moved largest test at the end, and fixed the output names.

--- tests/mf/run.sh
+++ tests/mf/run.sh
@@ -16,7 +16,7 @@ do
     fi
 done
 
-DEFAULT="--device=0 --quick=true"
+DEFAULT="--device=2 --quick=true"
 
 OPTION[0]=${DEFAULT}" --source=0 --sink=3"
 OPTION[1]=${DEFAULT}" --source=3 --sink=0"
@@ -24,9 +24,8 @@ OPTION[2]=${DEFAULT}" --source=226 --sink=227"
 OPTION[3]=${DEFAULT}" --source=235 --sink=236"
 OPTION[4]=${DEFAULT}" --source=0 --sink=1"
 OPTION[5]=${DEFAULT}" --source=3242 --sink=5435"
-OPTION[6]=${DEFAULT}" --source=35688 --sink=35689"
-OPTION[7]=${DEFAULT}" --source=8922 --sink=8923"
-
+OPTION[6]=${DEFAULT}" --source=8922 --sink=8923"
+OPTION[7]=${DEFAULT}" --source=35688 --sink=35689"
 
 
 
@@ -41,17 +40,16 @@ NAME[2]="$DATADIR/wei_problem_10_22"
 NAME[3]="$DATADIR/std_added.mtx"
 NAME[4]="$DATADIR/Wei_test_positive"
 NAME[5]="$LARGEDIR/larger.mtx"
-NAME[6]="$LARGEDIR/larger/file0.mtx"
-NAME[7]="$LARGEDIR/small_run1/file0.mtx"
-
+NAME[6]="$LARGEDIR/small_run1/file0.mtx"
+NAME[7]="$LARGEDIR/larger/file0.mtx"
  
 for k in {0..7}
 do
     SUFFIX="ubuntu16.04_TitanV"
     mkdir -p eval/$SUFFIX
 
-    echo $EXCUTION market ${NAME[$k]} ${OPTION[$k]} "> eval/$SUFFIX/$k_$SUFFIX.txt"
-    $EXCUTION market ${NAME[$k]} ${OPTION[$k]} > eval/$SUFFIX/$k_$SUFFIX.txt
+    echo $EXCUTION market ${NAME[$k]} ${OPTION[$k]} "> eval/$SUFFIX/${k}_$SUFFIX.txt"
+    $EXCUTION market ${NAME[$k]} ${OPTION[$k]} > eval/$SUFFIX/${k}_$SUFFIX.txt
     sleep 1
 done
 
