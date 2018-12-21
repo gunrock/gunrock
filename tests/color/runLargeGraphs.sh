55dee2d9e8037afd22d26e1ca678769a686bcd8c
samtruong <mstruong@ucdavis.edu>
2018-12-20 16:46:26 -0800

    FIX: run shell

--- tests/color/runLargeGraphs.sh
+++ tests/color/runLargeGraphs.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+
 read -p "--JPL: " JPL
 read -p "--no-conflict: " no_conflict
 read -p "--seed: " seed
 read -p "--user-iter: " usr_iter
 read -p "--hash-size: " hash_size
-read -p "--graph-file:" graph_file
-read -p "--quick:" quick
+read -p "--graph-file: " graph_file
+read -p "--quick: " quick
 read -p "--test-run: " test_run
 
 #address
@@ -22,15 +23,24 @@ addr10="ASIC_320ks/ASIC_320ks.mtx"
 addr11="cage13/cage13.mtx"
 addr12="atmosmodd/atmosmodd.mtx"
 
+ADDR_ARRAY=(
+  $addr1 $addr2 $addr3 $addr4 $addr5 $addr6
+  $addr7 $addr8 $addr9 $addr10 $addr11 $addr12
+)
+
 for i in `seq 1 12`;
 do
+  echo "/data-2/topc-datasets/gc-data/${ADDR_ARRAY[$i]}"
+  rm ./logs/$i.log
+  touch ./logs/$i.log
   ./bin/test_color_10.0_x86_64 --graph-type=market \
-  --graph-file="/data-2/topc-datasets/gc-data/ASIC_320ks/${addr${i}}"  \
+  --graph-file=/data-2/topc-datasets/gc-data/${ADDR_ARRAY[$i]} \
   --JPL=$JPL \
-  --no_conflict=$no_conflict \
+  --no-conflict=$no_conflict \
   --seed=$seed \
-  --usr_iter=$usr_iter \
-  --hash_size=$hash_size \
+  --user-iter=$usr_iter \
+  --hash-size=$hash_size \
   --quick=$quick \
-  --test-run=$test_run
+  --test-run=$test_run > ./logs/$i.log
 done
+
