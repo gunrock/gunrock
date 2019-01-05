b3e152473320c3daa60d302b52717a1f36a7255e
samtruong <mstruong@ucdavis.edu>
2019-01-05 11:25:19 -0800

    FIX: benchmark can run on any test on google sheet

--- tests/color/benchmark.sh
+++ tests/color/benchmark.sh
@@ -12,17 +12,17 @@
 #addresses
 cyangDataSet="/data-2/topc-datasets/gc-data/"
 addr0=${cyangDataSet}"offshore/offshore.mtx"
-#addr1=${cyangDataSet}"af_shell3/af_shell3.mtx"
-#addr2=${cyangDataSet}"parabolic_fem/parabolic_fem.mtx"
-#addr3=${cyangDataSet}"apache2/apache2.mtx"
-#addr4=${cyangDataSet}"ecology2/ecology2.mtx"
-#addr5=${cyangDataSet}"thermal2/thermal2.mtx"
-#addr6=${cyangDataSet}"G3_circuit/G3_circuit.mtx"
-#addr7=${cyangDataSet}"FEM_3D_thermal2/FEM_3D_thermal2.mtx"
-#addr8=${cyangDataSet}"thermomech_dK/thermomech_dK.mtx"
-#addr9=${cyangDataSet}"ASIC_320ks/ASIC_320ks.mtx"
-#addr10=${cyangDataSet}"cage13/cage13.mtx"
-#addr11=${cyangDataSet}"atmosmodd/atmosmodd.mtx"
+addr1=${cyangDataSet}"af_shell3/af_shell3.mtx"
+addr2=${cyangDataSet}"parabolic_fem/parabolic_fem.mtx"
+addr3=${cyangDataSet}"apache2/apache2.mtx"
+addr4=${cyangDataSet}"ecology2/ecology2.mtx"
+addr5=${cyangDataSet}"thermal2/thermal2.mtx"
+addr6=${cyangDataSet}"G3_circuit/G3_circuit.mtx"
+addr7=${cyangDataSet}"FEM_3D_thermal2/FEM_3D_thermal2.mtx"
+addr8=${cyangDataSet}"thermomech_dK/thermomech_dK.mtx"
+addr9=${cyangDataSet}"ASIC_320ks/ASIC_320ks.mtx"
+addr10=${cyangDataSet}"cage13/cage13.mtx"
+addr11=${cyangDataSet}"atmosmodd/atmosmodd.mtx"
 
 ADDR_ARRAY=($addr0 $addr1 $addr2 $addr3 $addr4 $addr5 $addr6 $addr7 $addr8 $addr9 $addr10 $addr11)
 
@@ -95,9 +95,6 @@ fi
 
 if [[ $option == "2" ]]
 then
-  echo "[1] Conflict resolve with random array"
-  echo "[2] Conflict resolve with degree heuristic"
-  read -p "Option: " resolve_type
   logsFolder="logsJPLminmax"
   mkdir $logsFolder
   for i in `seq 0 11`;
@@ -109,7 +106,7 @@ then
     ./bin/test_color_10.0_x86_64 --graph-type=market \
     --graph-file=${ADDR_ARRAY[$i]} \
     --JPL=true \
-    --no-conflict=$resolve_type\
+    --no-conflict=0 \
     --user-iter=0 \
     --hash-size=0 \
     --quick=true \
@@ -132,10 +129,6 @@ then
   do
       ITR_ARRAY=("${ITR_ARRAY[@]}" $line)
   done
-
-  echo "[1] Conflict resolve with random array"
-  echo "[2] Conflict resolve with degree heuristic"
-  read -p "Option: " resolve_type
   logsFolder="logsJPLminmaxFast"
   mkdir $logsFolder
   for i in `seq 0 11`;
@@ -147,7 +140,7 @@ then
     ./bin/test_color_10.0_x86_64 --graph-type=market \
     --graph-file=${ADDR_ARRAY[$i]} \
     --JPL=true \
-    --no-conflict=$resolve_type\
+    --no-conflict=0 \
     --user-iter=${ITR_ARRAY[$i]} \
     --hash-size=0 \
     --quick=true \
