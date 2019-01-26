3381a1c1c5c5bfa668a806db69e556a2ba410f08
Leyuan Wang <laurawly@gmail.com>
2019-01-26 15:27:40 -0800

    problem updated

--- gunrock/app/sm/sm_problem.cuh
+++ gunrock/app/sm/sm_problem.cuh
@@ -70,13 +70,57 @@ struct Problem : ProblemBase<_GraphT, _FLAG>
     struct DataSlice : BaseDataSlice
     {
         // sm-specific storage arrays
-        util::Array1D<SizeT, VertexT >    subgraphs  ; // number of subgraphs
+        util::Array1D<SizeT, VertexT >    subgraphs   ; // number of subgraphs
+        util::Array1D<SizeT, VertexT >    query_labels; // query graph labels
+        util::Array1D<SizeT, VertexT >    data_labels ; // data graph labels
+
+        util::Array1D<SizeT, SizeT   >    query_ro    ; // query graph row offsets
+        util::Array1D<SizeT, VertexT >    query_ci    ; // query graph column indices
+        util::Array1D<SizeT, SizeT   >    data_degree ; // data graph nodes' degrees
+        util::Array1D<SizeT, SizeT   >    query_degree; // query graph nodes' degrees
+        util::Array1D<SizeT, bool    >    isValid;     /** < Used for data node validation    */
+        util::Array1D<SizeT, SizeT   >    data_ne;     /** < Used for data graph node ne info */
+        util::Array1D<SizeT, SizeT   >    query_ne;    /** < Used for query graph node ne info*/
+        util::Array1D<SizeT, SizeT   >    counter;       /** < Used for counting iBFS sources   */
+        util::Array1D<SizeT, SizeT   >    num_subs;      /** < Used for counting iBFS sources   */
+        util::Array1D<SizeT, VertexT >    NG;          /** < Used for query node explore seq  */
+        util::Array1D<SizeT, SizeT   >    NG_ro;       /** < Used for query node sequence non-tree edge info */
+        util::Array1D<SizeT, VertexT>    NG_ci;       /** < Used for query node sequence non-tree edge info */
+        util::Array1D<SizeT, VertexT>    partial;     /** < Used for storing partial results */
+        util::Array1D<SizeT, VertexT>    src_node_id; /** < Used for storing compacted src nodes */
+        util::Array1D<SizeT, VertexT>    index;        /** < Used for storing intermediate flag val */
+        SizeT    nodes_data;       /** < Used for number of data nodes  */
+        SizeT    nodes_query;      /** < Used for number of query nodes */
+        SizeT    edges_data;       /** < Used for number of data edges   */
+        SizeT    num_matches;      /** < Used for number of matches in the result */
+
+        // query graph col_indices
         /*
          * @brief Default constructor
          */
         DataSlice() : BaseDataSlice()
         {
             subgraphs       .SetName("subgraphs"  	);
+            query_labels    .SetName("query_labels");
+            data_labels     .SetName("data_labels");
+            query_ro        .SetName("query_ro");
+            data_degree     .SetName("data_degree");
+            query_degree    .SetName("query_degree");
+            isValid         .SetName("isValid");
+            data_ne         .SetName("data_ne");
+            query_ne        .SetName("query_ne");
+            counter         .SetName("counter");
+            num_subs        .SetName("num_subs");
+            NG              .SetName("NG");
+            NG_ro           .SetName("NG_ci");
+            NG_ci           .SetName("NG_ro");
+            partial         .SetName("partial");
+            src_node_id     .SetName("src_node_id");
+            index           .SetName("index");
+            nodes_data      = 0;
+            nodes_query     = 0;       
+            edges_data      = 0;
+            num_matches     = 0; 
         }
 
         /*
@@ -99,6 +143,20 @@ struct Problem : ProblemBase<_GraphT, _FLAG>
                 GUARD_CU(util::SetDevice(this->gpu_idx));
 
             GUARD_CU(subgraphs      .Release(target));
+            GUARD_CU(query_labels   .Release(target));
+            GUARD_CU(data_labels    .Release(target));
+            GUARD_CU(data_degree    .Release(target));
+            GUARD_CU(query_degree   .Release(target));
+            GUARD_CU(isValid        .Release(target));
+            GUARD_CU(query_ne       .Release(target));
+            GUARD_CU(counter        .Release(target));
+            GUARD_CU(num_subs       .Release(target));
+            GUARD_CU(NG             .Release(target));
+            GUARD_CU(NG_ro          .Release(target));
+            GUARD_CU(NG_ci          .Release(target));
+            GUARD_CU(partial        .Release(target));
+            GUARD_CU(src_node_id    .Release(target));
+            GUARD_CU(index          .Release(target));
             GUARD_CU(BaseDataSlice ::Release(target));
             return retval;
         }
@@ -113,7 +171,9 @@ struct Problem : ProblemBase<_GraphT, _FLAG>
          * \return    cudaError_t Error message(s), if any
          */
         cudaError_t Init(
-            GraphT        &sub_graph,
+            GraphT         &sub_graph,
+            GraphT         &data_graph,
+            GraphT         &query_graph,
             int            num_gpus = 1,
             int            gpu_idx = 0,
             util::Location target  = util::DEVICE,
@@ -121,13 +181,111 @@ struct Problem : ProblemBase<_GraphT, _FLAG>
         {
             cudaError_t retval  = cudaSuccess;
 
-            GUARD_CU(BaseDataSlice::Init(
-                sub_graph, num_gpus, gpu_idx, target, flag));
-            GUARD_CU(subgraphs  	.Allocate(sub_graph.nodes, target));
+            GUARD_CU(BaseDataSlice::Init(sub_graph, num_gpus, gpu_idx, target, flag));
+            GUARD_CU(subgraphs      .Allocate(sub_graph.nodes, target));
+            GUARD_CU(query_ro       .Allocate(query_graph.nodes + 1, util::HOST | util::DEVICE)); 
+            GUARD_CU(data_degree    .Allocate(data_graph.nodes, util::DEVICE));
+            GUARD_CU(query_degree   .Allocate(query_graph.nodes, util::HOST | util::DEVICE));
+            GUARD_CU(isValid        .Allocate(data_graph.nodes, util::DEVICE));
+            GUARD_CU(query_ne       .Allocate(query_graph.nodes, util::HOST | util::DEVICE));
+            GUARD_CU(counter        .Allocate(1, util::HOST | util::DEVICE));
+            GUARD_CU(num_subs       .Allocate(1, util::HOST | util::DEVICE));
+            GUARD_CU(NG             .Allocate(query_graph.nodes, util::HOST | util::DEVICE));
+            //query node sequence connection row offsets/column indices
+            if(query_graph.nodes > 2) {
+                GUARD_CU(NG_ro      .Allocate(query_graph.nodes - 1, util::HOST | util::DEVICE));
+                GUARD_CU(NG_ci      .Allocate(query_graph.edges/2 - query_graph.nodes + 1, util::HOST | util::DEVICE));
+            }
+            // partial results storage: as much as possible
+            GUARD_CU(partial        .Allocate(query_graph.nodes * data_graph.edges,  util::DEVICE));
+            GUARD_CU(src_node_id    .Allocate(data_graph.edges,  util::DEVICE));
+            GUARD_CU(index          .Allocate(data_graph.edges,  util::DEVICE));
+
+            // Initialize query graph node degree by row offsets
+            // neighbor node encoding = sum of neighbor node labels
+            for(int i = 0; i < query_graph.nodes; i++) {
+                query_degree[i] = query_graph.row_offsets[i+1] - query_graph.row_offsets[i];
+                query_ne[i] = 0;
+                for(int j = query_graph.row_offsets[i]; j < query_graph.row_offsets[i+1]; j++){ 
+                    query_ne[i] += 1;
+                }
+            }
+            // Generate query graph node exploration sequence based on maximum likelihood estimation (MLE)
+            // node mapping degree, TODO:probablity estimation based on label and degree, degree
+            int *d_m = new int[query_graph.nodes];
+            memset(d_m, 0, sizeof(int)*query_graph.nodes);
+            int degree_max = query_degree[0];
+            int index = 0;
+            for(int i=0; i < query_graph.nodes; i++) {
+                if(i == 0) {
+                    for(int j = 1; j < query_graph.nodes; j++) {
+                        if(query_degree[j] > degree_max) {
+                            index = j;
+                            degree_max = query_degree[j];
+                        }
+                    }
+                }
+                else {
+                    int dm_max=0;
+                    index = 0;
+                    for(int j=0; j<query_graph.nodes; j++){
+                        if(d_m[j]>=0) {
+                            if(index*degree_max+query_degree[j]>dm_max){
+                                dm_max = index*degree_max+query_degree[j];
+                                index = j;
+                            }
+                        }
+                    }
+                }
+                NG[i] = index;
+                d_m[index] = -1;
+                for(int j = query_graph.row_offsets[index]; j < query_graph.row_offsets[index+1]; j++)
+                    if(d_m[query_graph.column_indices[j]]!=-1)
+                        d_m[query_graph.column_indices[j]]++;
+            }
+            delete[] d_m;
+            // fill query node non-tree edges info 
+            if(query_graph.nodes>2){
+                NG_ro[0] = 0;
+                for(int id=2; id<query_graph.nodes; id++){
+                    int idx=0;
+                    for(int j=0; j<id-1; j++)
+                        for(int i = query_graph.row_offsets[id]; i < query_graph.row_offsets[id+1]; i++)
+                            if(NG[j]==query_graph.column_indices[i])
+                                // store the index of the dest node instead of the node id itself
+                                NG_ci[NG_ro[id-2]+idx++] = j;
+                    NG_ro[id-1] = idx;
+                }
+            }
+            // Initialize query row offsets with query_graph.row_offsets
+            GUARD_CU(query_ro.ForAll([query_ro, query_graph] 
+                __host__(SizeT * x, const SizeT &pos) { x[pos] = query_graph.row_offsets[pos]; }, 
+                query_graph.nodes + 1, target, this->stream));
+
+	    GUARD_CU(isValid.ForAll(
+	        [] __device__(bool * x, const SizeT &pos) { x[pos] = false; },
+		data_graph.nodes, target, this->stream));
+	    GUARD_CU(data_degree.ForAll(
+	        [] __device__(SizeT * x, const SizeT &pos) { x[pos] = 0; },
+		data_graph.nodes, target, this->stream));
+	    GUARD_CU(counter.ForAll(
+	        [] __device__(SizeT * x, const SizeT &pos) { x[pos] = 0; },
+		1, target, this->stream));
+	    GUARD_CU(num_subs.ForAll(
+	        [] __device__(SizeT * x, const SizeT &pos) { x[pos] = 0; },
+		1, target, this->stream));
+
+            nodes_data   = data_graph.nodes;
+            nodes_query  = query_graph.nodes;
+            edges_data   = data_graph.edges;
+
             if (target & util::DEVICE)
             {
                 GUARD_CU(sub_graph.CsrT::Move(util::HOST, target, this -> stream));
+                GUARD_CU(query_ro.Move(util::HOST, target, this -> stream));
+                return retval;
             }
+
             return retval;
         } // Init
 
@@ -290,11 +448,12 @@ struct Problem : ProblemBase<_GraphT, _FLAG>
      * \return    cudaError_t Error message(s), if any
      */
     cudaError_t Init(
-        GraphT           &graph,
-        util::Location    target = util::DEVICE)
+        GraphT           &data_graph,
+        GraphT           &query_graph,
+        util::Location   target = util::DEVICE)
     {
         cudaError_t retval = cudaSuccess;
-        GUARD_CU(BaseProblem::Init(graph, target));
+        GUARD_CU(BaseProblem::Init(data_graph, target));
         data_slices = new util::Array1D<SizeT, DataSlice>[this->num_gpus];
 
         for (int gpu = 0; gpu < this->num_gpus; gpu++)
@@ -306,7 +465,7 @@ struct Problem : ProblemBase<_GraphT, _FLAG>
             GUARD_CU(data_slices[gpu].Allocate(1, target | util::HOST));
 
             auto &data_slice = data_slices[gpu][0];
-            GUARD_CU(data_slice.Init(this -> sub_graphs[gpu],
+            GUARD_CU(data_slice.Init(this -> sub_graphs[gpu], data_graph, query_graph,
                 this -> num_gpus, this -> gpu_idx[gpu], target, this -> flag));
         } // end for (gpu)
 
