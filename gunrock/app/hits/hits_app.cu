195db746f227df1fa724cdafd970d25e03b50396
Jonathan Wapman <jdwapman@ucdavis.edu>
2019-02-12 11:27:16 -0800

    Removed unneeded commented code

--- gunrock/app/hits/hits_app.cu
+++ gunrock/app/hits/hits_app.cu
@@ -31,13 +31,6 @@ cudaError_t UseParameters(util::Parameters &parameters)
     GUARD_CU(UseParameters_problem(parameters));
     GUARD_CU(UseParameters_enactor(parameters));
 
-    // GUARD_CU(parameters.Use<std::string>(
-    //     "output-filename",
-    //     util::REQUIRED_ARGUMENT | util::SINGLE_VALUE | util::OPTIONAL_PARAMETER,
-    //     "",
-    //     "file to output ranking values",
-    //     __FILE__, __LINE__));
-
     return retval;
 }
 
@@ -153,32 +146,6 @@ cudaError_t RunTests(
             }
     }
 
-    // if (parameters.Get<std::string>("output-filename") != "")
-    // {
-    //     cpu_timer.Start();
-    //     std::ofstream fout;
-    //     size_t buf_size = 1024 * 1024 * 16;
-    //     char *fout_buf = new char[buf_size];
-    //     fout.rdbuf() -> pubsetbuf(fout_buf, buf_size);
-    //     fout.open(parameters.Get<std::string>("output-filename").c_str());
-
-    //     fout << "Hub Ranks:" << std::endl;
-    //     for (VertexT v = 0; v < graph.nodes; v++)
-    //     {
-    //         fout<< v << "," << h_hrank[v] << std::endl;
-    //     }
-
-    //     fout << "Authority Ranks:" << std::endl;
-    //     for (VertexT v = 0; v < graph.nodes; v++)
-    //     {
-    //         fout<< v << "," << h_arank[v] << std::endl;
-    //     }
-    //     fout.close();
-    //     delete[] fout_buf; fout_buf = NULL;
-    //     cpu_timer.Stop();
-    //     parameters.Set("write-time", cpu_timer.ElapsedMillis());
-    // }
-
     // compute running statistics
     // <TODO> change NULL to problem specific per-vertex visited marker, e.g. h_distances
     info.ComputeTraversalStats(enactor, (VertexT*)NULL);
@@ -205,147 +172,8 @@ cudaError_t RunTests(
 } // namespace app
 } // namespace gunrock
 
-// ===========================================================================================
-// ========================= CODE BELOW THIS LINE NOT NEEDED FOR TESTS =======================
-// ===========================================================================================
-
-// /*
-// * @brief Entry of gunrock_template function
-// * @tparam     GraphT     Type of the graph
-// * @tparam     ValueT     Type of the distances
-// * @param[in]  parameters Excution parameters
-// * @param[in]  graph      Input graph
-// * @param[out] distances  Return shortest distance to source per vertex
-// * @param[out] preds      Return predecessors of each vertex
-// * \return     double     Return accumulated elapsed times for all runs
-// */
-// template <typename GraphT, typename ValueT = typename GraphT::ValueT>
-// double gunrock_Template(
-//     gunrock::util::Parameters &parameters,
-//     GraphT &graph
-//     // TODO: add problem specific outputs, e.g.:
-//     //ValueT **distances
-//     )
-// {
-//     typedef typename GraphT::VertexT VertexT;
-//     typedef gunrock::app::Template::Problem<GraphT  > ProblemT;
-//     typedef gunrock::app::Template::Enactor<ProblemT> EnactorT;
-//     gunrock::util::CpuTimer cpu_timer;
-//     gunrock::util::Location target = gunrock::util::DEVICE;
-//     double total_time = 0;
-//     if (parameters.UseDefault("quiet"))
-//         parameters.Set("quiet", true);
-
-//     // Allocate problem and enactor on GPU, and initialize them
-//     ProblemT problem(parameters);
-//     EnactorT enactor;
-//     problem.Init(graph  , target);
-//     enactor.Init(problem, target);
-
-//     int num_runs = parameters.Get<int>("num-runs");
-//     // TODO: get problem specific inputs, e.g.:
-//     // std::vector<VertexT> srcs = parameters.Get<std::vector<VertexT>>("srcs");
-//     // int num_srcs = srcs.size();
-//     for (int run_num = 0; run_num < num_runs; ++run_num)
-//     {
-//         // TODO: problem specific inputs, e.g.:
-//         // int src_num = run_num % num_srcs;
-//         // VertexT src = srcs[src_num];
-//         problem.Reset(/*src,*/ target);
-//         enactor.Reset(/*src,*/ target);
-
-//         cpu_timer.Start();
-//         enactor.Enact(/*src*/);
-//         cpu_timer.Stop();
-
-//         total_time += cpu_timer.ElapsedMillis();
-//         // TODO: extract problem specific data, e.g.:
-//         problem.Extract(/*distances[src_num]*/);
-//     }
-
-//     enactor.Release(target);
-//     problem.Release(target);
-//     // TODO: problem specific clean ups, e.g.:
-//     // srcs.clear();
-//     return total_time;
-// }
-
-
-//  * @brief Simple interface take in graph as CSR format
-//  * @param[in]  num_nodes   Number of veritces in the input graph
-//  * @param[in]  num_edges   Number of edges in the input graph
-//  * @param[in]  row_offsets CSR-formatted graph input row offsets
-//  * @param[in]  col_indices CSR-formatted graph input column indices
-//  * @param[in]  edge_values CSR-formatted graph input edge weights
-//  * @param[in]  num_runs    Number of runs to perform SSSP
-//  * @param[in]  sources     Sources to begin traverse, one for each run
-//  * @param[in]  mark_preds  Whether to output predecessor info
-//  * @param[out] distances   Return shortest distance to source per vertex
-//  * @param[out] preds       Return predecessors of each vertex
-//  * \return     double      Return accumulated elapsed times for all runs
- 
-// template <
-//     typename VertexT = int,
-//     typename SizeT   = int,
-//     typename GValueT = unsigned int,
-//     typename TValueT = GValueT>
-// float Template(
-//     const SizeT        num_nodes,
-//     const SizeT        num_edges,
-//     const SizeT       *row_offsets,
-//     const VertexT     *col_indices,
-//     const GValueT     *edge_values,
-//     const int          num_runs
-//     // TODO: add problem specific inputs and outputs, e.g.:
-//     //      VertexT     *sources,
-//     //      SSSPValueT **distances
-//     )
-// {
-//     // TODO: change to other graph representation, if not using CSR
-//     typedef typename gunrock::app::TestGraph<VertexT, SizeT, GValueT,
-//         gunrock::graph::HAS_EDGE_VALUES | gunrock::graph::HAS_CSR>
-//         GraphT;
-//     typedef typename GraphT::CsrT CsrT;
-
-//     // Setup parameters
-//     gunrock::util::Parameters parameters("Template");
-//     gunrock::graphio::UseParameters(parameters);
-//     gunrock::app::Template::UseParameters(parameters);
-//     gunrock::app::UseParameters_test(parameters);
-//     parameters.Parse_CommandLine(0, NULL);
-//     parameters.Set("graph-type", "by-pass");
-//     parameters.Set("num-runs", num_runs);
-//     // TODO: problem specific inputs, e.g.:
-//     // std::vector<VertexT> srcs;
-//     // for (int i = 0; i < num_runs; i ++)
-//     //     srcs.push_back(sources[i]);
-//     // parameters.Set("srcs", srcs);
-
-//     bool quiet = parameters.Get<bool>("quiet");
-//     GraphT graph;
-//     // Assign pointers into gunrock graph format
-//     // TODO: change to other graph representation, if not using CSR
-//     graph.CsrT::Allocate(num_nodes, num_edges, gunrock::util::HOST);
-//     graph.CsrT::row_offsets   .SetPointer(row_offsets, num_nodes + 1, gunrock::util::HOST);
-//     graph.CsrT::column_indices.SetPointer(col_indices, num_edges, gunrock::util::HOST);
-//     graph.CsrT::edge_values   .SetPointer(edge_values, num_edges, gunrock::util::HOST);
-//     graph.FromCsr(graph.csr(), true, quiet);
-//     gunrock::graphio::LoadGraph(parameters, graph);
-
-//     // Run the Template
-//     // TODO: add problem specific outputs, e.g.
-//     double elapsed_time = gunrock_Template(parameters, graph /*, distances*/);
-
-//     // Cleanup
-//     graph.Release();
-//     // TODO: problem specific cleanup
-//     // srcs.clear();
-
-//     return elapsed_time;
-// }
-
-// // Leave this at the end of the file
-// // Local Variables:
-// // mode:c++
-// // c-file-style: "NVIDIA"
-// // End:
+// Leave this at the end of the file
+// Local Variables:
+// mode:c++
+// c-file-style: "NVIDIA"
+// End:
