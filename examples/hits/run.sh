b1a0bd977817bc469723718614d1f405f4b6f07c
neoblizz <osama94@gmail.com>
2019-04-17 14:10:13 -0700

    Moved tests to examples.

--- examples/hits/run.sh
+++ examples/hits/run.sh
@@ -1,28 +1,37 @@
 #!/bin/bash
 
-# get all execution files in ./bin
-files=./bin/*
-# split file names into arr
+OPTION[0]="--max-iter=100 --tol=0.000001"
+
+MARK[0]=""
+
+#get all execution files in ./bin
+files=(./bin/*)
+#split file names into arr
 arr=$(echo $files | tr " " "\n")
 max_ver_num="$"
 exe_file=${arr[0]}
-# iterate over all file names to get the largest version number
+#iterate over all file names to get the largest version number
 for x in $arr
 do
-    output=$(grep -o "[0-9]\.[0-9]" <<< "$x")
+    output=$(grep -o "[0-9]\.[0-9]" <<<"$x")
     if [ "$output" \> "$max_ver_num" ]; then
         exe_file=$x
     fi
 done
 
-# put OS and Device type here
-SUFFIX="GUNROCK_v0-5-0"
+#put OS and Device type here
+SUFFIX="ubuntu16.04_GTX1060"
+EXECUTION=$exe_file
+DATADIR="../../dataset/small"
 
 mkdir -p eval/$SUFFIX
 
-for i in delaunay_n13 delaunay_n21 delaunay_n24 soc-LiveJournal1 hollywood-2009 indochina-2004
+for i in test_hits test_pr chesapeake
 do
-    echo $exe_file market ../../dataset/large/$i/$i.mtx"> ./eval/$SUFFIX/${i}.$SUFFIX${MARKS}.txt"
-         $exe_file market ../../dataset/large/$i/$i.mtx > ./eval/$SUFFIX/${i}.$SUFFIX${MARKS}.txt
-    sleep 1
+    for j in 0
+    do
+        echo $EXECUTION market $DATADIR/$i.mtx ${OPTION[$j]} "> eval/$SUFFIX/$i.$SUFFIX${MARK[$j]}.txt"
+             $EXECUTION market $DATADIR/$i.mtx ${OPTION[$j]}  > eval/$SUFFIX/$i.$SUFFIX${MARK[$j]}.txt
+        sleep 1
+    done
 done
--- tests/tc/run.sh
+++
@@ -1,1 +0,0 @@
-./bin/tc_main_10.0_x86_64 market ../../dataset/small/data_sm.mtx --undirected=1 --num-runs=10
