fa25e503e0401f802f3aef176a187a4ec8e720fe
neoblizz <osama94@gmail.com>
2019-11-05 13:09:02 -0800

    Cleaner implementation of coloring

---
+++ gunrock/app/color/color_enactor_new.cuh
@@ -0,0 +1,358 @@
+// ----------------------------------------------------------------
+// Gunrock -- Fast and Efficient GPU Graph Library
+// ----------------------------------------------------------------
+// This source code is distributed under the terms of LICENSE.TXT
+// in the root directory of this source distribution.
+// ----------------------------------------------------------------
+
+/**
+ * @file
+ * color_enactor.cuh
+ *
+ * @brief color Problem Enactor
+ */
+
+#pragma once
+
+#include <gunrock/util/sort_device.cuh>
+
+#include <gunrock/app/enactor_base.cuh>
+#include <gunrock/app/enactor_iteration.cuh>
+#include <gunrock/app/enactor_loop.cuh>
+
+#include <gunrock/app/color/color_problem.cuh>
+
+#if defined(__CUDA_ARCH__) && __CUDA_ARCH__ >= 600
+#include <gunrock/oprtr/1D_oprtr/for.cuh>
+#endif
+
+#include <gunrock/oprtr/oprtr.cuh>
+
+namespace gunrock {
+namespace app {
+namespace color {
+
+/**
+ * @brief Speciflying parameters for color Enactor
+ * @param parameters The util::Parameter<...> structure holding all parameter
+ * info \return cudaError_t error message(s), if any
+ */
+cudaError_t UseParameters_enactor(util::Parameters &parameters) {
+  cudaError_t retval = cudaSuccess;
+  GUARD_CU(app::UseParameters_enactor(parameters));
+
+  return retval;
+}
+
+/**
+ * @brief defination of color iteration loop
+ * @tparam EnactorT Type of enactor
+ */
+template <typename EnactorT>
+struct ColorIterationLoop
+    : public IterationLoopBase<EnactorT, Use_FullQ | Push> {
+  typedef typename EnactorT::VertexT VertexT;
+  typedef typename EnactorT::SizeT SizeT;
+  typedef typename EnactorT::ValueT ValueT;
+  typedef typename EnactorT::Problem::GraphT::CsrT CsrT;
+  typedef typename EnactorT::Problem::GraphT::GpT GpT;
+
+  typedef IterationLoopBase<EnactorT, Use_FullQ | Push> BaseIterationLoop;
+
+  ColorIterationLoop() : BaseIterationLoop() {}
+
+  /**
+   * @brief Core computation of color, one iteration
+   * @param[in] peer_ Which GPU peers to work on, 0 means local
+   * \return cudaError_t error message(s), if any
+   */
+  cudaError_t Core(int peer_ = 0) {
+    // --
+    // Alias variables
+
+    auto &data_slice = this->enactor->problem->data_slices[this->gpu_num][0];
+
+    auto &enactor_slice =
+        this->enactor
+            ->enactor_slices[this->gpu_num * this->enactor->num_gpus + peer_];
+
+    auto &enactor_stats = enactor_slice.enactor_stats;
+    auto &graph = data_slice.sub_graph[0];
+    auto &frontier = enactor_slice.frontier;
+    auto &oprtr_parameters = enactor_slice.oprtr_parameters;
+    auto &retval = enactor_stats.retval;
+    auto &iteration = enactor_stats.iteration;
+
+    auto &colors = data_slice.colors;
+    auto &rand = data_slice.rand;
+    auto &color_predicate = data_slice.color_predicate;
+    auto &color_temp = data_slice.color_temp;
+    auto &color_temp2 = data_slice.color_temp2;
+    auto &color_balance = data_slice.color_balance;
+    auto &test_run = data_slice.test_run;
+    auto &min_color = data_slice.min_color;
+    auto stream = oprtr_parameters.stream;
+    auto gen = data_slice.gen;
+
+    //======================================================================//
+    // Jones-Plassman-Luby Graph Coloring: Compute Operator                 //
+    //======================================================================//
+    if (iteration % 2) {
+      curandGenerateUniform(gen, rand.GetPointer(util::DEVICE),
+                            graph.nodes);
+    }
+
+    // The filter operation
+    auto filter_op = [graph, colors, rand, iteration, colored] 
+    __host__ __device__(
+      const VertexT &src, VertexT &dest,
+      const SizeT &edge_id, const VertexT &input_item,
+      const SizeT &input_pos, SizeT &output_pos) -> bool {
+      
+      if (!util::isValid(dest)) return false;
+      VertexT v = dest;
+
+      SizeT start_edge = graph.CsrT::GetNeighborListOffset(v);
+      SizeT num_neighbors = graph.CsrT::GetNeighborListLength(v);
+
+      bool colormax = true;
+      bool colormin = true;
+      int color = iteration * 2;
+
+      for (SizeT e = start_edge; e < start_edge + num_neighbors; e++) {
+        VertexT u = graph.CsrT::GetEdgeDest(e);
+
+        if ((util::isValid(colors[u])) && (colors[u] != color + 1) &&
+                (colors[u] != color + 2) ||
+            (v == u))
+          continue;
+        if (rand[v] <= rand[u]) colormax = false;
+        if (rand[v] >= rand[u]) colormin = false;
+      }
+
+      if (colormax) { 
+        colors[v] = color + 1; 
+        return false; 
+      } else if (colormin) { 
+        colors[v] = color + 2; 
+        return false; 
+      } else { 
+        return true; 
+      }
+    };
+
+    frontier.queue_reset = true;
+    GUARD_CU(oprtr::Filter<oprtr::OprtrType_V2V>(
+      graph.csr(), frontier.V_Q(), frontier.Next_V_Q(), oprtr_parameters,
+      filter_op));
+
+    // Get back the resulted frontier length
+    GUARD_CU(frontier.work_progress.GetQueueLength(
+      frontier.queue_index, frontier.queue_length, false,
+      oprtr_parameters.stream, true));
+
+    return retval;
+  }
+
+  /**
+   * @brief Routine to combine received data and local data
+   * @tparam NUM_VERTEX_ASSOCIATES Number of data associated with each
+   * transmition item, typed VertexT
+   * @tparam NUM_VALUE__ASSOCIATES Number of data associated with each
+   * transmition item, typed ValueT
+   * @param  received_length The numver of transmition items received
+   * @param[in] peer_ which peer GPU the data came from
+   * \return cudaError_t error message(s), if any
+   */
+  template <int NUM_VERTEX_ASSOCIATES, int NUM_VALUE__ASSOCIATES>
+  cudaError_t ExpandIncoming(SizeT &received_length, int peer_) {
+    // ================ INCOMPLETE TEMPLATE - MULTIGPU ====================
+
+    auto &data_slice = this->enactor->problem->data_slices[this->gpu_num][0];
+    auto &enactor_slice =
+        this->enactor
+            ->enactor_slices[this->gpu_num * this->enactor->num_gpus + peer_];
+    // auto iteration = enactor_slice.enactor_stats.iteration;
+    // auto         &distances          =   data_slice.distances;
+
+    auto expand_op = [] __host__ __device__(
+                         VertexT & key, const SizeT &in_pos,
+                         VertexT *vertex_associate_ins,
+                         ValueT *value__associate_ins) -> bool {
+      // ValueT in_val  = value__associate_ins[in_pos];
+      // ValueT old_val = atomicMin(distances + key, in_val);
+      // if (old_val <= in_val)
+      //     return false;
+      return true;
+    };
+
+    cudaError_t retval =
+        BaseIterationLoop::template ExpandIncomingBase<NUM_VERTEX_ASSOCIATES,
+                                                       NUM_VALUE__ASSOCIATES>(
+            received_length, peer_, expand_op);
+    return retval;
+  }
+};  // end of colorIteration
+
+/**
+ * @brief Color enactor class.
+ * @tparam _Problem Problem type we process on
+ * @tparam ARRAY_FLAG Flags for util::Array1D used in the enactor
+ * @tparam cudaHostRegisterFlag Flags for util::Array1D used in the enactor
+ */
+template <typename _Problem, util::ArrayFlag ARRAY_FLAG = util::ARRAY_NONE,
+          unsigned int cudaHostRegisterFlag = cudaHostRegisterDefault>
+class Enactor : public EnactorBase<typename _Problem::GraphT,
+                                   typename _Problem::GraphT::VertexT,
+                                   // types used for the operators,
+                                   // e.g.: typename
+                                   // _Problem::LabelT,
+                                   typename _Problem::GraphT::ValueT,
+                                   // types used for inter GPU
+                                   // communication, e.g.: typename
+                                   // _Problem::ValueT,
+                                   ARRAY_FLAG, cudaHostRegisterFlag> {
+ public:
+  typedef _Problem Problem;
+  typedef typename Problem::SizeT SizeT;
+  typedef typename Problem::VertexT VertexT;
+  typedef typename Problem::GraphT GraphT;
+  typedef typename GraphT::VertexT LabelT;
+  typedef typename GraphT::ValueT ValueT;
+  typedef EnactorBase<GraphT, LabelT, ValueT, ARRAY_FLAG, cudaHostRegisterFlag>
+      BaseEnactor;
+  typedef Enactor<Problem, ARRAY_FLAG, cudaHostRegisterFlag> EnactorT;
+  typedef ColorIterationLoop<EnactorT> IterationT;
+
+  Problem *problem;
+  IterationT *iterations;
+
+  /**
+   * @brief color constructor
+   */
+  Enactor() : BaseEnactor("Color"), problem(NULL) {
+    this->max_num_vertex_associates = 0;
+    this->max_num_value__associates = 1;
+  }
+
+  /**
+   * @brief color destructor
+   */
+  virtual ~Enactor() { /*Release();*/
+  }
+
+  /*
+   * @brief Releasing allocated memory space
+   * @param target The location to release memory from
+   * \return cudaError_t error message(s), if any
+   */
+  cudaError_t Release(util::Location target = util::LOCATION_ALL) {
+    cudaError_t retval = cudaSuccess;
+    GUARD_CU(BaseEnactor::Release(target));
+    delete[] iterations;
+    iterations = NULL;
+    problem = NULL;
+    return retval;
+  }
+
+  /**
+   * @brief Initialize the problem.
+   * @param[in] problem The problem object.
+   * @param[in] target Target location of data
+   * \return cudaError_t error message(s), if any
+   */
+  cudaError_t Init(Problem &problem, util::Location target = util::DEVICE) {
+    cudaError_t retval = cudaSuccess;
+    this->problem = &problem;
+
+    // Lazy initialization
+    GUARD_CU(BaseEnactor::Init(problem, Enactor_None, 2, NULL, target, false));
+    for (int gpu = 0; gpu < this->num_gpus; gpu++) {
+      GUARD_CU(util::SetDevice(this->gpu_idx[gpu]));
+      auto &enactor_slice = this->enactor_slices[gpu * this->num_gpus + 0];
+      auto &graph = problem.sub_graphs[gpu];
+      GUARD_CU(enactor_slice.frontier.Allocate(graph.nodes, graph.edges,
+                                               this->queue_factors));
+    }
+
+    iterations = new IterationT[this->num_gpus];
+    for (int gpu = 0; gpu < this->num_gpus; gpu++) {
+      GUARD_CU(iterations[gpu].Init(this, gpu));
+    }
+
+    GUARD_CU(this->Init_Threads(
+        this, (CUT_THREADROUTINE) & (GunrockThread<EnactorT>)));
+    return retval;
+  }
+
+  /**
+   * @brief one run of color, to be called within GunrockThread
+   * @param thread_data Data for the CPU thread
+   * \return cudaError_t error message(s), if any
+   */
+  cudaError_t Run(ThreadSlice &thread_data) {
+    gunrock::app::Iteration_Loop<
+        //       per element in the inter-GPU sub-frontiers
+        0, 1, IterationT>(thread_data, iterations[thread_data.thread_num]);
+    return cudaSuccess;
+  }
+
+  /**
+   * @brief Reset enactor
+...
+   * @param[in] target Target location of data
+   * \return cudaError_t error message(s), if any
+   */
+  cudaError_t Reset(util::Location target = util::DEVICE) {
+    typedef typename GraphT::GpT GpT;
+    cudaError_t retval = cudaSuccess;
+    GUARD_CU(BaseEnactor::Reset(target));
+
+    SizeT num_nodes = this->problem->data_slices[0][0].sub_graph[0].nodes;
+
+    // In this case, we add a single `src` to the frontier
+    for (int gpu = 0; gpu < this->num_gpus; gpu++) {
+      if (this->num_gpus == 1) {
+        this->thread_slices[gpu].init_size = num_nodes;
+        for (int peer_ = 0; peer_ < this->num_gpus; peer_++) {
+          auto &frontier =
+              this->enactor_slices[gpu * this->num_gpus + peer_].frontier;
+          frontier.queue_length = (peer_ == 0) ? num_nodes : 0;
+          if (peer_ == 0) {
+            GUARD_CU(frontier.V_Q()->ForAll(
+            [] __host__ __device__ (VertexT * v, const SizeT &i) {
+              v[i] = i;
+            }, frontier.queue_length, target, 0));
+          }
+        }
+      } else {
+        util::PrintMsg("Multi-GPU Coloring not supported.", this->flag & Debug);
+      }
+    }
+
+    GUARD_CU(BaseEnactor::Sync());
+    return retval;
+  }
+
+  /**
+   * @brief Enacts a color computing on the specified graph.
+...
+   * \return cudaError_t error message(s), if any
+   */
+  cudaError_t Enact() {
+    cudaError_t retval = cudaSuccess;
+    GUARD_CU(this->Run_Threads(this));
+    util::PrintMsg("GPU Color Done.", this->flag & Debug);
+    return retval;
+  }
+};
+
+}  // namespace color
+}  // namespace app
+}  // namespace gunrock
+
+// Leave this at the end of the file
+// Local Variables:
+// mode:c++
+// c-file-style: "NVIDIA"
+// End:
