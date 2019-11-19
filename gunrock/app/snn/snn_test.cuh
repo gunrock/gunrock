9c46a6c415b1d255741f3288a2ed708f55c89f73
Agnieszka Lupinska <lupinska.agnieszka@gmail.com>
2019-11-19 02:51:29 -0800

    New implemenation of CPU_Reference and Validate_Results - classic version with labels

--- gunrock/app/snn/snn_test.cuh
+++ gunrock/app/snn/snn_test.cuh
@@ -14,18 +14,22 @@
 
 #pragma once
 
-// #define SNN_DEBUG 1
+//#define SNN_DEBUG 1
 
 #ifdef SNN_DEBUG
-#define debug(a...) fprintf(stderr, a)
+    #define debug(a...) fprintf(stderr, a)
 #else
-#define debug(a...)
+    #define debug(a...)
 #endif
 
-#include <set>
+#include <iostream>
+#include <unordered_set>
 #include <vector>
+#include <algorithm>
+#include <cassert>
 
 #include <gunrock/app/knn/knn_test.cuh>
+#include <gunrock/app/snn/snn_helpers.cuh>
 
 namespace gunrock {
 namespace app {
@@ -48,240 +52,196 @@ template <typename GraphT,
          typename SizeT = typename GraphT::SizeT>
 double CPU_Reference(
     const GraphT &graph,
-    SizeT k,        // number of nearest neighbor
-    SizeT eps,      // min number of SNN to increase snn-density
-    SizeT min_pts,  // mininum snn-density to be core point
-    VertexT point_x,// index of reference point
-    VertexT point_y,// index of reference point
-    SizeT *cluster, // cluster id
-    SizeT *core_point_counter, 
-    SizeT *cluster_counter, 
+    SizeT num_points,          // number of points
+    SizeT k,                   // number of nearest neighbor
+    SizeT eps,                 // min number of SNN to increase snn-density
+    SizeT min_pts,             // mininum snn-density to be core point
+    SizeT *knns,               // k nearest neighbor array
+    SizeT *cluster,            // cluster id
+    SizeT *core_point_counter, // counter of core points
+    SizeT *cluster_counter,    // counter of clusters
     bool quiet) {
   typedef typename GraphT::ValueT ValueT;
   typedef typename GraphT::CsrT CsrT;
 
-  struct Point {
-    VertexT x;
-    SizeT e_id;
-    ValueT dist;
+  //util::PrintMsg("#threads: " + std::to_string(omp_get_num_threads()));
 
-    Point() {}
-    Point(VertexT X, SizeT E_id, ValueT Dist) : x(X), e_id(E_id), dist(Dist) {}
-  };
-
-  struct comp {
-    inline bool operator()(const Point &p1, const Point &p2) {
-      return (p1.dist < p2.dist);
-    }
-  };
-
-  auto nodes = graph.nodes;
-  auto edges = graph.edges;
-
-  // util::PrintMsg("#threads: " + std::to_string(omp_get_num_threads()));
-
-#pragma omp parallel for
-  for (auto x = 0; x < nodes; ++x) {
-    cluster[x] = x;
-  }
-
-  SizeT *knns = (SizeT*)malloc(sizeof(SizeT) * graph.nodes * k);    // knns
-  std::set<SizeT> core_points;
-  std::vector<std::set<SizeT>> adj;
-  adj.resize(nodes);
-
-#pragma omp parallel for
-  for (SizeT x = 0; x < nodes; ++x) {
-    auto x_start = graph.CsrT::GetNeighborListOffset(x);
-    auto num = graph.CsrT::GetNeighborListLength(x);
-    auto x_end = x_start + num;
-    for (auto y = x_start; y < x_end; ++y) {
-      auto neighbor = graph.CsrT::GetEdgeDest(y);
-      adj[x].insert(neighbor);
-    }
-  }
-
-  Point *distance = (Point *)malloc(sizeof(Point) * edges);
   util::CpuTimer cpu_timer;
   cpu_timer.Start();
 
-// implement CPU reference implementation
 #pragma omp parallel for
-  for (SizeT x = 0; x < nodes; ++x) {
-    auto x_start = graph.CsrT::GetNeighborListOffset(x);
-    auto num = graph.CsrT::GetNeighborListLength(x);
-    auto x_end = x_start + num;
-    for (SizeT i = x_start; i < x_end; ++i) {
-      VertexT y = graph.column_indices[i];
-      ValueT d1 = (ValueT)(x - point_x);
-      ValueT d2 = (ValueT)(y - point_y);
-      ValueT dist = d1*d1 + d2*d2;
-      distance[i] = Point(x, i, dist);
-    }
+  for (auto x = 0; x < num_points; ++x) {
+    cluster[x] = util::PreDefinedValues<SizeT>::InvalidValue;
   }
 
-// Sort distances for each adjacency list
-#pragma omp parallel for
-  for (SizeT x = 0; x < nodes; ++x) {
-    auto x_start = graph.CsrT::GetNeighborListOffset(x);
-    auto num = graph.CsrT::GetNeighborListLength(x);
-    auto x_end = x_start + num;
-    std::sort(distance + x_start, distance + x_end, comp());
+  // For each point make a binary search tree of its k nearest neighbors 
+  std::vector<std::set<SizeT>> knns_set; knns_set.resize(num_points);
+  for (SizeT x = 0; x < num_points; ++x){
+    knns_set[x] = std::set<SizeT>(knns + (x*k), knns + ((x+1)*k));
   }
 
-  /*
-  //debug only
-  for (int tested_node = 0; tested_node < nodes; ++tested_node){
-  //    auto tested_node = 62734;
-      auto e_start = graph.CsrT::GetNeighborListOffset(tested_node);
-      auto num_neighbors = graph.CsrT::GetNeighborListLength(tested_node);
-      auto e_end = e_start + num_neighbors;
-      printf("sorted neighbors of thread %d\n", tested_node);
-      for (int x = e_start; x < e_end; ++x) {
-          printf("%d(%lld) ", graph.column_indices[distance[x].e_id], distance[x].dist);
+#ifdef SNN_DEBUG
+  for (auto x = 0; x < num_points; ++x){
+      debug("knns[%d]: ", x);
+      for (auto y : knns_set[x]){
+          debug("%d ", y);
       }
-      printf("\n");
-  }
-  */
-
-  // Debug
-#if SNN_DEBUG
-  for (SizeT x = 0; x < nodes; ++x) {
-    auto x_start = graph.CsrT::GetNeighborListOffset(x);
-    auto num = graph.CsrT::GetNeighborListLength(x);
-    auto x_end = x_start + num;
-    debug("sorted neighbors of %d\n", x);
-    for (int neighbor = x_start; neighbor < x_end; ++neighbor) {
-      debug("(%d, %d)(%d) ", distance[neighbor].x,
-            graph.column_indices[distance[neighbor].e_id],
-            distance[neighbor].dist);
-    }
-    debug("\n");
+      debug("\n");
   }
 #endif
 
-// Find k nearest neighbors
-#pragma omp parallel for
-  for (SizeT x = 0; x < nodes; ++x) {
-    auto x_start = graph.CsrT::GetNeighborListOffset(x);
-    auto num = graph.CsrT::GetNeighborListLength(x);
-    if (num < k) continue;
-    auto x_end = x_start + num;
-    debug("%d nearest neighbors\n", k);
-    int i = 0;
-    for (int neighbor = x_start; neighbor < x_end && i < k; ++neighbor, ++i) {
-      // k nearest points to (point_x, pointy)
-      knns[x * k + i] = graph.CsrT::GetEdgeDest(distance[neighbor].e_id);
-      debug("%d ", graph.CsrT::GetEdgeDest(distance[neighbor].e_id));
-    }
-    debug("\n");
+  // Table of sets of shared nn for each point
+  std::vector<std::set<SizeT>> snns; snns.resize(num_points);
+
+  // Set of core points
+  std::set<SizeT> core_points;
+  // Intersection of two knns sets             
+  std::vector<SizeT> common_knns; common_knns.resize(k);
+
+  debug("Looking for snns\n");
+  for (SizeT x = 0; x < num_points; ++x){
+      int snn_density = 0;
+      //for each q in kNN(x)
+      debug("Snn of %d\n", x);
+      for (SizeT i = 0; i < k; ++i){
+          SizeT q = knns[x * k + i];
+          debug("%d - knn[%d]\t", q, x);
+          //if x is in kNN(q)
+          /*if (snns[q].find(x) != snns[q].end()){
+              debug("\n");
+              continue;
+          }*/
+          if (knns_set[q].find(x) != knns_set[q].end()){
+              debug("%d - knn[%d]\t", x, q);
+              // checking size of set the common 
+              auto it = std::set_intersection(knns_set[x].begin(), 
+                      knns_set[x].end(), knns_set[q].begin(), 
+                      knns_set[q].end(), common_knns.begin());
+              common_knns.resize(it-common_knns.begin());
+              debug("they shared %d neighbors\t", common_knns.size());
+              if (common_knns.size() >= eps){
+                  snns[x].insert(q);
+                  snns[q].insert(x);
+                  debug("%d %d - snn\n", x, q);
+              }else{
+                  debug("\n");
+              }
+          }else{
+              debug("\n");
+          }
+      }
   }
 
-  // Find snn-density
-  for (SizeT x = 0; x < nodes; ++x) {
-    auto x_start = graph.CsrT::GetNeighborListOffset(x);
-    auto num = graph.CsrT::GetNeighborListLength(x);
-    if (num < k) continue;
-    auto x_end = x_start + num;
-    int snn_density = 0;
-#pragma omp parallel for reduction(+ : snn_density)
-    for (int i = 0; i < k; ++i) {
-      auto near_neighbor = knns[x * k + i];
-      int counter = 0;
-      auto y_start = graph.CsrT::GetNeighborListOffset(near_neighbor);
-      auto y_num = graph.CsrT::GetNeighborListLength(near_neighbor);
-      for (int z = y_start; z < y_start + y_num; ++z) {
-        auto y = graph.CsrT::GetEdgeDest(z);
-        if (adj[x].find(y) != adj[x].end()) {
-          ++counter;
-        }
+  // Find core points:
+  for (SizeT x = 0; x < num_points; ++x){
+      if (snns[x].size() > 0){
+          debug("density[%d] = %d\t", x, snns[x].size());
+          debug("snns: ");
+          for (auto ss :snns[x]){
+            debug("%d ", ss);
+          }
+          debug("\n");
       }
-      if (counter >= eps) snn_density += 1;
-      debug("density of %d is %d\n", x, snn_density);
-    }
-    if (snn_density >= min_pts) {
-      core_points.insert(x);
-    }
+      if (snns[x].size() >= min_pts){
+          core_points.insert(x);
+          debug("%d is core_point\n", x);
+      }else
+          debug("%d is not core_point\n", x);
   }
+  
+  // Set core points counter
+  *core_point_counter = core_points.size();
 
 #if SNN_DEBUG
-  debug("core points: ");
+  debug("core points (%d): ", core_points.size());
   for (auto cpb = core_points.begin(); cpb != core_points.end(); ++cpb) {
     debug("%d ", *cpb);
   }
   debug("\n");
 #endif
-  *core_point_counter = core_points.size();
 
-  for (SizeT x = 0; x < nodes; ++x) {
-    if (core_points.find(x) != core_points.end()) {
-      for (SizeT y = 0; y < nodes; ++y) {
-        if (x != y && core_points.find(y) != core_points.end()) {
-          int counter = 0;
-          auto y_start = graph.CsrT::GetNeighborListOffset(y);
-          auto y_num = graph.CsrT::GetNeighborListLength(y);
-          //#pragma omp parallel for reduction(+:counter)
-          for (int z = y_start; z < y_start + y_num; ++z) {
-            auto m = graph.CsrT::GetEdgeDest(z);
-            if (adj[x].find(m) != adj[x].end()) {
-              counter += 1;
-            }
-          }
-          if (counter >= eps) {
-            // Merge x and y core points
-            auto m = min(cluster[x], cluster[y]);
-            cluster[x] = m;
-            cluster[y] = m;
-          }
-        }
-      }
-    }
-  }
+  // Create empty clusters:
+  DisjointSet<SizeT> clusters(num_points);
 
-#pragma omp parallel for
-  for (int i = 0; i < nodes; ++i) {
-    // only non-core points
-    if (core_points.find(i) == core_points.end()) {
-      auto num_neighbors = graph.CsrT::GetNeighborListLength(i);
-      // only non-noise points
-      if (num_neighbors >= k) {  // was k
-        auto e_start = graph.CsrT::GetNeighborListOffset(i);
-        for (auto e = e_start; e < e_start + num_neighbors; ++e) {
-          auto m = graph.CsrT::GetEdgeDest(distance[e].e_id);
-          if (core_points.find(m) != core_points.end()) {
-            cluster[i] = cluster[m];
-            break;
-          }
-        }
+  // Find clusters. Union core points
+  for (auto c1 : core_points){
+    for (auto c2 : core_points){
+      if (snns[c1].find(c2) != snns[c1].end()){
+        clusters.Union(c1, c2);
+        cluster[c1] = cluster[c2] = clusters.Find(c1);
       }
     }
   }
 
 #if SNN_DEBUG
-  for (int i = 0; i < nodes; ++i) printf("cluster[%d] = %d\n", i, cluster[i]);
+  debug("clusters after union core points:\n");
+  for (int i = 0; i < num_points; ++i) 
+    debug("cluster[%d] = %d\n", i, cluster[i]);
 #endif
 
-  std::set<SizeT> cluster_set; 
-#if SNN_DEBUG
-  printf("cpu clusters: ");
-#endif
-  for (int i = 0; i < nodes; ++i){
-      
-      if (cluster_set.find(cluster[i]) == cluster_set.end()){
-          cluster_set.insert(cluster[i]);
-#if SNN_DEBUG
-          printf("%d ", cluster[i]);
-#endif
+  debug("assign non core points\n");
+  // Assign non core points
+  for (SizeT x = 0; x < num_points; ++x){
+    if (core_points.find(x) == core_points.end()){
+      // x is non core point
+      debug("%d - non core point\n", x);
+      SizeT nearest_core_point = util::PreDefinedValues<SizeT>::InvalidValue;
+      SizeT similarity_to_nearest_core_point = 0;
+      for (auto q : knns_set[x]){
+        debug("%d, knn of %d\n", q);
+        if (core_points.find(q) != core_points.end()){
+          debug("\t%d is core point\n", q);
+          // q is core point
+          auto it = std::set_intersection(knns_set[x].begin(), knns_set[x].end(),
+                  knns_set[q].begin(), knns_set[q].end(), common_knns.begin());
+          common_knns.resize(it-common_knns.begin());
+          if (!util::isValid(nearest_core_point) || 
+                  common_knns.size() > similarity_to_nearest_core_point){
+            similarity_to_nearest_core_point = common_knns.size();
+            nearest_core_point = q;
+          }
+        }
       }
+      if (util::isValid(nearest_core_point) && 
+              similarity_to_nearest_core_point >= eps){
+        // x is not a noise point
+        clusters.Union(x, nearest_core_point);
+        cluster[x] = cluster[nearest_core_point] = clusters.Find(nearest_core_point);
+      }
+    }
   }
+
 #if SNN_DEBUG
-  printf("\n");
+  debug("clusters after assigne non core points\n");
+  for (int i = 0; i < num_points; ++i) 
+    debug("cluster[%d] = %d\n", i, cluster[i]);
 #endif
+
+  std::unordered_set<SizeT> cluster_set; 
+  for (SizeT x = 0; x < num_points; ++x){
+    if (util::isValid(cluster[x])){
+      cluster_set.insert(clusters.Find(x)); // have to be clusters.Find(x) because array stores not updated cluster numbers
+    }
+  }
+
+
+#if SNN_DEBUG
+  debug("cpu clusters: ");
+  for (SizeT x = 0; x < num_points; ++x){
+    if (!util::isValid(cluster[x])){
+      debug("%x does not have cluster, it is noise point", x);
+    }else{
+      debug("cluster of %d is %d\n", x, clusters.Find(x));
+    }
+  }
+#endif
+
+ 
+  // Set cluster counter
   *cluster_counter = cluster_set.size();
 
   cpu_timer.Stop();
   float elapsed = cpu_timer.ElapsedMillis();
-  delete[] distance;
   return elapsed;
 }
 
@@ -304,8 +264,6 @@ typename GraphT::SizeT Validate_Results(util::Parameters &parameters,
                                         SizeT *ref_cluster,
                                         SizeT *ref_core_point_counter,
                                         SizeT *ref_cluster_counter,
-                                        //SizeT *h_knns,
-                                        //SizeT *ref_knns,
                                         bool verbose = true) {
   typedef typename GraphT::VertexT VertexT;
   typedef typename GraphT::CsrT CsrT;
@@ -313,56 +271,48 @@ typename GraphT::SizeT Validate_Results(util::Parameters &parameters,
   SizeT num_errors = 0;
   bool quiet = parameters.Get<bool>("quiet");
   bool quick = parameters.Get<bool>("quick");
-  bool snn = parameters.Get<bool>("snn");
+  SizeT num_points = parameters.Get<SizeT>("n");
   SizeT k = parameters.Get<int>("k");
+  SizeT eps = parameters.Get<int>("eps");
+  SizeT min_pts = parameters.Get<int>("min-pts");
 
   if (quick) return num_errors;
 
   printf("Validate results start, num_errors so far %d\n", num_errors);
 
+  printf("number of points: %d\n", num_points);
+  printf("cpu core point counter %d, gpu core point counter %d\n",
+              *ref_core_point_counter, *h_core_point_counter);
+
   if (*ref_core_point_counter != *h_core_point_counter){
       ++num_errors;
-      printf("cpu core point counter %d, gpu core point counter %d\n",
-              *ref_core_point_counter, *h_core_point_counter);
+      printf("error\n");
   }
 
+  printf("cpu cluster counter %d, gpu cluster counter %d\n",
+              *ref_cluster_counter, *h_cluster_counter);
+  
   if (*ref_cluster_counter != *h_cluster_counter){
       ++num_errors;
-      printf("cpu cluster counter %d, gpu cluster counter %d\n",
-              *ref_cluster_counter, *h_cluster_counter);
+      printf("error\n");
   }
-/*
-  for (SizeT v = 0; v < graph.nodes; ++v) {
-    auto v_start = graph.CsrT::GetNeighborListOffset(v);
-    auto num = graph.CsrT::GetNeighborListLength(v);
-    if (num < k) continue;
-    auto v_end = v_start + num;
-    int i = 0;
-    for (SizeT neighbor = v_start; neighbor < v_end && i < k; ++neighbor, ++i) {
-      if (h_knns[v * k + i] != ref_knns[v * k + i]) {
-         // if (v*k + i < 100)
-          //    printf("knns[%d] %d != %d\n", v*k+i, h_knns[v*k+i], ref_knns[v*k+i]);
-        ++num_errors;
+
+  std::vector<bool> unvisited_cluster_of; unvisited_cluster_of.resize(num_points, true);
+  for (SizeT i = 0; i < num_points; ++i) {
+    if (unvisited_cluster_of[i]){
+      unvisited_cluster_of[i] = false;
+      auto h_cluster_of_i = h_cluster[i];
+      auto ref_cluster_of_i = ref_cluster[i];
+      for (SizeT j = 0; j < num_points; ++j){
+        if (not unvisited_cluster_of[j]) continue;
+        if (ref_cluster[j] == ref_cluster_of_i){
+          if (h_cluster[j] != h_cluster_of_i){
+            printf("error: gpu %d and %d supposed to be in one cluster but are: %d != %d\n", i, j, h_cluster[i], h_cluster[j]);
+            ++num_errors;
+          }
+          unvisited_cluster_of[j] = false;
+        }
       }
-    }
-  }
-  printf("Validate results stop, num_errors so far %d\n", num_errors);
-
-  if (num_errors > 0) {
-    util::PrintMsg(std::to_string(num_errors) + " errors occurred in KNN.",
-                   !quiet);
-  } else {
-    util::PrintMsg("PASSED KNN", !quiet);
-  }
-
-  SizeT knn_errors = num_errors;
-  */
-  num_errors = 0;
-
-  for (SizeT i = 0; i < graph.nodes; ++i) {
-    if (snn && (h_cluster[i] != ref_cluster[i])) {
-      printf("[%d] %d != %d\n", i, h_cluster[i], ref_cluster[i]);
-      ++num_errors;
     }
   }
 
