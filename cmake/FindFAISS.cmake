97bd8a0268f3ab3571f92d6215bcfedf581401cf
agalup <lupinska.agnieszka@gmail.com>
2020-03-08 19:18:20 -0700

    Fix compilation with FAISS lib

--- cmake/FindFAISS.cmake
+++ cmake/FindFAISS.cmake
@@ -3,10 +3,12 @@
 # ------------------------------------------------------------------------
 FIND_LIBRARY( FAISS_LIBRARY
               NAMES faiss
-              PATHS ${CMAKE_SOURCE_DIR}/externals/faiss /lib /usr/lib /usr/lib64 /usr/local/lib)
+              PATHS ../faiss /lib /usr/lib /usr/lib64 /usr/local/lib)
+
+FIND_PATH(  FAISS_INCLUDE_DIR
+            NAMES faiss/gpu/GpuIndexFlat.h
+            PATHS /usr/include ../)
 
 IF (FAISS_LIBRARY)
-    SET(FAISS_FOUND TRUE)
-ELSE ()
-    SET(FAISS_FOUND FALSE)
+    ADD_DEFINITIONS( -DFAISS_FOUND=1)
 ENDIF (FAISS_LIBRARY)
