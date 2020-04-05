34a34eb13152f966a67682d3a22d78f5644e5e52
maawad <mawad@ucdavis.edu>
2020-04-05 07:03:43 -0700

    Cleanup example file

--- examples/dynamic_graph/test_dynamic_graph.cu
+++ examples/dynamic_graph/test_dynamic_graph.cu
@@ -55,11 +55,6 @@ struct main_struct {
                                     graph::HAS_COO |
                                     graph::HAS_DYN>;
 
-    using UnweightedGraphT = app::TestGraph<VertexT, SizeT, ValueT,
-                                    graph::HAS_CSR | 
-                                    graph::HAS_COO |
-                                    graph::HAS_DYN>;
-
     using CSRGraphT = app::TestGraph<VertexT, SizeT, ValueT,
                                     graph::HAS_EDGE_VALUES | 
                                     graph::HAS_CSR>;
@@ -68,51 +63,12 @@ struct main_struct {
     util::CpuTimer cpu_timer;
     
     WeightedGraphT weighted_graph;
-    //UnweightedGraphT unweighted_graph;
 
     cpu_timer.Start();
     GUARD_CU(graphio::LoadGraph(parameters, weighted_graph));
-    //GUARD_CU(graphio::LoadGraph(parameters, unweighted_graph));
     cpu_timer.Stop();
     parameters.Set("load-time", cpu_timer.ElapsedMillis());
 
-
-    CSRGraphT csr_graph;
-
-    SizeT nodes = weighted_graph.csr().nodes;
-    SizeT edges = weighted_graph.csr().edges;
-
-    csr_graph.csr().Allocate(nodes, edges, util::DEVICE | util::HOST);
-
-    weighted_graph.dyn().ToCsr(csr_graph.csr());
-
-    csr_graph.csr().Move(util::DEVICE, util::HOST);
-
-    auto ref_graph = weighted_graph.csr();
-    auto res_graph = csr_graph.csr();
-
-    ref_graph.Sort();
-    res_graph.Sort();
-
-
-    for(SizeT node = 0; node < nodes; node++){
-    	SizeT ref_start = ref_graph.row_offsets[node];
-    	SizeT ref_end   = ref_graph.row_offsets[node + 1];
-
-    	SizeT res_start = res_graph.row_offsets[node];
-    	SizeT res_end   = res_graph.row_offsets[node + 1];
-
-    	printf("%i:[ %i - %i] =? ",node, ref_start, ref_end);
-    	printf("[ %i - %i] \n", res_start, res_end);
-
-    	//
-    	for(VertexT eid = ref_start; eid <= ref_end; eid++){
-    		VertexT e_ref = ref_graph.column_indices[eid];
-    		VertexT e_res = ref_graph.column_indices[eid];
-        	printf("E: %i:[ %i =? %i] \n",eid, e_ref, e_res);
-
-    	}
-    }
     return retval;
   }
 };
@@ -132,12 +88,6 @@ int main(int argc, char **argv) {
                            app::SIZET_U32B | 
                            app::VALUET_S32B | app::DIRECTED | app::UNDIRECTED>(
       parameters, main_struct());
-
-
-  //return app::Switch_Types<app::VERTEXT_U32B | app::VERTEXT_U64B |
-  //                         app::SIZET_U32B | app::SIZET_U64B |
-  //                         app::VALUET_S32B | app::DIRECTED | app::UNDIRECTED>(
-  //    parameters, main_struct());
 }
 
 // Leave this at the end of the file
