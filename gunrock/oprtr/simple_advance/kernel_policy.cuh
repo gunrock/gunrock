d004f65367d1f48067d6921f41a85ceabdb20c5c
maawad <mawad@ucdavis.edu>
2020-04-23 14:42:14 -0700

    Adding SimpleAdvance

---
+++ gunrock/oprtr/simple_advance/kernel_policy.cuh
@@ -0,0 +1,27 @@
+// ----------------------------------------------------------------
+// Gunrock -- Fast and Efficient GPU Graph Library
+// ----------------------------------------------------------------
+// This source code is distributed under the terms of LICENSE.TXT
+// in the root directory of this source distribution.
+// ----------------------------------------------------------------
+
+/**
+ * @file
+ * kernel_policy.cuh
+ *
+ * @brief Kernel configuration policy for Simple Advance
+ */
+
+#pragma once
+
+namespace gunrock {
+namespace oprtr {
+namespace SimpleAdvance {}  // namespace SimpleAdvance
+}  // namespace oprtr
+}  // namespace gunrock
+
+// Leave this at the end of the file
+// Local Variables:
+// mode:c++
+// c-file-style: "NVIDIA"
+// End:
