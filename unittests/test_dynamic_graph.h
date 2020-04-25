5e17165afa3ca39512721a0ff1d9699dc0af83e4
maawad <mawad@ucdavis.edu>
2020-04-25 07:10:11 -0700

    Adding directed insertion test

--- unittests/test_dynamic_graph.h
+++ unittests/test_dynamic_graph.h
@@ -103,7 +103,7 @@ TEST(dynamicGraph, insertUndirectedWeighted) {
 
   // generate a random batch of edges to insert
   using PairT = uint2;
-  SizeT batch_size = 100;
+  SizeT batch_size = 10000;
   util::Array1D<SizeT, PairT> edges_batch;
   util::Array1D<SizeT, ValueT> edges_batch_values;
   edges_batch.Allocate(batch_size, util::HOST | util::DEVICE);
@@ -133,6 +133,81 @@ TEST(dynamicGraph, insertUndirectedWeighted) {
       auto res_1 = ref_dyn[edges_batch[e].y].insert(edges_batch[e].x);
       if (res_0.second) new_edges_count++;
       if (res_1.second) new_edges_count++;
+    }
+  }
+
+  // New static graph
+  WeightedCSRGraphT ref_graph_updated;
+  auto& ref_csr_graph_updated = ref_graph_updated.csr();
+  ref_csr_graph_updated.Allocate(nodes, new_edges_count, util::HOST);
+  ref_csr_graph_updated.directed = directed;
+  RandomWeightedGraphToCsr(ref_dyn, ref_graph_updated, 1);
+
+  // convert dynamic graph back to CSR
+  auto& result_csr_graph = result_graph.csr();
+  result_csr_graph.Allocate(nodes, new_edges_count, util::HOST | util::DEVICE);
+  result_dynamic_graph.ToCsr(result_csr_graph);
+  result_csr_graph.Move(util::DEVICE, util::HOST);
+
+  CompareWeightedCSRs(ref_graph_updated, result_graph);
+
+  result_dynamic_graph.Release();
+  ref_graph.Release(util::HOST | util::DEVICE);
+  ref_graph_updated.Release(util::HOST | util::DEVICE);
+}
+
+TEST(dynamicGraph, insertDirectedWeighted) {
+  // graph parameters
+  SizeT nodes = 100;
+  SizeT edges_per_node = 5;
+  SizeT edges = nodes * edges_per_node * 2;
+  bool directed = true;
+
+  // reference CSR graph
+  WeightedCSRGraphT ref_graph;
+  auto& ref_csr_graph = ref_graph.csr();
+  ref_csr_graph.Allocate(nodes, edges, util::HOST);
+  ref_csr_graph.directed = directed;
+
+  DynamicHostGraphT ref_dyn(nodes);
+  GenerateRandomWeightedGraph(ref_dyn, nodes, edges, edges_per_node, !directed);
+  RandomWeightedGraphToCsr(ref_dyn, ref_graph, 1);
+
+  // convert CSR to dynamic graph
+  WeightedDYNGraphT result_graph;
+  auto& result_dynamic_graph = result_graph.dyn();
+  result_dynamic_graph.FromCsr(ref_csr_graph);
+
+  // generate a random batch of edges to insert
+  using PairT = uint2;
+  SizeT batch_size = 10000;
+  util::Array1D<SizeT, PairT> edges_batch;
+  util::Array1D<SizeT, ValueT> edges_batch_values;
+  edges_batch.Allocate(batch_size, util::HOST | util::DEVICE);
+  edges_batch_values.Allocate(batch_size, util::HOST | util::DEVICE);
+  std::mt19937 rng(0);
+
+  for (auto e = 0; e < batch_size; e++) {
+    VertexT edge_src = rng() % nodes;
+    VertexT edge_dst = rng() % nodes;
+    edges_batch[e] = make_uint2(edge_src, edge_dst);
+    edges_batch_values[e] = 1;
+  }
+
+  // move to GPU
+  edges_batch.Move(util::HOST, util::DEVICE);
+  edges_batch_values.Move(util::HOST, util::DEVICE);
+
+  // insert the edges batch
+  result_dynamic_graph.InsertEdgesBatch(edges_batch, edges_batch_values,
+                                        batch_size, util::DEVICE);
+
+  // Apply batch to host graph & generate values as well
+  SizeT new_edges_count = edges;
+  for (auto e = 0; e < batch_size; e++) {
+    if (edges_batch[e].x != edges_batch[e].y) {
+      auto res_0 = ref_dyn[edges_batch[e].x].insert(edges_batch[e].y);
+      if (res_0.second) new_edges_count++;
     }
   }
 
