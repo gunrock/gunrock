dc18d2982b4c26c52526640745f074e0956e74d5
agalup <lupinska.agnieszka@gmail.com>
2020-05-02 00:03:23 -0700

    Update run scripts

--- examples/snn/run.sh
+++ examples/snn/run.sh
@@ -17,38 +17,40 @@ do
 done
 
 EXECUTION=$exe_file
-DEVICE=1
+DEVICE=0
+
+DATASETS_FOLDER="$HOME/clustering_dataset" #/data/clustering_dataset/
 
 mkdir JSON
 
 # addresses
-PATH[0]="/data/clustering_dataset/KDDCUP04Bio"
-PATH[1]="/data/clustering_dataset/3DRN"
-PATH[2]="/home/agnes/clustering_dataset/Font/dim11"
-PATH[3]="/home/agnes/clustering_dataset/Halo/dim9"
-PATH[4]="/home/agnes/clustering_dataset/Halo/dim9"
-PATH[5]="/home/agnes/clustering_dataset/Font/dim11"
-PATH[6]="/home/agnes/clustering_dataset/Berton/dim3"
-PATH[7]="/home/agnes/clustering_dataset/Berton/dim3"
-PATH[8]="/home/agnes/clustering_dataset/Bower/dim3"
-PATH[9]="/home/agnes/clustering_dataset/FOF"
-PATH[10]="/home/agnes/clustering_dataset/FOF"
-PATH[11]="/home/agnes/clustering_dataset/FOF"
-PATH[12]="/home/agnes/clustering_dataset/FOF"
-PATH[13]="/home/agnes/clustering_dataset/FOF"
-PATH[14]="/home/agnes/clustering_dataset/FOF"
-PATH[15]="/home/agnes/clustering_dataset/FOF"
+PATH[0]="${DATASETS_FOLDER}/KDDCUP04Bio"
+PATH[1]="${DATASETS_FOLDER}/3DRN"
+PATH[2]="${DATASETS_FOLDER}/Font"
+PATH[3]="${DATASETS_FOLDER}/Halo"
+PATH[4]="${DATASETS_FOLDER}/Halo"
+PATH[5]="${DATASETS_FOLDER}/Font"
+PATH[6]="${DATASETS_FOLDER}/Berton"
+PATH[7]="${DATASETS_FOLDER}/Berton"
+PATH[8]="${DATASETS_FOLDER}/Bower"
+PATH[9]="${DATASETS_FOLDER}/FOF"
+PATH[10]="${DATASETS_FOLDER}/FOF"
+PATH[11]="${DATASETS_FOLDER}/FOF"
+PATH[12]="${DATASETS_FOLDER}/FOF"
+PATH[13]="${DATASETS_FOLDER}/FOF"
+PATH[14]="${DATASETS_FOLDER}/FOF"
+PATH[15]="${DATASETS_FOLDER}/FOF"
 
 # datasets
 NAME[0]="KDDCUP04Bio.txt"           && K[0]="30"     && EPS[0]="5"   && MINPTS[0]="5"
-NAME[1]="3D_spatial_network.txt"    && K[1]="30"     && EPS[1]="12"  && MINPTS[1]="15"
+NAME[1]="3DRN"                      && K[1]="30"     && EPS[1]="12"  && MINPTS[1]="15"
 NAME[2]="DGF572K11d" 		        && K[2]="30"     && EPS[2]="12"  && MINPTS[2]="15"
 NAME[3]="MPAH1M9d" 	                && K[3]="40"     && EPS[3]="15"  && MINPTS[3]="20"
 NAME[4]="MPAH1.5M9d" 	            && K[4]="40"     && EPS[4]="15"  && MINPTS[4]="20"
 NAME[5]="DGF2M11d" 		            && K[5]="50"     && EPS[5]="20"  && MINPTS[5]="25"
-NAME[6]="MPAGB23K3d_0" 		        && K[6]="30"     && EPS[6]="12"  && MINPTS[6]="15"
-NAME[7]="MPAGB8M3d_0" 		        && K[7]="50"     && EPS[7]="20"  && MINPTS[7]="25"
-NAME[8]="DGB8M3d_0" 		        && K[8]="50"     && EPS[8]="20"  && MINPTS[8]="25"
+NAME[6]="MPAGB23K3d" 		        && K[6]="30"     && EPS[6]="12"  && MINPTS[6]="15"
+NAME[7]="MPAGB8M3d" 		        && K[7]="50"     && EPS[7]="20"  && MINPTS[7]="25"
+NAME[8]="DGB8M3d"   		        && K[8]="50"     && EPS[8]="20"  && MINPTS[8]="25"
 NAME[9]="FOF11M3d" 		            && K[9]="30"     && EPS[9]="12"  && MINPTS[9]="15"
 NAME[10]="FOF20M3d" 		        && K[10]="30"    && EPS[10]="12" && MINPTS[10]="15"
 NAME[11]="FOF22M3d" 		        && K[11]="30"    && EPS[11]="12" && MINPTS[11]="15"
