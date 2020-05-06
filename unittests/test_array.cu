213a2f97610b328cbae080ca4aa1014dd23b0081
Muhammad Osama <osama94@gmail.com>
2020-05-06 07:44:23 -0700

    :wrench: unittests: dummy template

--- unittests/test_array.cu
+++ unittests/test_array.cu
@@ -1,5 +1,7 @@
+// XXX: dummy template for unit testing
+
 #include <gunrock/data_structs/array.cuh>
-typedef gunrock::error_t error_t;
+typedef cudaError_t error_t;
 
 error_t
 test_array()
@@ -8,7 +10,7 @@ test_array()
 
   error_t retval = cudaSuccess;
   size_t N = 128;
-  datastruct::dense::array<int, N> a;
+  gunrock::datastruct::dense::array<int, N> a;
 
   return retval;
 }
