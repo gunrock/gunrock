5e858dfa9a72d35060afc5fd329159c313205cd2
Jonathan Wapman <jdwapman@ucdavis.edu>
2020-06-29 16:09:31 -0700

    Python script progress

--- examples/hits/hits.py
+++ examples/hits/hits.py
@@ -3,29 +3,36 @@ import numpy as np
 import sys
 from scipy.io import mmread
 from scipy.sparse import coo_matrix
+np.set_printoptions(threshold=sys.maxsize)
 
 if len(sys.argv) != 2:
     print("Usage: python3 ./hits.py <file.mtx>")
     exit()
 
 graph_mtx = mmread(sys.argv[1])
-graph_nx = nx.from_scipy_sparse_matrix(graph_mtx)
 
+# print(graph_mtx.nnz)
+
+graph_nx = nx.from_scipy_sparse_matrix(graph_mtx)
+# print(nx.is_directed(graph_nx))
 max_iter = 1
 tol = 100
 hubs_nx, auths_nx = nx.hits(graph_nx, max_iter, tol, normalized=True)
 
 # Numpy implementation
-curr_hrank = np.zeros((100, 1))
-curr_arank = np.zeros((100, 1))
+curr_hrank = np.zeros((graph_mtx.shape[0], 1))
+curr_arank = np.zeros((graph_mtx.shape[0], 1))
 
 graph_coo = coo_matrix(graph_mtx)
 curr_hrank += 1/graph_coo.shape[0]
 curr_arank += 1/graph_coo.shape[0]
 
+print(curr_hrank)
+print(curr_arank)
+
 for _ in range(0, max_iter):
-    next_hrank = np.zeros((100, 1))
-    next_arank = np.zeros((100, 1))
+    next_hrank = np.zeros((graph_coo.shape[0], 1))
+    next_arank = np.zeros((graph_coo.shape[0], 1))
 
     for edge in range(0, graph_coo.nnz):
         src = int(graph_coo.row[edge])
@@ -34,6 +41,11 @@ for _ in range(0, max_iter):
         next_arank[dest] += curr_hrank[src]
 
     # Normalize
+
+    print(next_hrank)
+    print(next_arank)
+    next_hrank = next_hrank / np.max(next_hrank)
+    next_arank = next_arank / np.max(next_arank)
     next_hrank = next_hrank / np.linalg.norm(next_hrank, ord=1)
     next_arank = next_arank / np.linalg.norm(next_arank, ord=1)
 
@@ -52,10 +64,9 @@ for i in range(0, graph_coo.shape[0]):
     hubs_np[i] = curr_hrank[i]
     auths_np[i] = curr_arank[i]
 
-print("Hubs: ")
-for key, val in sorted(hubs_nx.items(), key=lambda x: x[1], reverse=True):
-    print(key, val, hubs_np[key])
-print("Authorities: ")
-for key, val in sorted(auths_nx.items(), key=lambda x: x[1], reverse=True):
-    print(key, val, auths_np[key])
-
+# print("Hubs: ")
+# for key, val in sorted(hubs_nx.items(), key=lambda x: x[1], reverse=True):
+#     print(key, val, hubs_np[key])
+# print("Authorities: ")
+# for key, val in sorted(auths_nx.items(), key=lambda x: x[1], reverse=True):
+#     print(key, val, auths_np[key])
