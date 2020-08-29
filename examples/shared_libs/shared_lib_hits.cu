a8a126228ae5ddc8a71c0635fa126905700e5dab
Jonathan Wapman <jdwapman@ucdavis.edu>
2020-08-29 02:13:31 -0700

    Remove old commented-out test code

--- examples/shared_libs/shared_lib_hits.cu
+++ examples/shared_libs/shared_lib_hits.cu
@@ -61,31 +61,3 @@ int main(int argc, char** argv) {
                       printf("Node_ID: [%d], Hub Score: [%f], Auth Score: [%f]\n", i, d_hub_ranks[i], d_auth_ranks[i]);
                    });
 }
-
-
-// int main(int argc, char *argv[]) {
-//   ////////////////////////////////////////////////////////////////////////////
-
-//   int num_nodes = 7, num_edges = 26;
-//   int row_offsets[8] = {0, 3, 6, 11, 15, 19, 23, 26};
-//   int col_indices[26] = {1, 2, 3, 0, 2, 4, 0, 1, 3, 4, 5, 0, 2,
-//                          5, 6, 1, 2, 5, 6, 2, 3, 4, 6, 3, 4, 5};
-
-//   // HITS
-//   float *hub_ranks = (float *)malloc(sizeof(float) * num_nodes);
-//   float *auth_ranks = (float *)malloc(sizeof(float) * num_nodes);
-//   int max_iter = 1000;
-//   float tol = 1e-6;
-//   int hits_norm = 2;
-//   unsigned int device = 0x01; //CPU
-
-//   double elapsed_hits = hits(num_nodes, num_edges, row_offsets, col_indices, max_iter, tol, hits_norm, hub_ranks, auth_ranks, device);
-
-//   for (int node = 0; node < num_nodes; ++node)
-//     printf("Node_ID: [%d], Hub Score: [%f], Auth Score: [%f]\n", node, hub_ranks[node], auth_ranks[node]);
-
-//   if(hub_ranks) free(hub_ranks);
-//   if(auth_ranks) free(auth_ranks);
-
-//   return 0;
-// }
