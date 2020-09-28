fef92dfd3f881fb0e0001e750cf2409c57e6faac
neoblizz <osama94@gmail.com>
2020-09-28 16:17:08 -0700

    unsure of how csr matrix should be structured, for now, I am opting for the simplest implementation

--- gunrock/formats/csr.hxx
+++ gunrock/formats/csr.hxx
@@ -20,63 +20,9 @@ struct csr_t {
     index_t num_columns;
     index_t num_nonzeros;
 
-    memory::memory_space_t location;
-
     std::shared_ptr<offset_t> row_offsets; // Ap
     std::shared_ptr<index_t> column_indices; // Aj
     std::shared_ptr<value_t> nonzero_values; // Ax
-
-    // Default constructor
-    csr_t() : 
-        num_rows(0),
-        num_columns(0),
-        num_nonzeros(0),
-        row_offsets(std::make_shared<offset_t>()),
-        column_indices(std::make_shared<index_t>()),
-        nonzero_values(std::make_shared<value_t>()) {}
-
-    // Build using r, c, nnz
-    csr_t(index_t r, index_t c, index_t nnz,
-          memory::memory_space_t loc = memory::memory_space_t::device) :
-        num_rows(r),
-        num_columns(c),
-        num_nonzeros(nnz),
-        location(loc),
-        row_offsets(
-            memory::allocate<offset_t>((r+1) * sizeof(offset_t), location), 
-            [&](offset_t* ptr) { memory::free(ptr, location); }),
-        column_indices(
-            memory::allocate<index_t>((nnz) * sizeof(index_t), location), 
-            [&](index_t* ptr) { memory::free(ptr, location); }),
-        nonzero_values(
-            memory::allocate<value_t>((nnz) * sizeof(value_t), location), 
-            [&](value_t* ptr) { memory::free(ptr, location); }) {}
-
-    // Wrapper over existing pointers
-    // allocated in the known memory space
-    csr_t(index_t r, index_t c, index_t nnz,
-          offset_t* Ap, index_t* Aj, value_t* Ax,
-          memory::memory_space_t loc) :
-        num_rows(r),
-        num_columns(c),
-        num_nonzeros(nnz),
-        location(loc),
-        row_offsets(Ap, [&](offset_t* ptr) { memory::free(ptr, location); }),
-        column_indices(Aj, [&](index_t* ptr) { memory::free(ptr, location); }),
-        nonzero_values(Ax, [&](value_t* ptr) { memory::free(ptr, location); }) {}
-
-    // Wrapper over existing pointers
-    // allocated in the unknown memory space
-    csr_t(index_t r, index_t c, index_t nnz,
-          offset_t* Ap, index_t* Aj, value_t* Ax) :
-        num_rows(r),
-        num_columns(c),
-        num_nonzeros(nnz),
-        location(memory::memory_space_t::unknown),
-        row_offsets(Ap),
-        column_indices(Aj),
-        nonzero_values(Ax) {}
-
 }; // struct csr_t
 
 } // namespace format
--- unittests/graph/test_graph.cu
+++ unittests/graph/test_graph.cu
@@ -18,26 +18,10 @@ void test_graph()
   using g_csc_t   = graph::graph_csc_t<vertex_t, edge_t, weight_t>;
   using g_coo_t   = graph::graph_coo_t<vertex_t, edge_t, weight_t>;
 
-  constexpr bool HAS_CSR = false;
-  constexpr bool HAS_CSC = false;
-  constexpr bool HAS_COO = false;
-
   error::error_t status = cudaSuccess;
 
-  // graph::graph_t<HAS_COO, HAS_CSR, HAS_CSC,
-  //         vertex_t, edge_t, weight_t> graph;
-
   // XXX: (hide behind load) CSR array with space allocated (4x4x4)
-  std::size_t r = 4, c = 4, nnz = 4;
   memory::memory_space_t location = memory::memory_space_t::host;
-
-  // CSR Matrix Representation
-  // V            = [ 5 8 3 6 ]
-  // COL_INDEX    = [ 0 1 2 1 ]
-  // ROW_OFFSETS  = [ 0 0 2 3 4 ]
-  edge_t *Ap = memory::allocate<edge_t>((r+1)*sizeof(edge_t), location);
-  vertex_t *Aj = memory::allocate<vertex_t>((nnz)*sizeof(vertex_t), location);
-  weight_t *Ax = memory::allocate<weight_t>((nnz)*sizeof(weight_t), location);
 
   // Logical Matrix Representation
   // r/c  0 1 2 3
@@ -53,12 +37,40 @@ void test_graph()
   // (2, 2) [3]
   // (3, 1) [6]
 
+  // CSR Matrix Representation
+  // V            = [ 5 8 3 6 ]
+  // COL_INDEX    = [ 0 1 2 1 ]
+  // ROW_OFFSETS  = [ 0 0 2 3 4 ]
+
+  using csr_t = format::csr_t<edge_t, vertex_t, weight_t>;
+
   // XXX: ugly way to initialize these, but it works.
+  csr_t csr;
+
+  csr.num_rows = csr.num_columns = csr.num_nonzeros = 4;
+
+  csr.row_offsets = std::shared_ptr<edge_t>(
+                      memory::allocate<edge_t>(
+                        (csr.num_rows+1)*sizeof(edge_t), location),
+                      [&](edge_t* ptr){ memory::free(ptr, location); });
+
+  csr.column_indices = std::shared_ptr<vertex_t>(
+                        memory::allocate<vertex_t>(
+                          (csr.num_nonzeros)*sizeof(vertex_t), location),
+                        [&](vertex_t* ptr){ memory::free(ptr, location); });
+
+  csr.nonzero_values = std::shared_ptr<weight_t>(
+                        memory::allocate<weight_t>(
+                          (csr.num_nonzeros)*sizeof(weight_t), location),
+                        [&](weight_t* ptr){ memory::free(ptr, location); });
+  
+  auto Ap = csr.row_offsets.get();
+  auto Aj = csr.column_indices.get();
+  auto Ax = csr.nonzero_values.get();
+
   Ap[0] = 0; Ap[1] = 0; Ap[2] = 2; Ap[3] = 3; Ap[4] = 4;
   Aj[0] = 0; Aj[1] = 1; Aj[2] = 2; Aj[3] = 3;
   Ax[0] = 5; Ax[1] = 8; Ax[2] = 3; Ax[3] = 6;
-
-  format::csr_t<edge_t, vertex_t, weight_t> csr(r, c, nnz, Ap, Aj, Ax, location);
 
   graph::graph_t<vertex_t, edge_t, weight_t, g_csr_t, g_csc_t /* , g_coo_t*/> graph_slice;
 
