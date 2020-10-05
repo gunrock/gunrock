d653679c647432dbfae8ffb627bb81105323cdac
Charles Rozhon <carozhon@ucdavis.edu>
2020-10-04 22:28:21 -0700

    Fix for specifying quick and validation #773 (#775)
    * Fix for specifying quick and validation
    
    * Add test for combination of quick and validation

--- gunrock/app/sssp/sssp_app.cu
+++ gunrock/app/sssp/sssp_app.cu
@@ -71,6 +71,7 @@ cudaError_t RunTests(util::Parameters &parameters, GraphT &graph,
 
   // parse configurations from parameters
   bool quiet_mode = parameters.Get<bool>("quiet");
+  bool quick_mode = parameters.Get<bool>("quick");
   bool mark_pred = parameters.Get<bool>("mark-pred");
   int num_runs = parameters.Get<int>("num-runs");
   std::string validation = parameters.Get<std::string>("validation");
@@ -109,7 +110,7 @@ cudaError_t RunTests(util::Parameters &parameters, GraphT &graph,
             " ms, src = " + std::to_string(src) + ", #iterations = " +
             std::to_string(enactor.enactor_slices[0].enactor_stats.iteration),
         !quiet_mode);
-    if (validation == "each") {
+    if (!quick_mode && validation == "each") {
       GUARD_CU(problem.Extract(h_distances, h_preds));
       SizeT num_errors = app::sssp::Validate_Results(
           parameters, graph, src, h_distances, h_preds,
@@ -121,7 +122,7 @@ cudaError_t RunTests(util::Parameters &parameters, GraphT &graph,
   cpu_timer.Start();
   // Copy out results
   GUARD_CU(problem.Extract(h_distances, h_preds));
-  if (validation == "last") {
+  if (!quick_mode && validation == "last") {
     SizeT num_errors = app::sssp::Validate_Results(
         parameters, graph, src, h_distances, h_preds,
         ref_distances == NULL ? NULL
