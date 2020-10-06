cf95407fc8ed7b66fc0254f996bfc62daa408755
neoblizz <osama94@gmail.com>
2020-10-06 11:43:03 -0700

    Contexts unit test

---
+++ unittests/contexts/test_contexts.cu
@@ -0,0 +1,28 @@
+#include <cstdlib>  // EXIT_SUCCESS
+
+#include <gunrock/cuda/context.hxx>
+#include <gunrock/error.hxx>  // error checking
+
+void test_context() {
+  using namespace gunrock;
+
+  // List of devices we care about
+  std::vector<cuda::device_id_t> devices;
+
+  // Initialize
+  devices.push_back(0);
+  // devices.push_back(1);
+
+  // Create contexts for all the devices
+  cuda::multi_context_t multi_context(devices);
+
+  auto context_device_0 = multi_context.get_context(0);
+  // auto context_device_1 = multi_context.get_context(0);
+
+  context_device_0.print_properties();
+}
+
+int main(int argc, char** argv) {
+  test_context();
+  return EXIT_SUCCESS;
+}
