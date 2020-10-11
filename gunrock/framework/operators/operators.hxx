3547a676d8d2c23ca30a1e5b2e347eb48ad62387
neoblizz <osama94@gmail.com>
2020-10-10 19:51:54 -0700

    Load-balancing enums

--- gunrock/framework/operators/operators.hxx
+++ gunrock/framework/operators/operators.hxx
@@ -12,7 +12,21 @@
 #pragma once
 
 namespace gunrock {
-namespace operators {}  // namespace operators
+namespace operators {
+
+/**
+ * @brief Load balancing type options, not all operators support load-balancing
+ * or have need to balance work.
+ *
+ */
+enum load_balance_type {
+  merge_based,    // Merrill & Garland (SpMV)
+  bucketing,      // Davidson et al. (SSSP)
+  work_stealing,  // <find cite>
+  none            // No ;oad-balancing applied
+};
+
+}  // namespace operators
 }  // namespace gunrock
 
 #include <gunrock/framework/operators/advance/advance.hxx>
