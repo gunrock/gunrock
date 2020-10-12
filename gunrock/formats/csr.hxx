a469bb782f9917b05417a8a32c4e7fc51a44a574
neoblizz <osama94@gmail.com>
2020-10-12 14:27:28 -0700

    Simple example of how we can write convertors for different sparse formats

--- gunrock/formats/csr.hxx
+++ gunrock/formats/csr.hxx
@@ -2,6 +2,7 @@
 
 #include <gunrock/memory.hxx>
 #include <gunrock/container/vector.hxx>
+#include <gunrock/formats/formats.hxx>
 
 namespace gunrock {
 namespace format {
@@ -22,7 +23,7 @@ template <memory_space_t space,
 struct csr_t {
   index_t number_of_rows;
   index_t number_of_columns;
-  index_t number_of_nonzeros;
+  offset_t number_of_nonzeros;
 
   typename vector<offset_t, space>::type row_offsets;    // Ap
   typename vector<index_t, space>::type column_indices;  // Aj
@@ -36,7 +37,7 @@ struct csr_t {
         column_indices(),
         nonzero_values() {}
 
-  csr_t(index_t r, index_t c, index_t nnz)
+  csr_t(index_t r, index_t c, offset_t nnz)
       : number_of_rows(r),
         number_of_columns(c),
         number_of_nonzeros(nnz),
@@ -45,6 +46,61 @@ struct csr_t {
         nonzero_values(nnz) {}
 
   ~csr_t() {}
+
+  /**
+   * @brief Convert a Coordinate Sparse Format into Compressed Sparse Row
+   * Format.
+   *
+   * @tparam index_t
+   * @tparam offset_t
+   * @tparam value_t
+   * @param coo
+   * @return csr_t<memory_space_t::host, index_t, offset_t, value_t>&
+   */
+  csr_t<memory_space_t::host, index_t, offset_t, value_t>& operator=(
+      const coo_t<memory_space_t::host, index_t, offset_t, value_t>& coo) {
+    number_of_rows = coo.number_of_rows;
+    number_of_columns = coo.number_of_columns;
+    number_of_nonzeros = coo.number_of_nonzeros;
+
+    // Allocate space for vectors
+    row_offsets.resize(number_of_rows + 1);
+    column_indices.resize(number_of_columns);
+    nonzero_values.resize(number_of_nonzeros);
+
+    // compute number of non-zero entries per row of A
+    for (offset_t n = 0; n < number_of_nonzeros; n++) {
+      row_offsets[coo.row_indices[n]]++;
+    }
+
+    // cumulative sum the nnz per row to get row_offsets[]
+    for (index_t i = 0, sum = 0; i < number_of_rows; i++) {
+      index_t temp = row_offsets[i];
+      row_offsets[i] = sum;
+      sum += temp;
+    }
+    row_offsets[number_of_rows] = number_of_nonzeros;
+
+    // write coordinate column indices and nonzero values into CSR's
+    // column indices and nonzero values
+    for (offset_t n = 0; n < number_of_nonzeros; n++) {
+      index_t row = coo.row_indices[n];
+      index_t dest = row_offsets[row];
+
+      column_indices[dest] = coo.column_indices[n];
+      nonzero_values[dest] = coo.nonzero_values[n];
+
+      row_offsets[row]++;
+    }
+
+    for (index_t i = 0, last = 0; i <= number_of_rows; i++) {
+      index_t temp = row_offsets[i];
+      row_offsets[i] = last;
+      last = temp;
+    }
+
+    return *this;  // CSR representation (with possible duplicates)
+  }
 
 };  // struct csr_t
 
---
+++ unittests/coo_to_csr/test_coo_to_csr.cu
@@ -0,0 +1,53 @@
+#include <cstdlib>  // EXIT_SUCCESS
+
+#include <gunrock/error.hxx>             // error checking
+#include <gunrock/formats/formats.hxx>   // formats (csr, coo)
+#include <gunrock/memory.hxx>            // memory space
+#include <gunrock/io/matrix_market.hxx>  // matrix_market support
+
+void test_coo_to_csr(int num_arguments, char** argument_array) {
+  if (num_arguments != 2) {
+    std::cerr << "usage: ./test_coo_to_csr filename.mtx" << std::endl;
+    exit(1);
+  }
+
+  std::string filename = argument_array[1];
+
+  using namespace gunrock;
+  using namespace memory;
+
+  using vertex_t = int;
+  using edge_t = int;
+  using weight_t = float;
+
+  io::matrix_market_t<vertex_t, edge_t, weight_t> mm;
+  auto coo = mm.load(filename);
+  for (int i = 0; i < coo.number_of_nonzeros; ++i) {
+    std::cout << "(" << coo.row_indices[i] << ", " << coo.column_indices[i]
+              << ")"
+              << " = " << coo.nonzero_values[i] << std::endl;
+  }
+
+  format::csr_t<memory_space_t::host, vertex_t, edge_t, weight_t> csr;
+  csr = coo;
+
+  std::cout << "Row offsets: ";
+  for (auto offset : csr.row_offsets)
+    std::cout << offset << " ";
+  std::cout << std::endl;
+
+  std::cout << "Column Indices: ";
+  for (auto j : csr.column_indices)
+    std::cout << j << " ";
+  std::cout << std::endl;
+
+  std::cout << "Nonzero Values: ";
+  for (auto nz : csr.nonzero_values)
+    std::cout << nz << " ";
+  std::cout << std::endl;
+}
+
+int main(int argc, char** argv) {
+  test_coo_to_csr(argc, argv);
+  return EXIT_SUCCESS;
+}
