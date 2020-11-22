ff7b4099209dcd7b7e32c545e9aaf82e5b302bc6
cameronshinn <cameron.shinn@hotmail.com>
2020-11-21 19:51:57 -0800

    Updated to be simpler and use SM version over arch

--- gunrock/cuda/launch_box.hxx
+++ gunrock/cuda/launch_box.hxx
@@ -10,150 +10,121 @@
  */
 #pragma once
 
-#include <gunrock/cuda/device_properties.hxx>
-
-// Don't need SM_TAG right now, but will in the future
-#ifdef __CUDA_ARCH__
-#if   __CUDA_ARCH__ >= 860
-  #define SM_TAG   sm_86
-  #define ARCH_TAG ampere
-#elif __CUDA_ARCH__ >= 800
-  #define SM_TAG   sm_80
-  #define ARCH_TAG ampere
-#elif __CUDA_ARCH__ == 750
-  #define SM_TAG   sm_75
-  #define ARCH_TAG turing
-#elif __CUDA_ARCH__ >= 700
-  #define SM_TAG   sm_70
-  #define ARCH_TAG volta
-#elif __CUDA_ARCH__ == 620
-  #define SM_TAG   sm_62
-  #define ARCH_TAG pascal
-#elif __CUDA_ARCH__ >= 610
-  #define SM_TAG   sm_61
-  #define ARCH_TAG pascal
-#elif __CUDA_ARCH__ >= 600
-  #define SM_TAG   sm_60
-  #define ARCH_TAG pascal
-#elif __CUDA_ARCH__ == 530
-  #define SM_TAG   sm_53
-  #define ARCH_TAG maxwell
-#elif __CUDA_ARCH__ >= 520
-  #define SM_TAG   sm_52
-  #define ARCH_TAG maxwell
-#elif __CUDA_ARCH__ >= 500
-  #define SM_TAG   sm_50
-  #define ARCH_TAG maxwell
-#elif __CUDA_ARCH__ == 370
-  #define SM_TAG   sm_37
-  #define ARCH_TAG kepler
-#elif __CUDA_ARCH__ >= 350
-  #define SM_TAG   sm_35
-  #define ARCH_TAG kepler
-#elif __CUDA_ARCH__ == 320
-  #define SM_TAG   sm_32
-  #define ARCH_TAG kepler
-#elif __CUDA_ARCH__ >= 300
-  #define SM_TAG   sm_30
-  #define ARCH_TAG kepler
-#else
-  #error "Gunrock only supports sm_30 and above"
-#endif  // __CUDA_ARCH__
-#else
-  // What should these macros be on the host side?
-  #define SM_TAG   sm_00
-  #define ARCH_TAG ampere
-#endif
+#include <type_traits>
 
 namespace gunrock {
 namespace cuda {
 
-//////////////// Move this section to another file?
-/**
- * @brief Blank struct to use as a default base to make inheritance optional
- */
-struct empty_t {};
-
-// Inheritance structs to expand the variadic launch types passed into the launch box and define each them within the launch box's namespace
-template<typename... base_v>
-struct inherit_t;
-
-template<typename base_t, typename... base_v>
-struct inherit_t<base_t, base_v...> :
-base_t::template rebind<inherit_t<base_v...>> {};
-
-template<typename base_t>
-struct inherit_t<base_t> : base_t {};
-////////////////
-
 /**
  * @brief Struct holding kernel parameters will be passed in upon launch
- * @tparam block_dimension_ 1D block dimensions to launch with
- * @tparam grid_dimension_ 1D grid dimensions to launch with
+ * @tparam block_dimensions_ Block dimensions to launch with
+ * @tparam grid_dimensions_ Grid dimensions to launch with
  * @tparam smem_bytes_ Amount of shared memory to allocate
+ *
+ * @todo dimensions should be dim3 instead of unsigned int
  */
 template<
-  unsigned int block_dimension_,
-  unsigned int grid_dimension_,
+  unsigned int block_dimensions_,
+  unsigned int grid_dimensions_,
   unsigned int smem_bytes_ = 0
 >
 struct launch_params_t {
   enum : unsigned int {
-    block_dimension = block_dimension_,
-    grid_dimension = grid_dimension_,
+    block_dimensions = block_dimensions_,
+    grid_dimensions = grid_dimensions_,
     smem_bytes = smem_bytes_
   };
 };
 
-// Create launch param structs for each architecture for use in the launch box struct
-// rebind type allows inherit_t to re-set the base class to each different arch_launch_param_t
-// Is there a way to make archname##_arch_t generic and only macro the type alias template?
-#define NAMED_ARCH_TYPES(archname)                                     \
-  template<typename launch_params_t, typename base_t = empty_t>        \
-  struct archname##_arch_t : base_t {                                  \
-    typedef launch_params_t archname;                                  \
-                                                                       \
-    template<typename new_base_t>                                      \
-    using rebind = archname##_arch_t<launch_params_t, new_base_t>;     \
-  };                                                                   \
-                                                                       \
-  template<                                                            \
-    unsigned int block_dimension_,                                     \
-    unsigned int grid_dimension_,                                      \
-    unsigned int smem_bytes_ = 0                                       \
-  >                                                                    \
-  using archname = archname##_arch_t<launch_params_t<block_dimension_, \
-                                                     grid_dimension_,  \
-                                                     smem_bytes>>;  // launch_params_t wrapper template for each architecture
+#define TEST_SM 75  // Temporary until we figure out how to get cabability combined
 
-NAMED_ARCH_TYPES(kepler)
-NAMED_ARCH_TYPES(maxwell)
-NAMED_ARCH_TYPES(pascal)
-NAMED_ARCH_TYPES(volta)
-NAMED_ARCH_TYPES(turing)
-NAMED_ARCH_TYPES(ampere)
+/**
+ * @brief Struct holding kernel parameters for a specific SM version
+ * @tparam combined_ver_ Combined major and minor compute capability version
+ * @tparam block_dimensions_ Block dimensions to launch with
+ * @tparam grid_dimensions_ Grid dimensions to launch with
+ * @tparam smem_bytes_ Amount of shared memory to allocate
+ */
+template<
+  unsigned int combined_ver_,
+  unsigned int block_dimensions_,
+  unsigned int grid_dimensions_,
+  unsigned int smem_bytes_ = 0
+>
+struct sm_launch_params_t : launch_params_t<block_dimensions_, grid_dimensions_, smem_bytes_> {
+  enum : unsigned int {combined_ver = combined_ver_};
+};
 
-#undef NAMED_ARCH_LP_TYPE
+// Easier declaration inside launch box template
+template<
+  unsigned int combined_ver_,
+  unsigned int block_dimensions_,
+  unsigned int grid_dimensions_,
+  unsigned int smem_bytes_ = 0
+>
+using sm = sm_launch_params_t<
+             combined_ver_,
+             block_dimensions_,
+             grid_dimensions_,
+             smem_bytes_
+           >;
 
-template<typename... archs_launch_params_t>
-struct launch_box_t : inherit_t<archs_launch_params_t...> {
-  typedef inherit_t<archs_launch_params_t...> base_t;
-  // typedef all the types in the archs_launch_params_t pack
-  #define INHERIT_ARCH_PARAMS(archname) typedef typename base_t::archname archname;
+// Define named sm_launch_params_t structs for each SM version
+#define SM_LAUNCH_PARAMS(combined) \
+template<                                 \
+  unsigned int block_dimensions_,         \
+  unsigned int grid_dimensions_,          \
+  unsigned int smem_bytes_ = 0            \
+>                                         \
+using sm##combined = sm_launch_params_t<  \
+                       combined,          \
+                       block_dimensions_, \
+                       grid_dimensions_,  \
+                       smem_bytes_        \
+                    >;
 
-  INHERIT_ARCH_PARAMS(ampere)
-  INHERIT_ARCH_PARAMS(turing)
-  INHERIT_ARCH_PARAMS(pascal)
-  // Right now these lines will cause an error in unittest since there are no fallbacks
-  // The launch box expects every architecture listed in this section
-  // Leaving these lines commented out so we can later test fallbacks
-  // INHERIT_ARCH_PARAMS(volta)
-  // INHERIT_ARCH_PARAMS(turing)
-  INHERIT_ARCH_PARAMS(kepler)
+SM_LAUNCH_PARAMS(86)
+SM_LAUNCH_PARAMS(80)
+SM_LAUNCH_PARAMS(75)
+SM_LAUNCH_PARAMS(72)
+SM_LAUNCH_PARAMS(70)
+SM_LAUNCH_PARAMS(62)
+SM_LAUNCH_PARAMS(61)
+SM_LAUNCH_PARAMS(60)
+SM_LAUNCH_PARAMS(53)
+SM_LAUNCH_PARAMS(52)
+SM_LAUNCH_PARAMS(50)
+SM_LAUNCH_PARAMS(37)
+SM_LAUNCH_PARAMS(35)
+SM_LAUNCH_PARAMS(30)
 
-  #undef INHERIT_ARCH_PARAMS
+#undef SM_LAUNCH_PARAMS
 
-  typedef typename launch_box_t::ARCH_TAG launch_params;  // Launch params for the current GPU architecture
+template<typename... sm_lp_v>
+struct device_launch_params_t;
+
+template<typename sm_lp_t, typename... sm_lp_v>
+struct device_launch_params_t<sm_lp_t, sm_lp_v...> :
+std::conditional<
+  sm_lp_t::combined_ver == TEST_SM,
+  sm_lp_t,
+  device_launch_params_t<sm_lp_v...>
+>::type {};
+
+template<typename sm_lp_t>
+struct device_launch_params_t<sm_lp_t> :
+std::enable_if<
+  sm_lp_t::combined_ver == TEST_SM,
+  sm_lp_t
+>::type {};
+
+/**
+ * @brief Collection of kernel launch parameters for multiple architectures
+ * @tparam sm_lp_v... Pack of sm_launch_params_t types for each desired arch
+ */
+template<typename... sm_lp_v>
+struct launch_box_t : device_launch_params_t<sm_lp_v...> {
+  // Some methods to make it easy to access launch_params
 };
 
 }  // namespace gunrock
--- unittests/launch_box/test_launch_box.cu
+++ unittests/launch_box/test_launch_box.cu
@@ -5,15 +5,32 @@
 using namespace gunrock::cuda;
 
 int main(void) {
+
+  // First template argument signifies the SM version
   typedef launch_box_t<
-    ampere<16, 64, 2>,
-    turing<16, 32, 4>,
-    pascal<32, 64, 8>,
-    kepler<64, 64, 16>
+    sm_launch_params_t<86, 16, 64, 2>,
+    sm_launch_params_t<80, 16, 32, 4>,
+    sm_launch_params_t<75, 32, 64, 8>,
+    sm_launch_params_t<35, 64, 64, 16>
   > launch_t;
 
-  typedef LAUNCH_PARAMS(launch_t) arch_launch_t;
-  std::cout << "blockDim:  " << launch_t::launch_params::blockDim  << std::endl
-            << "gridDim:   " << launch_t::launch_params::gridDim   << std::endl
-            << "smemBytes: " << launch_t::launch_params::smemBytes << std::endl;
+  // They also have a short type name
+  typedef launch_box_t<
+    sm<86, 16, 64, 2>,
+    sm<80, 16, 32, 4>,
+    sm<75, 32, 64, 8>,
+    sm<35, 64, 64, 16>
+  > short_launch_t;
+
+  // SM launch params can also be specified via their named type
+  typedef launch_box_t<
+    sm86<16, 64, 2>,
+    sm80<16, 32, 4>,
+    sm75<32, 64, 8>,
+    sm35<64, 64, 16>
+  > named_launch_t;
+
+  std::cout << "block_dimensions: " << launch_t::block_dimensions << std::endl
+            << "grid_dimensions:  " << launch_t::grid_dimensions  << std::endl
+            << "smem_bytes:       " << launch_t::smem_bytes      << std::endl;
 }
