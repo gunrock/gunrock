6652a3d0f45c5d001a3a1830ac583fdd3b46d311
neoblizz <osama94@gmail.com>
2021-04-05 12:14:34 -0700

    cuda:: extended support, clean-up. block-mapped bugged for large graphs

---
+++ include/gunrock/cuda/global.hxx
@@ -0,0 +1,87 @@
+/**
+ * @file global.hxx
+ * @author Muhammad Osama (mosama@ucdavis.edu)
+ * @brief
+ * @version 0.1
+ * @date 2021-04-05
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+
+#pragma once
+namespace gunrock {
+namespace cuda {
+
+typedef int thread_idx_t;
+
+namespace thread {
+namespace global {
+namespace id {
+__device__ __forceinline__ int x() {
+  return threadIdx.x + (blockDim.x * blockIdx.x);
+}
+
+__device__ __forceinline__ int y() {
+  return threadIdx.y + (blockDim.y * blockIdx.y);
+}
+
+__device__ __forceinline__ int z() {
+  return threadIdx.z + (blockDim.z * blockIdx.z);
+}
+}  // namespace id
+}  // namespace global
+
+namespace local {
+namespace id {
+__device__ __forceinline__ int x() {
+  return threadIdx.x;
+}
+
+__device__ __forceinline__ int y() {
+  return threadIdx.y;
+}
+
+__device__ __forceinline__ int z() {
+  return threadIdx.z;
+}
+}  // namespace id
+}  // namespace local
+}  // namespace thread
+
+namespace block {
+namespace id {
+__device__ __forceinline__ int x() {
+  return blockIdx.x;
+}
+
+__device__ __forceinline__ int y() {
+  return blockIdx.y;
+}
+
+__device__ __forceinline__ int z() {
+  return blockIdx.z;
+}
+}  // namespace id
+
+namespace size {
+__device__ __forceinline__ int x() {
+  return blockDim.x;
+}
+
+__device__ __forceinline__ int y() {
+  return blockDim.y;
+}
+
+__device__ __forceinline__ int z() {
+  return blockDim.z;
+}
+
+__device__ __forceinline__ int total() {
+  return x() * y() * z();
+}
+}  // namespace size
+}  // namespace block
+
+}  // namespace cuda
+}  // namespace gunrock
--- include/gunrock/framework/operators/advance/block_mapped.hxx
+++ include/gunrock/framework/operators/advance/block_mapped.hxx
@@ -14,6 +14,7 @@
 #include <gunrock/util/math.hxx>
 #include <gunrock/cuda/context.hxx>
 #include <gunrock/cuda/launch_box.hxx>
+#include <gunrock/cuda/global.hxx>
 
 #include <gunrock/framework/operators/configs.hxx>
 
@@ -42,90 +43,96 @@ void __global__ block_mapped_kernel(graph_t const G,
   using vertex_t = typename graph_t::vertex_type;
   using edge_t = typename graph_t::edge_type;
 
+  // TODO: accept gunrock::frontier_t instead of typename frontier_t::type_t.
+  using type_t = frontier_t;
+
   // Specialize Block Scan for 1D block of THREADS_PER_BLOCK.
   using block_scan_t = cub::BlockScan<edge_t, THREADS_PER_BLOCK>;
   // using block_load_t =
   //     cub::BlockLoad<edge_t, THREADS_PER_BLOCK, ITEMS_PER_THREAD>;
+
+  auto global_idx = cuda::thread::global::id::x();
+  auto local_idx = cuda::thread::local::id::x();
 
   __shared__ union TempStorage {
     typename block_scan_t::TempStorage scan;
     // typename block_load_t::TempStorage load;
   } storage;
 
-  __shared__ vertex_t shared_vertices[THREADS_PER_BLOCK];
-  __shared__ edge_t shared_degrees[THREADS_PER_BLOCK];
-  __shared__ edge_t shared_starting_edges[THREADS_PER_BLOCK];
+  // Prepare data to process (to shmem/registers).
+  __shared__ vertex_t vertices[THREADS_PER_BLOCK];
+  __shared__ edge_t degrees[THREADS_PER_BLOCK];
+  __shared__ edge_t sedges[THREADS_PER_BLOCK];
+  edge_t th_deg[ITEMS_PER_THREAD];
 
-  auto idx = threadIdx.x + (blockDim.x * blockIdx.x);
-
-  if (idx < input_size) {
-    vertex_t v = input[idx];
-    shared_vertices[threadIdx.x] = v;
+  if (global_idx < input_size) {
+    vertex_t v = input[global_idx];
+    vertices[local_idx] = v;
     if (gunrock::util::limits::is_valid(v)) {
-      shared_starting_edges[threadIdx.x] = G.get_starting_edge(v);
-      shared_degrees[threadIdx.x] = G.get_number_of_neighbors(v);
+      sedges[local_idx] = G.get_starting_edge(v);
+      th_deg[0] = G.get_number_of_neighbors(v);
     } else {
-      shared_degrees[threadIdx.x] = 0;
+      th_deg[0] = 0;
     }
   } else {
-    shared_vertices[threadIdx.x] = gunrock::numeric_limits<vertex_t>::invalid();
-    shared_degrees[threadIdx.x] = 0;
+    vertices[local_idx] = gunrock::numeric_limits<vertex_t>::invalid();
+    th_deg[0] = 0;
   }
-
-  // Per-thread tile data
   __syncthreads();
-  edge_t deg[ITEMS_PER_THREAD];
-  deg[0] = shared_degrees[threadIdx.x];
 
-  // if (idx < input_size)
-  // printf("\t# input: shared_degrees = %i\n", shared_degrees[threadIdx.x]);
-
-  __syncthreads();
+  // Exclusive sum of degrees.
   vertex_t aggregate_degree_per_block;
-  block_scan_t(storage.scan).ExclusiveSum(deg, deg, aggregate_degree_per_block);
+  block_scan_t(storage.scan)
+      .ExclusiveSum(th_deg, th_deg, aggregate_degree_per_block);
   __syncthreads();
 
-  // Store back to shared memory
-  shared_degrees[threadIdx.x] = deg[0];
+  // Store back to shared memory.
+  degrees[local_idx] = th_deg[0];
   __syncthreads();
 
-  // if (idx < input_size)
-  //   printf("\t# output: shared_degrees = %i\n", shared_degrees[threadIdx.x]);
-
-  if (threadIdx.x == 0)
-    math::atomic::add((vertex_t*)output_size, aggregate_degree_per_block);
-
-  auto length = idx - threadIdx.x + blockDim.x;
+  // To search for which vertex id we are computing on.
+  auto length = global_idx - local_idx + cuda::block::size::x();
 
   // Bound check.
   if (input_size < length)
     length = input_size;
-  length -= idx - threadIdx.x;
 
-  for (int i = threadIdx.x; i < aggregate_degree_per_block; i += blockDim.x) {
-    // printf("global_edge_processing = %i\n", i);
-    int id = algo::search::binary::rightmost(shared_degrees, i, length);
+  length -= global_idx - local_idx;
 
-    // printf("id, length = %i, %i\n", id, length);
+  for (int i = local_idx;               // threadIdx.x
+       i < aggregate_degree_per_block;  // total degree to process
+       i += cuda::block::size::x()      // increment by blockDim.x
+  ) {
+    // Binary search to find which vertex id to work on.
+    int id = algo::search::binary::rightmost(degrees, i, length);
+
+    // If the id is greater than the width of the block or the input size, we
+    // exit.
     if (id >= length)
       continue;
 
-    vertex_t v = shared_vertices[id];
-
-    // printf("vertex = %i\n", v);
+    // Fetch the vertex corresponding to the id.
+    vertex_t v = vertices[id];
     if (!gunrock::util::limits::is_valid(v))
       continue;
 
-    auto e = shared_starting_edges[id] + i - shared_degrees[id];
+    // If the vertex is valid, get its edge, neighbor and edge weight.
+    auto e = sedges[id] + i - degrees[id];
     auto n = G.get_destination_vertex(e);
     auto w = G.get_edge_weight(e);
+
+    // Use-defined advance condition.
     bool cond = op(v, n, e, w);
 
-    /* printf("output[%i] = v, n, e, w, cond = %i, %i, %i, %f, %s\n", i, v, n,
-       e, w, cond ? "true" : "false"); */
-
-    output[i] =
+    // Store [neighbor] into the output frontier.
+    output[cuda::block::id::x() + i] =
         (cond && n != v) ? n : gunrock::numeric_limits<vertex_t>::invalid();
+  }
+
+  // Accumulate the output size to global memory, only done once per block.
+  if (local_idx == 0) {
+    math::atomic::add((vertex_t*)output_size, aggregate_degree_per_block);
+    // printf("aggregate_degree_per_block = %d\n", aggregate_degree_per_block);
   }
 }
 
@@ -156,6 +163,7 @@ void execute(graph_t& G,
   context.synchronize();
 
   thrust::host_vector<std::size_t> h_output_size = output_size;
+  // std::cout << "Output Size = " << h_output_size[0] << std::endl;
   output->set_number_of_elements(h_output_size[0]);
 }
 
