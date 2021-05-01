0746fdd25b4a24ed33798dbbf4db82972b5db52f
neoblizz <osama94@gmail.com>
2021-05-01 12:27:29 -0700

    Moved tests to unit_test directory

--- unittests/coo/test_coo.cu
+++ unittests/coo/test_coo.cu
@@ -23,8 +23,7 @@ void test_coo(int num_arguments, char** argument_array) {
 
   io::matrix_market_t<vertex_t, edge_t, weight_t> mm;
 
-  using csr_t =
-      format::csr_t<memory_space_t::host, vertex_t, edge_t, weight_t>;
+  using csr_t = format::csr_t<memory_space_t::host, vertex_t, edge_t, weight_t>;
   csr_t csr;
   csr.from_coo(mm.load(filename));
 
@@ -34,29 +33,35 @@ void test_coo(int num_arguments, char** argument_array) {
   thrust::host_vector<vertex_t> row_indices(csr.number_of_nonzeros);
 
   auto G = graph::build::from_csr<memory_space_t::host, graph::view_t::coo>(
-      csr.number_of_rows,               // rows
-      csr.number_of_columns,            // columns
-      csr.number_of_nonzeros,           // nonzeros
-      csr.row_offsets.data(),           // row_offsets
-      csr.column_indices.data(),        // column_indices
-      csr.nonzero_values.data(),        // values
-      row_indices.data()
-  );  // supports row_indices and column_offsets (default = nullptr)
+      csr.number_of_rows,         // rows
+      csr.number_of_columns,      // columns
+      csr.number_of_nonzeros,     // nonzeros
+      csr.row_offsets.data(),     // row_offsets
+      csr.column_indices.data(),  // column_indices
+      csr.nonzero_values.data(),  // values
+      row_indices.data());  // supports row_indices and column_offsets (default
+                            // = nullptr)
 
   // >>
-  std::cout << "G.get_number_of_vertices() : " << G.get_number_of_vertices() << std::endl;
-  std::cout << "G.get_number_of_edges()    : " << G.get_number_of_edges()    << std::endl;
-  
+  std::cout << "G.get_number_of_vertices()\t: " << G.get_number_of_vertices()
+            << std::endl;
+  std::cout << "G.get_number_of_edges()\t: " << G.get_number_of_edges()
+            << std::endl;
+  std::cout << "G.number_of_graph_representations()\t: "
+            << G.number_of_graph_representations() << std::endl;
+
   gunrock::print::head<vertex_t>(G.get_row_indices(), G.get_number_of_edges());
-  gunrock::print::head<vertex_t>(G.get_column_indices(), G.get_number_of_edges());
-  gunrock::print::head<weight_t>(G.get_nonzero_values(), G.get_number_of_edges());
-  
-  for(vertex_t i = 0; i < G.get_number_of_vertices(); i++)
+  gunrock::print::head<vertex_t>(G.get_column_indices(),
+                                 G.get_number_of_edges());
+  gunrock::print::head<weight_t>(G.get_nonzero_values(),
+                                 G.get_number_of_edges());
+
+  for (vertex_t i = 0; i < G.get_number_of_vertices(); i++)
     std::cout << i << " " << G.get_starting_edge(i) << std::endl;
-  
+
   std::cout << "-------" << std::endl;
-  
-  for(vertex_t i = 0; i < G.get_number_of_vertices(); i++)
+
+  for (vertex_t i = 0; i < G.get_number_of_vertices(); i++)
     std::cout << i << " " << G.get_number_of_neighbors(i) << std::endl;
 
   std::cout << "-------" << std::endl;
