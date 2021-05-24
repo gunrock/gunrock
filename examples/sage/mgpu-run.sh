49fa987d7276504bbf501955d566dafda1912ef5
Serban Porumbescu <porumbes@users.noreply.github.com>
2021-05-24 10:12:00 -0700

    Graph SAGE sage slurm script tweaks.

--- examples/sage/mgpu-run.sh
+++ examples/sage/mgpu-run.sh
@@ -1,17 +1,17 @@
-PARTITION_NAME="dgx2"
-NODE_NAME="rl-dgx2-c24-u16"
+#PARTITION_NAME="dgx2"
+#NODE_NAME="rl-dgx2-c24-u16"
 
-#PARTITION_NAME="dgxa100_1tb"
-#NODE_NAME="rl-dgxa-d22-u30"
+PARTITION_NAME="dgxa100_1tb"
+NODE_NAME="rl-dgxa-d22-u30"
 
-NUM_GPUS=16
-#NUM_GPUS=8
+#NUM_GPUS=16
+NUM_GPUS=8
 
 APP_NAME="sage"
 BIN_PREFIX="../../build/bin/"
 DATA_PREFIX="/home/u00u7u37rw7AjJoA4e357/data/gunrock/hive_datasets/mario-2TB/pokec"
 DATA1="pokec.mtx"
-APP_OPTIONS="--undirected --batch-size 16384"
+APP_OPTIONS="--undirected --batch-size 16384 --quick"
 
 OUTPUT_DIR="eval_mgpu/$PARTITION_NAME/$NODE_NAME"
 mkdir -p $OUTPUT_DIR
@@ -19,10 +19,10 @@ mkdir -p $OUTPUT_DIR
 for (( i=1; i<=$NUM_GPUS; i++))
 do
     SLURM_CMD="srun --cpus-per-gpu 1 -G $i -p $PARTITION_NAME -w $NODE_NAME"
-    echo $SLURM_CMD $BIN_PREFIX$APP_NAME \
+    $SLURM_CMD $BIN_PREFIX$APP_NAME \
                 market $DATA_PREFIX/$DATA1 \
                 $APP_OPTIONS \
-                "> ./$OUTPUT_DIR/${APP_NAME}_GPU$i.txt" # &
+                > ./$OUTPUT_DIR/${APP_NAME}_GPU$i.txt  &
 done
 
 
