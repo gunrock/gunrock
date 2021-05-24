d6186858d7d8131f4a7fac685c4cd97b8d32c708
Serban Porumbescu <porumbes@users.noreply.github.com>
2021-05-24 10:10:38 -0700

    Graph Search rw multi-gpu slrum script tweaks.

--- examples/rw/mgpu-run-directed-uniform.sh
+++ examples/rw/mgpu-run-directed-uniform.sh
@@ -1,13 +1,13 @@
 # random walk: directed, uniform
 
-PARTITION_NAME="dgx2"
-NODE_NAME="rl-dgx2-c24-u16"
+#PARTITION_NAME="dgx2"
+#NODE_NAME="rl-dgx2-c24-u16"
 
-#PARTITION_NAME="dgxa100_1tb"
-#NODE_NAME="rl-dgxa-d22-u30"
+PARTITION_NAME="dgxa100_1tb"
+NODE_NAME="rl-dgxa-d22-u30"
 
-NUM_GPUS=1
-#NUM_GPUS=8
+#NUM_GPUS=16
+NUM_GPUS=8
 
 APP_NAME="rw"
 BIN_PREFIX="../../build/bin/"
@@ -23,9 +23,9 @@ mkdir -p $OUTPUT_DIR
 for (( i=1; i<=$NUM_GPUS; i++))
 do
     SLURM_CMD="srun --cpus-per-gpu 1 -G $i -p $PARTITION_NAME -w $NODE_NAME"
-    echo $SLURM_CMD $BIN_PREFIX$APP_NAME \
+     $SLURM_CMD $BIN_PREFIX$APP_NAME \
                 market $DATA_PREFIX/$DATA1.mtx \
                 $APP_OPTIONS \
-                "> ./$OUTPUT_DIR/${APP_NAME}_${DATA1}_GPU$i.txt" &
+                > ./$OUTPUT_DIR/${APP_NAME}_${DATA1}_GPU$i.txt &
 done
 
