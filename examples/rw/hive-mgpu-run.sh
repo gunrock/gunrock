106bc7d66c043029f88916175e9566d3a5968096
Serban Porumbescu <porumbes@users.noreply.github.com>
2021-05-25 15:51:36 -0700

    Add graph search rw slurm scripts for directed uniform

--- examples/rw/hive-mgpu-run.sh
+++ examples/rw/hive-mgpu-run.sh
@@ -4,19 +4,23 @@ PARTITION_NAME="dgx2"
 NUM_GPUS=2
 
 #APP_SCRIPT="./hive-rw-test.sh"
-APP_SCRIPT="./hive-rw-undirected-uniform.sh"
+APP_SCRIPT[0]="./hive-rw-undirected-uniform.sh"
+APP_SCRIPT[1]="./hive-rw-directed-uniform.sh"
 
 OUTPUT_DIR="rw_eval_mgpu/$PARTITION_NAME"
 mkdir -p $OUTPUT_DIR
 
-for (( i=1; i<=$NUM_GPUS; i++))
+for app in {0..1}
 do
-    # prepare and run SLURM command
-    #SLURM_CMD="srun --cpus-per-gpu 1 -G $i -p $PARTITION_NAME -w $NODE_NAME"
+   for (( i=1; i<=$NUM_GPUS; i++))
+   do
+       # prepare and run SLURM command
+       #SLURM_CMD="srun --cpus-per-gpu 1 -G $i -p $PARTITION_NAME -w $NODE_NAME"
 
-    SLURM_CMD="srun --cpus-per-gpu 1 -G $i -p $PARTITION_NAME "
-    $SLURM_CMD $APP_SCRIPT $OUTPUT_DIR $i &
+       SLURM_CMD="srun --cpus-per-gpu 1 -G $i -p $PARTITION_NAME "
+       $SLURM_CMD ${APP_SCRIPT[$app]} $OUTPUT_DIR $i &
 
-    #echo "$SLURM_CMD $APP_SCRIPT $OUTPUT_DIR $i"
-    #sleep 1
+       #echo "$SLURM_CMD ${APP_SCRIPT[$app]} $OUTPUT_DIR $i"
+       #sleep 1
+   done
 done
