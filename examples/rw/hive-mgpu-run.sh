e8f1df75c75d9c8b2486e68dabf20e2e94cc4f5c
Serban Porumbescu <porumbes@users.noreply.github.com>
2021-05-25 15:35:49 -0700

    Add graph search rw slurm scripts for undirected-uniform

---
+++ examples/rw/hive-mgpu-run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+PARTITION_NAME="dgx2"
+NUM_GPUS=2
+
+#APP_SCRIPT="./hive-rw-test.sh"
+APP_SCRIPT="./hive-rw-undirected-uniform.sh"
+
+OUTPUT_DIR="rw_eval_mgpu/$PARTITION_NAME"
+mkdir -p $OUTPUT_DIR
+
+for (( i=1; i<=$NUM_GPUS; i++))
+do
+    # prepare and run SLURM command
+    #SLURM_CMD="srun --cpus-per-gpu 1 -G $i -p $PARTITION_NAME -w $NODE_NAME"
+
+    SLURM_CMD="srun --cpus-per-gpu 1 -G $i -p $PARTITION_NAME "
+    $SLURM_CMD $APP_SCRIPT $OUTPUT_DIR $i &
+
+    #echo "$SLURM_CMD $APP_SCRIPT $OUTPUT_DIR $i"
+    #sleep 1
+done
---
+++ examples/rw/hive-rw-undirected-uniform.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# random walk: undirected, uniform
+
+APP_NAME="rw"
+
+BIN_PREFIX="../../build/bin/"
+DATA_PREFIX="/home/u00u7u37rw7AjJoA4e357/data/gunrock/hive_datasets/mario-2TB/graphsearch"
+
+DATA1="dir_gs_twitter" #.mtx
+#DATA2="gs_twitter.values"
+
+WALK_MODE="0" #0, 1, or 2
+APP_OPTIONS="--walk-mode ${WALK_MODE} --walk-length 128 --undirected=true --store-walks 0 --quick --num-runs 10 --seed 123"
+
+OUTPUT_DIR=${1:-"rw_eval_mgpu"}
+NUM_GPUS=${2:-"1"}
+JSON_FILE=""
+TIMESTAMP=`date '+%Y-%m-%d_%H:%M:%S'`
+TAG=$TIMESTAMP
+
+NAME1[0]="dir_gs_twitter"
+GRAPH[0]="market $DATA_PREFIX/${NAME1[0]}.mtx"
+
+SUB_DIR="undirected-uniform"
+mkdir -p "$OUTPUT_DIR/$SUB_DIR"
+
+for i in {0..0}
+do
+   # prepare output json file name with number of gpus for this run
+   JSON_FILE="${APP_NAME}__${NAME1[$i]}__GPU${NUM_GPUS}"
+
+   #echo \
+   $BIN_PREFIX$APP_NAME \
+   ${GRAPH[$i]} \
+   $APP_OPTIONS \
+   --tag=$TAG \
+   --jsonfile="$OUTPUT_DIR/$SUB_DIR/$JSON_FILE.json" \
+   > "$OUTPUT_DIR/$SUB_DIR/$JSON_FILE.output.txt"
+done
+
