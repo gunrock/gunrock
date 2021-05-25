9f99a70bb696e85c85d2e37eb2ef04eaf63e9e87
Serban Porumbescu <porumbes@users.noreply.github.com>
2021-05-24 23:16:14 -0700

    Clean up scan statistics slurm scripts

--- examples/ss/hive-mgpu-run.sh
+++ examples/ss/hive-mgpu-run.sh
@@ -1,28 +1,21 @@
 #!/bin/bash
 
 PARTITION_NAME="dgx2"
-NUM_GPUS=16
-
-OUTPUT_DIR="eval_mgpu/$PARTITION_NAME"
-JSON_FILE=""
-TIMESTAMP=""
-TAG=""
+NUM_GPUS=2
 
 APP_SCRIPT="./hive-ss-test.sh"
 
+OUTPUT_DIR="eval_mgpu/$PARTITION_NAME"
 mkdir -p $OUTPUT_DIR
 
 for (( i=1; i<=$NUM_GPUS; i++))
 do
-    # prepare output json file name with number of gpus for this run
-    TIMESTAMP=`date '+%Y-%m-%d_%H:%M:%S'`
-    TAG=$TIMESTAMP
-    JSON_FILE="ss__${TAG}__GPU${i}"
-
     # prepare and run SLURM command
     #SLURM_CMD="srun --cpus-per-gpu 1 -G $i -p $PARTITION_NAME -w $NODE_NAME"
+
     SLURM_CMD="srun --cpus-per-gpu 1 -G $i -p $PARTITION_NAME "
-    #echo "$SLURM_CMD $APP_SCRIPT $OUTPUT_DIR/$JSON_FILE"
-    $SLURM_CMD $APP_SCRIPT $OUTPUT_DIR/$JSON_FILE &
+    $SLURM_CMD $APP_SCRIPT $OUTPUT_DIR $i &
+
+    #echo "$SLURM_CMD $APP_SCRIPT $OUTPUT_DIR $i"
     #sleep 1
 done
--- examples/ss/hive-ss-test.sh
+++ examples/ss/hive-ss-test.sh
@@ -8,8 +8,12 @@ DATA_PREFIX="/home/u00u7u37rw7AjJoA4e357/data/gunrock/hive_datasets/mario-2TB"
 
 APP_OPTIONS="--undirected --num-runs 10 --quick"
 
-#OUTPUT_DIR=${1:-"eval_mgpu"}
-JSON_FILE=${1:-""}
+OUTPUT_DIR=${1:-"eval_mgpu"}
+NUM_GPUS=${2:-"1"}
+JSON_FILE=""
+
+TIMESTAMP=`date '+%Y-%m-%d_%H:%M:%S'`
+TAG=$TIMESTAMP
 
 NAME[0]="pokec"
 
@@ -17,17 +21,14 @@ GRAPH[0]="market $DATA_PREFIX/${NAME[0]}/${NAME[0]}.mtx"
 
 for i in {0..0}
 do
+   # prepare output json file name with number of gpus for this run
+   JSON_FILE="${APP_NAME}__${NAME[$i]}__GPU${NUM_GPUS}"	
 
    #echo \
    $BIN_PREFIX$APP_NAME \
    ${GRAPH[$i]} \
    $APP_OPTIONS \
-   --jsonfile=$JSON_FILE.json \
-   > $JSON_FILE.output.txt
-   #> /dev/null 2>&1
-   #"> /dev/null 2>&1"
-   #"> $OUTPUT_DIR/${NAME[$i]}.$APP_NAME.output.txt"
-   #> $OUTPUT_DIR/${NAME[$i]}.$APP_NAME.output.txt
-   #--jsondir=$OUTPUT_DIR \
-
+   --tag=$TAG \
+   --jsonfile="$OUTPUT_DIR/$JSON_FILE.json" \
+   > "$OUTPUT_DIR/$JSON_FILE.output.txt"
 done
