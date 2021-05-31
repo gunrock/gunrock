1dc1545e17ae5c931bd1b98ebaa2d1126c252ae6
Serban Porumbescu <porumbes@users.noreply.github.com>
2021-05-31 10:19:44 -0700

    Add Local Graph Clustering pr_nibble slurm scripts

---
+++ examples/pr_nibble/hive-mgpu-run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+PARTITION_NAME="dgx2"
+NUM_GPUS=16
+
+APP_SCRIPT="./hive-pr_nibble-test.sh"
+
+OUTPUT_DIR="pr_nibble_eval_mgpu/$PARTITION_NAME"
+mkdir -p $OUTPUT_DIR
+
+for (( i=1; i<=$NUM_GPUS; i++))
+do
+    # prepare and run SLURM command
+    #SLURM_CMD="srun --cpus-per-gpu 1 -G $i -p $PARTITION_NAME -w $NODE_NAME"
+
+    SLURM_CMD="srun --cpus-per-gpu 1 -G $i -p $PARTITION_NAME -N 1 "
+    $SLURM_CMD $APP_SCRIPT $OUTPUT_DIR $i &
+
+    #echo "$SLURM_CMD $APP_SCRIPT $OUTPUT_DIR $i"
+    #sleep 1
+done
