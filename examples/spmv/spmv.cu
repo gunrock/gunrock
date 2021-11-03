073b07eab4cf305a5532ee72f33cd0d0e6efff5e
neoblizz <osama94@gmail.com>
2021-11-02 19:06:17 -0700

    Cleanup, narrowed segfault.

--- examples/spmv/spmv.cu
+++ examples/spmv/spmv.cu
@@ -1,8 +1,9 @@
 #include <gunrock/algorithms/algorithms.hxx>
 
-// Include the application code -- we'll comment this out now so we can compile a test quickly.
+// Include the application code -- we'll comment this out now so we can compile
+// a test quickly.
 #include <gunrock/algorithms/spmv.hxx>
-#include <random>
+#include <gunrock/algorithms/generate/random.hxx>
 
 using namespace gunrock;
 using namespace memory;
@@ -19,21 +20,22 @@ void test_spmv(int num_arguments, char** argument_array) {
   // - vertex ids (vertex_t)
   // - edge offsets (edge_t)
   // - edge weights (weight_t)
-  
+
   using vertex_t = int;
-  using edge_t   = int;
+  using edge_t = int;
   using weight_t = float;
 
   // --
   // IO
-  
+
   // Filename to be read
   std::string filename = argument_array[1];
 
   // Load the matrix-market dataset into csr format.
   // See `format` to see other supported formats.
   io::matrix_market_t<vertex_t, edge_t, weight_t> mm;
-  using csr_t = format::csr_t<memory_space_t::device, vertex_t, edge_t, weight_t>;
+  using csr_t =
+      format::csr_t<memory_space_t::device, vertex_t, edge_t, weight_t>;
   csr_t csr;
   csr.from_coo(mm.load(filename));
 
@@ -44,44 +46,28 @@ void test_spmv(int num_arguments, char** argument_array) {
   // `memory_space_t::device` -> the graph will be created on the GPU.
   // `graph::view_t::csr`     -> your input data is in `csr` format.
   //
-  // Note that `graph::build::from_csr` expects pointers, but the `csr` data arrays
-  // are `thrust` vectors, so we need to unwrap them w/ `.data().get()`.
+  // Note that `graph::build::from_csr` expects pointers, but the `csr` data
+  // arrays are `thrust` vectors, so we need to unwrap them w/ `.data().get()`.
   auto G = graph::build::from_csr<memory_space_t::device, graph::view_t::csr>(
-      csr.number_of_rows,
-      csr.number_of_columns,
-      csr.number_of_nonzeros,
-      csr.row_offsets.data().get(),
-      csr.column_indices.data().get(),
-      csr.nonzero_values.data().get() 
-  );
+      csr.number_of_rows, csr.number_of_columns, csr.number_of_nonzeros,
+      csr.row_offsets.data().get(), csr.column_indices.data().get(),
+      csr.nonzero_values.data().get());
 
   // --
   // Params and memory allocation
-
-  srand(42);
-
-
   vertex_t n_vertices = G.get_number_of_vertices();
   thrust::device_vector<weight_t> x(n_vertices);
-
-  for(int i = 0; i < n_vertices; i++){
-    x[i] = rand()%100;
-  }
-
   thrust::device_vector<weight_t> y(n_vertices);
 
+  gunrock::generate::random::uniform_distribution(0, n_vertices, x.begin());
 
   // --
   // GPU Run
+  float gpu_elapsed = gunrock::spmv::run(G, x.data().get(), y.data().get());
 
-  float gpu_elapsed = gunrock::spmv::run(G, thrust::raw_pointer_cast(&x[0]), thrust::raw_pointer_cast(&y[0]));
-
-
+  std::cout << "GPU y[:40] = ";
+  gunrock::print::head<weight_t>(y, 40);
   std::cout << "GPU Elapsed Time : " << gpu_elapsed << " (ms)" << std::endl;
-  for(int i = 0; i < n_vertices; i++){
-    std::cout << "y[" << i << "] = " << y[i] << std::endl;
-  }
-
 }
 
 // Main method, wrapping test function
--- include/gunrock/algorithms/spmv.hxx
+++ include/gunrock/algorithms/spmv.hxx
@@ -1,64 +1,53 @@
 #pragma once
 
 #include <gunrock/algorithms/algorithms.hxx>
-#include <gunrock/util/math.hxx>
 
 namespace gunrock {
 namespace spmv {
 
-// I can't figure out any params the user would provide
 template <typename weight_t>
 struct param_t {
   weight_t* x;
   param_t(weight_t* _x) : x(_x) {}
 };
 
-//result_t looks exactly like a matrix, since the user will get a matrix after mtx vector mult
 template <typename weight_t>
 struct result_t {
-    weight_t* y;
-    result_t(weight_t* _y) : y(_y) {}
+  weight_t* y;
+  result_t(weight_t* _y) : y(_y) {}
 };
 
-// <boilerplate>
 template <typename graph_t, typename param_type, typename result_type>
 struct problem_t : gunrock::problem_t<graph_t> {
   param_type param;
   result_type result;
 
-  problem_t(
-    graph_t& G,
-    param_type& _param,
-    result_type& _result,
-    std::shared_ptr<cuda::multi_context_t> _context
-  ) : gunrock::problem_t<graph_t>(G, _context), param(_param), result(_result) {}
+  problem_t(graph_t& G,
+            param_type& _param,
+            result_type& _result,
+            std::shared_ptr<cuda::multi_context_t> _context)
+      : gunrock::problem_t<graph_t>(G, _context),
+        param(_param),
+        result(_result) {}
 
   using vertex_t = typename graph_t::vertex_type;
   using edge_t = typename graph_t::edge_type;
   using weight_t = typename graph_t::weight_type;
-// </boilerplate>
+  // </boilerplate>
 
-  void init() override {}
+  thrust::device_vector<weight_t> dummy;
 
-  // `reset` function, described above.  Should be called
+  void init() override { this->dummy.resize(2); }
+
   void reset() override {
-    auto G = this->get_graph();
-    auto y = this->result.y;
-    thrust::fill(
-      thrust::device, 
-      y,
-      y + G.get_number_of_vertices(),
-      0
-      );
+    auto policy = this->context->get_context(0)->execution_policy();
+    auto g = this->get_graph();
+    thrust::fill_n(policy, this->result.y, g.get_number_of_vertices(), 0.0f);
   }
 };
 
-
 template <typename problem_t>
 struct enactor_t : gunrock::enactor_t<problem_t> {
-  // Use Base class constructor -- does this work? does it handle copy
-  // constructor?
-  
   using gunrock::enactor_t<problem_t>::enactor_t;
 
   using vertex_t = typename problem_t::vertex_t;
@@ -72,44 +61,45 @@ struct enactor_t : gunrock::enactor_t<problem_t> {
     auto E = this->get_enactor();
     auto P = this->get_problem();
     auto G = P->get_graph();
-    auto y = this->result.y;
-    auto x = this->param.x;
+    auto dummy = P->dummy.data().get();
 
-    auto spmv = [=] __host__ __device__(
-                             vertex_t const& source,     // ... source (row index)
-                             vertex_t const& neighbor,   // neighbor (column index)
-                             edge_t const& edge,         // edge (row ↦ column)
-                             weight_t const& weight      // weight (nonzero).
-                             ) -> bool {
+    // TODO: This causes segfault.
+    // auto re = P->result;
+    // auto y = P->result.y;
+    // auto x = P->param.x;
+
+    /* auto spmv = [=] __host__ __device__(
+                    vertex_t const& source,    // ... source (row index)
+                    vertex_t const& neighbor,  // neighbor (column index)
+                    edge_t const& edge,        // edge (row ↦ column)
+                    weight_t const& weight     // weight (nonzero).
+                    ) -> bool {
       // y[row index] += weight * x[column index]
-      math::atomic::add(&(y[source]), weight * x[neighbor]); 
-      return false; // ignored.
+      math::atomic::add(&(y[source]), weight * x[neighbor]);
+      return false;  // ignored.
     };
 
     // Perform advance on the above lambda-op
-    operators::advance::execute<operators::load_balance_t::block_mapped,    // flavor of load-balancing schedule
-                                operators::advance_direction_t::forward,    // direction (backward for transpose)
-                                operators::advance_io_type_t::graph,        // entire graph as input
-                                operators::advance_io_type_t::none>(        // no output frontier needed
-          G, E, spmv, context);
+    operators::advance::execute<
+        operators::load_balance_t::block_mapped,
+        operators::advance_direction_t::forward,  // direction (backward for
+                                                  // transpose)
+        operators::advance_io_type_t::graph,      // entire graph as input
+        operators::advance_io_type_t::none>(      // no output frontier needed
+        G, E, spmv, context); */
   }
 
   virtual bool is_converged(cuda::multi_context_t& context) {
-    if (this->iteration == 0){
-      return false;
-    }
-    return true;
-    }
-  };  // struct enactor_t
+    return this->iteration == 0 ? false : true;
+  }
+};  // struct enactor_t
 
 template <typename graph_t>
 float run(graph_t& G,
-          typename graph_t::weight_type* x, // Input vector
-          typename graph_t::weight_type* y // Output
+          typename graph_t::weight_type* x,  // Input vector
+          typename graph_t::weight_type* y   // Output vector
 ) {
-
   // <user-defined>
-  using vertex_t = typename graph_t::vertex_type;
   using weight_t = typename graph_t::weight_type;
 
   using param_type = param_t<weight_t>;
@@ -118,7 +108,7 @@ float run(graph_t& G,
   param_type param(x);
   result_type result(y);
   // </user-defined>
-  
+
   // <boiler-plate>
   auto multi_context =
       std::shared_ptr<cuda::multi_context_t>(new cuda::multi_context_t(0));
@@ -139,5 +129,5 @@ float run(graph_t& G,
   // </boiler-plate>
 }
 
-} // namespace spmv
-} // namespace gunrock
+}  // namespace spmv
+}  // namespace gunrock
