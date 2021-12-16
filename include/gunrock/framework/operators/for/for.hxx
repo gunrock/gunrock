a04502e724c9453f9ce986bbe339ce3e229c8df4
neoblizz <osama94@gmail.com>
2021-12-16 11:39:34 -0800

    Improved for operator.

--- include/gunrock/framework/operators/for/for.hxx
+++ include/gunrock/framework/operators/for/for.hxx
@@ -4,9 +4,8 @@
 
 #include <gunrock/framework/operators/configs.hxx>
 
-#include <thrust/transform.h>
+#include <thrust/for_each.h>
 #include <thrust/iterator/counting_iterator.h>
-#include <thrust/iterator/discard_iterator.h>
 
 namespace gunrock {
 namespace operators {
@@ -14,27 +13,33 @@ namespace parallel_for {
 
 template <parallel_for_each_t type, typename graph_t, typename operator_t>
 void execute(graph_t& G, operator_t op, cuda::multi_context_t& context) {
-  using type_t = std::conditional_t<type == parallel_for_each_t::vertex,
-                                    typename graph_t::vertex_type,
-                                    typename graph_t::edge_type>;
+  using index_t = std::conditional_t<type == parallel_for_each_t::vertex,
+                                     typename graph_t::vertex_type,
+                                     typename graph_t::edge_type>;
 
   auto single_context = context.get_context(0);
 
   std::size_t size = (type == parallel_for_each_t::vertex)
                          ? G.get_number_of_vertices()
                          : G.get_number_of_edges();
-  auto apply = [=] __device__(type_t const& x) {
-    op(x);
-    return x;  // output ignored.
-  };
 
-  thrust::transform(
-      thrust::cuda::par.on(single_context->stream()),
-      thrust::make_counting_iterator<type_t>(0),     // Begin: 0
-      thrust::make_counting_iterator<type_t>(size),  // End: # of Edges
-      thrust::make_discard_iterator(),               // output iterator: ignore
-      apply                                          // Unary Operator
-  );
+  if constexpr (type == parallel_for_each_t::weight) {
+    auto apply = [=] __device__(index_t const& x) { op(G.get_edge_weight(x)); };
+    thrust::for_each(
+        thrust::cuda::par.on(single_context->stream()),
+        thrust::make_counting_iterator<index_t>(0),     // Begin: 0
+        thrust::make_counting_iterator<index_t>(size),  // End: # of V/E
+        apply                                           // Unary Operator
+    );
+  } else {
+    auto apply = [=] __device__(index_t const& x) { op(x); };
+    thrust::for_each(
+        thrust::cuda::par.on(single_context->stream()),
+        thrust::make_counting_iterator<index_t>(0),     // Begin: 0
+        thrust::make_counting_iterator<index_t>(size),  // End: # of V/E
+        apply                                           // Unary Operator
+    );
+  }
 }
 
 }  // namespace parallel_for
