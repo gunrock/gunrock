85f59f1c1544e2d3610363fa0995f9a4d0d38dab
neoblizz <osama94@gmail.com>
2021-12-16 19:03:38 -0800

    Linking to correct CUB path instead of the symbolic link.

--- cmake/FetchThrustCUB.cmake
+++ cmake/FetchThrustCUB.cmake
@@ -19,4 +19,5 @@ if(NOT thrust_POPULATED)
   )
 endif()
 set(THRUST_INCLUDE_DIR "${thrust_SOURCE_DIR}")
-set(CUB_INCLUDE_DIR "${thrust_SOURCE_DIR}/cub")
+# Windows doesn't support symblink, so make sure we link to the real library.
+set(CUB_INCLUDE_DIR "${thrust_SOURCE_DIR}/dependencies/cub")
