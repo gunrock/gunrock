ad563d0cd6e3754a399b7984199beaa24fd9c82c
neoblizz <osama94@gmail.com>
2021-12-19 21:57:21 -0800

    Testing if the issue is type limits on device

--- unittests/type_limits/test_type_limits.cu
+++ unittests/type_limits/test_type_limits.cu
@@ -3,6 +3,10 @@
 
 #include <gunrock/util/type_limits.hxx>
 
+#include <thrust/execution_policy.h>
+#include <thrust/for_each.h>
+#include <thrust/iterator/counting_iterator.h>
+
 void test_type_limits() {
   std::cout << "invalid = " << gunrock::numeric_limits<int>::invalid()
             << " (is valid? " << std::boolalpha
@@ -27,6 +31,20 @@ void test_type_limits() {
             << gunrock::util::limits::is_valid(
                    gunrock::numeric_limits<unsigned int>::invalid())
             << ")" << std::endl;
+
+  auto apply = [=] __device__(int const& x) {
+    auto y = gunrock::numeric_limits<float>::invalid();
+    bool v = gunrock::util::limits::is_valid(x);
+    bool inv = gunrock::util::limits::is_valid(y);
+
+    printf("%f\n", y);
+  };
+
+  thrust::for_each(thrust::device,
+                   thrust::make_counting_iterator<int>(0),  // Begin: 0
+                   thrust::make_counting_iterator<int>(1),  // End: 1
+                   apply                                    // Unary Operator
+  );
 }
 
 int main(int argc, char** argv) {
