d4b73a4704707a9b9a55bb14e0bec6d59d668bbc
neoblizz <osama94@gmail.com>
2021-12-19 18:04:01 -0800

    changing unnamed to named class.

--- include/gunrock/cuda/device_properties.hxx
+++ include/gunrock/cuda/device_properties.hxx
@@ -18,7 +18,7 @@ namespace cuda {
 
 typedef cudaDeviceProp device_properties_t;
 
-typedef struct {
+struct compute_capability_t {
   unsigned major;
   unsigned minor;
   constexpr unsigned as_combined_number() const { return major * 10 + minor; }
@@ -28,7 +28,7 @@ typedef struct {
   constexpr bool operator<(int i) { return (int)as_combined_number() < i; }
   constexpr bool operator>=(int i) { return (int)as_combined_number() >= i; }
   constexpr bool operator<=(int i) { return (int)as_combined_number() <= i; }
-} compute_capability_t;
+};
 
 /**
  * @brief Make compute capability from major and minor versions.
@@ -76,18 +76,13 @@ enum : size_t { KiB = 1024, K = 1024 };
  * \return const char* architecture name or nullptr if capability is invalid
  */
 inline constexpr const char* arch_name(compute_capability_t capability) {
-  return (capability.major == 8)
-             ? "Ampere"
-             : (capability.major == 7 && capability.minor == 5)
-                   ? "Turing"
-                   : (capability.major == 7)
-                         ? "Volta"
-                         : (capability.major == 6)
-                               ? "Pascal"
-                               : (capability.major == 5)
-                                     ? "Maxwell"
-                                     : (capability.major == 3) ? "Kepler"
-                                                               : nullptr;
+  return (capability.major == 8)                            ? "Ampere"
+         : (capability.major == 7 && capability.minor == 5) ? "Turing"
+         : (capability.major == 7)                          ? "Volta"
+         : (capability.major == 6)                          ? "Pascal"
+         : (capability.major == 5)                          ? "Maxwell"
+         : (capability.major == 3)                          ? "Kepler"
+                                                            : nullptr;
 }
 
 // Device properties retrieved from:
@@ -115,11 +110,14 @@ inline constexpr unsigned warp_max_threads() {
  * \return unsigned
  */
 inline constexpr unsigned sm_max_ctas(compute_capability_t capability) {
-  return (capability >= 86) ? 16 :              // SM86+
-             (capability >= 80) ? 32 :          // SM80
-                 (capability >= 75) ? 16 :      // SM75
-                     (capability >= 50) ? 32 :  // SM50-SM72
-                         16;                    // SM30-SM37
+  return (capability >= 86) ? 16 :  // SM86+
+             (capability >= 80) ? 32
+                                :  // SM80
+             (capability >= 75) ? 16
+                                :  // SM75
+             (capability >= 50) ? 32
+                                :  // SM50-SM72
+             16;                   // SM30-SM37
 }
 
 /**
@@ -128,10 +126,12 @@ inline constexpr unsigned sm_max_ctas(compute_capability_t capability) {
  * \return unsigned
  */
 inline constexpr unsigned sm_max_threads(compute_capability_t capability) {
-  return (capability >= 86) ? 1536 :          // SM86+
-             (capability >= 80) ? 2048 :      // SM80
-                 (capability >= 75) ? 1024 :  // SM75
-                     2048;                    // SM30-SM72
+  return (capability >= 86) ? 1536 :  // SM86+
+             (capability >= 80) ? 2048
+                                :  // SM80
+             (capability >= 75) ? 1024
+                                :  // SM75
+             2048;                 // SM30-SM72
 }
 
 /**
@@ -140,9 +140,10 @@ inline constexpr unsigned sm_max_threads(compute_capability_t capability) {
  * \return unsigned
  */
 inline constexpr unsigned sm_registers(compute_capability_t capability) {
-  return (capability >= 50) ? 64 * K :       // SM50+
-             (capability >= 37) ? 128 * K :  // SM37
-                 64 * K;                     // SM30-SM35
+  return (capability >= 50) ? 64 * K :  // SM50+
+             (capability >= 37) ? 128 * K
+                                :  // SM37
+             64 * K;               // SM30-SM35
 }
 
 /**
@@ -158,30 +159,32 @@ template <enum cudaFuncCache sm3XCacheConfig = cudaFuncCachePreferNone>
 inline constexpr unsigned sm_max_shared_memory_bytes(
     compute_capability_t capability) {
   unsigned sm3XConfiguredSmem =
-      (sm3XCacheConfig == cudaFuncCachePreferNone)
-          ? 48 * KiB
-          : (sm3XCacheConfig == cudaFuncCachePreferShared)
-                ? 48 * KiB
-                : (sm3XCacheConfig == cudaFuncCachePreferL1)
-                      ? 16 * KiB
-                      : (sm3XCacheConfig == cudaFuncCachePreferEqual)
-                            ? 32 * KiB
-                            : 48 * KiB;
+      (sm3XCacheConfig == cudaFuncCachePreferNone)     ? 48 * KiB
+      : (sm3XCacheConfig == cudaFuncCachePreferShared) ? 48 * KiB
+      : (sm3XCacheConfig == cudaFuncCachePreferL1)     ? 16 * KiB
+      : (sm3XCacheConfig == cudaFuncCachePreferEqual)  ? 32 * KiB
+                                                       : 48 * KiB;
 
-  return (capability >= 86) ? 100 * KiB :                         // SM86+
-             (capability >= 80) ? 164 * KiB :                     // SM80
-                 (capability >= 75) ? 64 * KiB :                  // SM75
-                     (capability >= 70) ? 96 * KiB :              // SM70-SM72
-                         (capability >= 62) ? 64 * KiB :          // SM62
-                             (capability >= 61) ? 96 * KiB :      // SM61
-                                 (capability >= 53) ? 64 * KiB :  // SM53
-                                     (capability >= 52) ? 96 * KiB :  // SM52
-                                         (capability >= 50) ? 64 * KiB
-                                                            :  // SM50
-                                             (capability >= 37)
-                                                 ? 64 * KiB + sm3XConfiguredSmem
-                                                 :                    // SM37
-                                                 sm3XConfiguredSmem;  // SM30-SM35
+  return (capability >= 86) ? 100 * KiB :  // SM86+
+             (capability >= 80) ? 164 * KiB
+                                :  // SM80
+             (capability >= 75) ? 64 * KiB
+                                :  // SM75
+             (capability >= 70) ? 96 * KiB
+                                :  // SM70-SM72
+             (capability >= 62) ? 64 * KiB
+                                :  // SM62
+             (capability >= 61) ? 96 * KiB
+                                :  // SM61
+             (capability >= 53) ? 64 * KiB
+                                :  // SM53
+             (capability >= 52) ? 96 * KiB
+                                :  // SM52
+             (capability >= 50) ? 64 * KiB
+                                :  // SM50
+             (capability >= 37) ? 64 * KiB + sm3XConfiguredSmem
+                                :  // SM37
+             sm3XConfiguredSmem;   // SM30-SM35
 }
 
 /**
@@ -204,13 +207,10 @@ template <
 inline constexpr unsigned shared_memory_bank_stride() {
   // The default config on 3.x is the same constant value for later archs
   // Only let 3.x be configurable if stride later becomes dependent on arch
-  return (sm3XSmemConfig == cudaSharedMemBankSizeDefault)
-             ? 1 << 2
-             : (sm3XSmemConfig == cudaSharedMemBankSizeFourByte)
-                   ? 1 << 2
-                   : (sm3XSmemConfig == cudaSharedMemBankSizeEightByte)
-                         ? 1 << 3
-                         : 1 << 2;
+  return (sm3XSmemConfig == cudaSharedMemBankSizeDefault)     ? 1 << 2
+         : (sm3XSmemConfig == cudaSharedMemBankSizeFourByte)  ? 1 << 2
+         : (sm3XSmemConfig == cudaSharedMemBankSizeEightByte) ? 1 << 3
+                                                              : 1 << 2;
 }
 
 void print(device_properties_t& prop) {
