835b297b97eb82c44a595e577957ffb3fe81a788
neoblizz <osama94@gmail.com>
2021-12-22 18:07:52 -0800

    Adding support for cxxopts.

--- unittests/for/test_for.cu
+++ unittests/for/test_for.cu
@@ -1,4 +1,4 @@
-#include <cstdlib>  // EXIT_SUCCESS
+#include <cxxopts.hpp>
 
 #include <gunrock/error.hxx>                        // error checking
 #include <gunrock/graph/graph.hxx>                  // graph class
@@ -6,7 +6,7 @@
 #include <gunrock/cuda/cuda.hxx>                    // context to run on
 #include <gunrock/framework/operators/for/for.hxx>  // for operator
 
-void test_for(int num_arguments, char** argument_array) {
+void test_for(int argc, char** argv) {
   using namespace gunrock;
 
   using vertex_t = int;
@@ -19,17 +19,48 @@ void test_for(int num_arguments, char** argument_array) {
   // --
   // IO
 
-  csr_t csr;
-  std::string filename = argument_array[1];
+  cxxopts::Options options(argv[0], "Gunrock for operator test");
+  options.add_options()  // Allows to add options.
+      ("c,csr", "CSR binary file",
+       cxxopts::value<std::string>())  // CSR
+      ("m,market", "Matrix-market format file",
+       cxxopts::value<std::string>())  // Market
+      ("d,device", "Device to run on",
+       cxxopts::value<int>()->default_value("0"))  // Device
+      ("v,verbose", "Verbose output",
+       cxxopts::value<bool>()->default_value("false"))  // Verbose (not used)
+      ("h,help", "Print help");                         // Help
 
-  if (util::is_market(filename)) {
-    io::matrix_market_t<vertex_t, edge_t, weight_t> mm;
-    csr.from_coo(mm.load(filename));
-  } else if (util::is_binary_csr(filename)) {
-    csr.read_binary(filename);
+  auto result = options.parse(argc, argv);
+
+  if (result.count("help") ||
+      (result.count("market") == 0 && result.count("csr") == 0)) {
+    std::cout << options.help({""}) << std::endl;
+    exit(0);
+  }
+
+  csr_t csr;
+
+  if (result.count("market") == 1) {
+    std::string filename = result["market"].as<std::string>();
+    if (util::is_market(filename)) {
+      io::matrix_market_t<vertex_t, edge_t, weight_t> mm;
+      csr.from_coo(mm.load(filename));
+    } else {
+      std::cout << options.help({""}) << std::endl;
+      exit(0);
+    }
+  } else if (result.count("csr") == 1) {
+    std::string filename = result["csr"].as<std::string>();
+    if (util::is_binary_csr(filename))
+      csr.read_binary(filename);
+    else {
+      std::cout << options.help({""}) << std::endl;
+      exit(0);
+    }
   } else {
-    std::cerr << "Unknown file format: " << filename << std::endl;
-    exit(1);
+    std::cout << options.help({""}) << std::endl;
+    std::exit(0);
   }
 
   // --
@@ -42,10 +73,10 @@ void test_for(int num_arguments, char** argument_array) {
       csr.row_offsets.data().get(),     // row_offsets
       csr.column_indices.data().get(),  // column_indices
       csr.nonzero_values.data().get()   // values
-  );  // supports row_indices and column_offsets (default = nullptr)
+  );
 
   // Initialize the context.
-  cuda::device_id_t device = 0;
+  cuda::device_id_t device = result["device"].as<int>();
   cuda::multi_context_t context(device);
 
   auto vertex_op = [=] __device__(vertex_t const& v) -> void {
@@ -79,5 +110,4 @@ void test_for(int num_arguments, char** argument_array) {
 
 int main(int argc, char** argv) {
   test_for(argc, argv);
-  return EXIT_SUCCESS;
 }
