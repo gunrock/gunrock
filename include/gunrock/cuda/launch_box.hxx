3bab82315794b3ec7c97638483781165d0d38fa4
Cameron Shinn <ctshinn@ucdavis.edu>
2021-12-22 23:19:25 -0800

    Define launch func in launch box

--- include/gunrock/cuda/launch_box.hxx
+++ include/gunrock/cuda/launch_box.hxx
@@ -65,48 +65,6 @@ struct dim3_t {
 };
 
 /**
- * @brief Collection of kernel launch parameters for different architectures.
- *
- * @par Overview
- * A launch box is a collection of sets of CUDA kernel launch parameters each
- * corresponding to one or more SM architectures. At compile time, the launch
- * box's type resolves to the **first** launch parameters type (derived from
- * `launch_param_base_t`) that match the SM architecture that Gunrock is being
- * compiled for. If there isn't an explicit match, launch parameters for any SM
- * version can be specified at the end of the parameter pack using the
- * `fallback` enum for the `sm_flag_t` template parameter (note that this will
- * invalidate any launch parameter types later in the parameter pack). In the
- * case that there isn't a fallback and the compiler can't find launch
- * parameters for the architecture being compiled for, a static assert will be
- * raised. All launch parameters *should* use the same struct template so there
- * isn't any ambiguity as to what the launch box's constructor is, though this
- * isn't enforced.
- *
- * @par Example
- * The following code is an example of how to instantiate a launch box.
- *
- * \code
- * typedef launch_box_t<
- *     launch_params_t<sm_86 | sm_80, dim3_t<16, 2, 2>, dim3_t<64, 1, 4>, 2>,
- *     launch_params_t<sm_75 | sm_70, dim3_t<32, 2, 4>, dim3_t<64, 8, 8>>,
- *     launch_params_t<sm_61 | sm_60, dim3_t<8, 4, 4>, dim3_t<32, 1, 4>, 2>,
- *     launch_params_t<sm_35, dim3_t<64>, dim3_t<64>, 16>,
- *     launch_params_t<fallback, dim3_t<16>, dim3_t<2>, 4>>
- *     launch_t;
- *
- * launch_t my_launch_box(context);
- * \endcode
- *
- * @tparam lp_v Pack of `launch_params_t` types for each corresponding
- * architecture(s).
- */
-template <typename... lp_v>
-using launch_box_t = std::conditional_t<
-    (std::tuple_size<detail::match_launch_params_t<lp_v...>>::value == 0),
-    detail::raise_not_found_error_t<void>,  // Couldn't find params for SM ver
-    std::tuple_element_t<0, detail::match_launch_params_t<lp_v...>>>;
-
-/**
  * @brief Set of launch parameters for a CUDA kernel.
  *
  * @tparam sm_flags_ Bit flags for the SM architectures the launch parameters
@@ -129,41 +87,6 @@ struct launch_params_t
       block_dimensions_t::dimensions();
   static constexpr dimensions_t grid_dimensions =
       grid_dimensions_t::dimensions();
-
-  /**
-   * @brief Launch a kernel within the given launch box.
-   *
-   * @par Overview
-   * This function is a reimplementation of `std::apply`, that allows for
-   * launching cuda kernels with launch param members of the class and a context
-   * argument. It follows the "possible implementation" of `std::apply` in the
-   * C++ reference: https://en.cppreference.com/w/cpp/utility/apply.
-   *
-   * @tparam func_t The type of the kernel function being passed in.
-   * @tparam args_tuple_t The type of the tuple of arguments being passed in.
-   * @param __ Kernel function to call.
-   * @param args Tuple of arguments to be expanded as the arguments of the
-   * kernel function.
-   * @param context Reference to the context used to launch the kernel (used for
-   * the context's stream).
-   */
-  template <typename func_t, typename args_tuple_t>
-  void launch(func_t&& __, args_tuple_t&& args, standard_context_t& context) {
-    expand_launch(
-        std::forward<func_t>(__), std::forward<args_tuple_t>(args), context,
-        std::make_index_sequence<
-            std::tuple_size_v<std::remove_reference_t<args_tuple_t>>>{});
-  }
-
- private:
-  template <typename func_t, typename args_tuple_t, std::size_t... I>
-  void expand_launch(func_t&& __,
-                     args_tuple_t&& args,
-                     standard_context_t& context,
-                     std::index_sequence<I...>) {
-    __<<<grid_dimensions, block_dimensions, base_t::shared_memory_bytes,
-         context.stream()>>>(std::get<I>(std::forward<args_tuple_t>(args))...);
-  }
 };
 
 /**
@@ -192,27 +115,83 @@ struct launch_params_dynamic_grid_t
     grid_dimensions = dimensions_t(
         (num_elements + block_dimensions.x - 1) / block_dimensions.x, 1, 1);
   }
+};
+
+/**
+ * @brief Alias a selected a launch params type from valid options on
+ * architecture being compiled for.
+ *
+ * @tparam lp_v
+ */
+template <typename... lp_v>
+using select_launch_params_t = std::conditional_t<
+    (std::tuple_size<detail::match_launch_params_t<lp_v...>>::value == 0),
+    detail::raise_not_found_error_t<void>,  // Couldn't find arch params
+    std::tuple_element_t<0, detail::match_launch_params_t<lp_v...>>>;
+
+/**
+ * @brief Collection of kernel launch parameters for different
+ * architectures.
+ *
+ * @par Overview
+ * A launch box is a collection of sets of CUDA kernel launch parameters
+ * each corresponding to one or more SM architectures. At compile time, the
+ * launch box's type resolves to the **first** launch parameters type
+ * (derived from `launch_param_base_t`) that match the SM architecture that
+ * Gunrock is being compiled for. If there isn't an explicit match, launch
+ * parameters for any SM version can be specified at the end of the
+ * parameter pack using the `fallback` enum for the `sm_flag_t` template
+ * parameter (note that this will invalidate any launch parameter types
+ * later in the parameter pack). In the case that there isn't a fallback and
+ * the compiler can't find launch parameters for the architecture being
+ * compiled for, a static assert will be raised. All launch parameters
+ * *should* use the same struct template so there isn't any ambiguity as to
+ * what the launch box's constructor is, though this isn't enforced.
+ *
+ * @par Example
+ * The following code is an example of how to instantiate a launch box.
+ *
+ * \code
+ * typedef launch_box_t<
+ *     launch_params_t<sm_86 | sm_80, dim3_t<16, 2, 2>, dim3_t<64, 1, 4>,
+ * 2>, launch_params_t<sm_75 | sm_70, dim3_t<32, 2, 4>, dim3_t<64, 8, 8>>,
+ *     launch_params_t<sm_61 | sm_60, dim3_t<8, 4, 4>, dim3_t<32, 1, 4>, 2>,
+ *     launch_params_t<sm_35, dim3_t<64>, dim3_t<64>, 16>,
+ *     launch_params_t<fallback, dim3_t<16>, dim3_t<2>, 4>>
+ *     launch_t;
+ *
+ * launch_t my_launch_box(context);
+ * \endcode
+ *
+ * @tparam lp_v Pack of `launch_params_t` types for each corresponding
+ * architecture(s).
+ */
+template <typename... lp_v>
+struct launch_box_t : select_launch_params_t<lp_v...> {
+  typedef select_launch_params_t<lp_v...> base_t;
 
   /**
    * @brief Launch a kernel within the given launch box.
    *
    * @par Overview
-   * This function is a reimplementation of `std::apply`, that allows for
-   * launching cuda kernels with launch param members of the class and a context
-   * argument. It follows the "possible implementation" of `std::apply` in the
-   * C++ reference: https://en.cppreference.com/w/cpp/utility/apply.
+   * This function is a reimplementation of `std::apply`, that allows
+   * for launching cuda kernels with launch param members of the class
+   * and a context argument. It follows the "possible implementation" of
+   * `std::apply` in the C++ reference:
+   * https://en.cppreference.com/w/cpp/utility/apply.
    *
    * @tparam func_t The type of the kernel function being passed in.
-   * @tparam args_tuple_t The type of the tuple of arguments being passed in.
+   * @tparam args_tuple_t The type of the tuple of arguments being
+   * passed in.
    * @param __ Kernel function to call.
-   * @param args Tuple of arguments to be expanded as the arguments of the
-   * kernel function.
-   * @param context Reference to the context used to launch the kernel (used for
-   * the context's stream).
+   * @param args Tuple of arguments to be expanded as the arguments of
+   * the kernel function.
+   * @param context Reference to the context used to launch the kernel
+   * (used for the context's stream).
    */
   template <typename func_t, typename args_tuple_t>
   void launch(func_t&& __, args_tuple_t&& args, standard_context_t& context) {
-    expand_launch(
+    launch_impl(
         std::forward<func_t>(__), std::forward<args_tuple_t>(args), context,
         std::make_index_sequence<
             std::tuple_size_v<std::remove_reference_t<args_tuple_t>>>{});
@@ -220,12 +199,13 @@ struct launch_params_dynamic_grid_t
 
  private:
   template <typename func_t, typename args_tuple_t, std::size_t... I>
-  void expand_launch(func_t&& __,
-                     args_tuple_t&& args,
-                     standard_context_t& context,
-                     std::index_sequence<I...>) {
-    __<<<grid_dimensions, block_dimensions, base_t::shared_memory_bytes,
-         context.stream()>>>(std::get<I>(std::forward<args_tuple_t>(args))...);
+  void launch_impl(func_t&& __,
+                   args_tuple_t&& args,
+                   standard_context_t& context,
+                   std::index_sequence<I...>) {
+    __<<<base_t::grid_dimensions, base_t::block_dimensions,
+         base_t::shared_memory_bytes, context.stream()>>>(
+        std::get<I>(std::forward<args_tuple_t>(args))...);
   }
 };
 
@@ -254,6 +234,5 @@ inline float occupancy(func_t kernel) {
 }
 
 }  // namespace launch_box
-
 }  // namespace cuda
 }  // namespace gunrock
