294db60d8848dcfe45a6229a893b05d3462d6b41
Annie <annie@annielytical.com>
2022-02-18 15:15:45 -0800

    initial MST CPU implementation

---
+++ examples/algorithms/mst/mst.cu
@@ -0,0 +1,78 @@
+#include <gunrock/algorithms/algorithms.hxx>
+
+//#include <gunrock/algorithms/mst.hxx>
+#include "mst_cpu.hxx"  // Reference implementation
+
+using namespace gunrock;
+using namespace memory;
+
+void test_mst(int num_arguments, char** argument_array) {
+  if (num_arguments != 2) {
+    std::cerr << "usage: ./bin/<program-name> filename.mtx" << std::endl;
+    exit(1);
+  }
+
+  // --
+  // Define types
+  
+  using vertex_t = int;
+  using edge_t   = int;
+  using weight_t = float;
+  using csr_t =
+      format::csr_t<memory_space_t::device, vertex_t, edge_t, weight_t>;
+
+  // --
+  // IO
+  
+  csr_t csr;
+  std::string filename = argument_array[1];
+
+  if (util::is_market(filename)) {
+    io::matrix_market_t<vertex_t, edge_t, weight_t> mm;
+    csr.from_coo(mm.load(filename));
+  } else if (util::is_binary_csr(filename)) {
+    csr.read_binary(filename);
+  } else {
+    std::cerr << "Unknown file format: " << filename << std::endl;
+    exit(1);
+  }
+
+  // --
+  // Build graph
+
+  auto G = graph::build::from_csr<memory_space_t::device, graph::view_t::csr>(
+      csr.number_of_rows,
+      csr.number_of_columns,
+      csr.number_of_nonzeros,
+      csr.row_offsets.data().get(),
+      csr.column_indices.data().get(),
+      csr.nonzero_values.data().get() 
+  );
+
+  // --
+  // Params and memory allocation
+  
+  vertex_t n_vertices = G.get_number_of_vertices();
+  thrust::device_vector<weight_t> distances(n_vertices);
+
+  // --
+  // GPU Run
+
+  //float gpu_elapsed = gunrock::mst::run(G, single_source, distances.data().get());
+
+  // --
+  // CPU Run
+
+  float cpu_elapsed = mst_cpu::run<csr_t, vertex_t, edge_t, weight_t>(
+      csr);
+
+  // --
+  // Log + Validate
+
+  std::cout << "CPU Elapsed Time : " << cpu_elapsed << " (ms)" << std::endl;
+  //std::cout << "GPU Elapsed Time : " << gpu_elapsed << " (ms)" << std::endl;
+}
+
+int main(int argc, char** argv) {
+  test_mst(argc, argv);
+}
---
+++ examples/algorithms/mst/mst_cpu.hxx
@@ -0,0 +1,118 @@
+
+#pragma once
+
+#include <chrono>
+
+#include <thrust/iterator/counting_iterator.h>
+#include <thrust/random.h>
+
+#include <gunrock/algorithms/generate/random.hxx>
+
+namespace mst_cpu {
+
+using namespace std;
+using namespace std::chrono;
+  
+using vertex_t = int;
+using edge_t   = int;
+using weight_t = float;
+
+struct super {
+  super* root;
+  weight_t min_weight;
+  vertex_t min_neighbor;
+};
+
+// pointer jumping to form super vertices
+void jump_pointers(super* supers, vertex_t v) {
+    super* u = supers[v].root;
+    while (u->root != u) {
+      u = u->root;
+    }
+    supers[v].root = u;
+    return;
+}
+
+template <typename csr_t, typename vertex_t, typename edge_t, typename weight_t>
+float run(csr_t& csr) {
+  // Copy data to CPU
+  thrust::host_vector<edge_t> row_offsets(csr.row_offsets);
+  thrust::host_vector<vertex_t> column_indices(csr.column_indices);
+  thrust::host_vector<weight_t> nonzero_values(csr.nonzero_values);
+
+  auto t_start = high_resolution_clock::now();
+
+  vertex_t n_vertices = csr.number_of_rows;
+
+  struct super* supers = new super[n_vertices];
+  int super_vertices = n_vertices;
+
+  edge_t* mst = new edge_t[nonzero_values.size()];
+
+  float mst_weight = 0;
+
+  for (vertex_t i = 0; i < n_vertices; i++) {
+    supers[i].root = &supers[i];
+    supers[i].min_neighbor = i;
+  }
+  int mst_edges = 0;
+  int it=0;
+  while (super_vertices > 1) {
+    it++;
+    for (vertex_t i = 0; i < n_vertices; i++) {
+      supers[i].min_weight = std::numeric_limits<weight_t>::max();
+      supers[i].min_neighbor = i;
+    }
+
+    for (vertex_t v = 0; v < n_vertices; v++) {
+      edge_t start_edge = row_offsets[v];
+      edge_t num_neighbors = row_offsets[v + 1] - row_offsets[v];
+
+      for (edge_t e = start_edge; e < start_edge + num_neighbors; ++e) {
+        vertex_t u = column_indices[e];
+        if (supers[v].root == supers[u].root) {
+          continue;
+        }
+        weight_t new_dist = nonzero_values[e];
+        if (new_dist < supers[v].root->min_weight) {
+          supers[v].root->min_weight = new_dist;
+          supers[v].root->min_neighbor = u;
+        }
+        if (new_dist < supers[u].root->min_weight) {
+          supers[u].root->min_weight = new_dist;
+          supers[u].root->min_neighbor = v;
+        }
+      }
+    }
+
+    for (vertex_t v = 0; v < n_vertices; v++) {
+      if (supers[v].min_weight != std::numeric_limits<weight_t>::max()) {
+        jump_pointers(supers,v);
+        jump_pointers(supers,supers[v].min_neighbor);
+        if (supers[supers[v].min_neighbor].root != supers[v].root) {
+          if (supers[supers[v].min_neighbor].min_neighbor != v ||
+              v < supers[v].min_neighbor) {
+            mst_weight += supers[v].min_weight;
+            mst_edges++;
+            supers[v].root->root = supers[supers[v].min_neighbor].root;
+            super_vertices--;
+          }
+        }
+      }
+    }
+    // add stop condition when super vertices not decremented
+    
+    for (vertex_t v = 0; v < n_vertices; v++) {
+        jump_pointers(supers,v);
+    }
+  }
+
+  printf("mst weight %f\n", mst_weight);
+  std::cout << "mst edges " << mst_edges << "\n";
+
+  auto t_stop = high_resolution_clock::now();
+  auto elapsed = duration_cast<microseconds>(t_stop - t_start).count();
+  return (float)elapsed / 1000;
+}
+
+}  // namespace mst_cpu
