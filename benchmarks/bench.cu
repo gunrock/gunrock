2c6f0397110d36614278b4d41a41cdf14ef91ceb
Annie <annie@annielytical.com>
2022-05-09 22:22:13 -0700

    more benchmarks

--- benchmarks/bench.cu
+++ benchmarks/bench.cu
@@ -8,11 +8,11 @@
 #include <iostream>
 #include <gunrock/algorithms/algorithms.hxx>
 #include <gunrock/algorithms/mst.hxx>
-
-char** args;
+#include <gunrock/algorithms/bfs.hxx>
 
 namespace gunrock {
 namespace benchmark {
+
 void mst_bench(nvbench::state& state) {
   // Build a graph using a sample csr.
   auto csr = io::sample_large::csr();
@@ -23,17 +23,44 @@ void mst_bench(nvbench::state& state) {
   cuda::device_id_t device = 0;
   cuda::multi_context_t context(device);
 
+  // --
+  // Params and memory allocation
   thrust::device_vector<weight_t> mst_weight(1);
 
   // --
   // GPU Run
-  
   state.exec(nvbench::exec_tag::sync, [&](nvbench::launch& launch) {
     gunrock::mst::run(G, mst_weight.data().get());
   });
 }
 
-NVBENCH_BENCH(mst_bench).set_name("mst_bench");
+void bfs_bench(nvbench::state& state) {
+  // Build a graph using a sample csr.
+  auto csr = io::sample_large::csr();
+  auto G =
+      graph::build::from_csr<memory_space_t::device, graph::view_t::csr>(csr);
+
+  // Initialize the context.
+  cuda::device_id_t device = 0;
+  cuda::multi_context_t context(device);
+
+  // --
+  // Params and memory allocation
+  vertex_t single_source = 0;
+  vertex_t n_vertices = G.get_number_of_vertices();
+  thrust::device_vector<vertex_t> distances(n_vertices);
+  thrust::device_vector<vertex_t> predecessors(n_vertices);
+
+  // --
+  // GPU Run
+  state.exec(nvbench::exec_tag::sync, [&](nvbench::launch& launch) {
+    gunrock::bfs::run(G, single_source, distances.data().get(),
+                      predecessors.data().get());
+  });
+}
+
+NVBENCH_BENCH(mst_bench);
+NVBENCH_BENCH(bfs_bench);
 
 }  // namespace benchmark
 }  // namespace gunrock
