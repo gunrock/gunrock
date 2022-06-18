c9231d485783e2cf4b8cb9b40f0550dff2fcf8fd
Muhammad Awad <mawad@ucdavis.edu>
2022-06-17 18:09:10 -0700

    Count total number of triangles

--- examples/algorithms/tc/tc.cu
+++ examples/algorithms/tc/tc.cu
@@ -4,18 +4,18 @@ using namespace gunrock;
 using namespace memory;
 
 void test_tc(int num_arguments, char** argument_array) {
-  if (num_arguments != 2) {
-    std::cerr << "usage: ./bin/tc filename.mtx" << std::endl;
+  if (num_arguments != 3) {
+    std::cerr << "usage: ./bin/tc filename.mtx reduce" << std::endl;
     exit(1);
   }
 
   // --
   // Define types
 
-  using vertex_t = int;
-  using edge_t = int;
+  using vertex_t = uint32_t;
+  using edge_t = uint32_t;
   using weight_t = float;
-  using count_t = int;
+  using count_t = vertex_t;
 
   using csr_t =
       format::csr_t<memory_space_t::device, vertex_t, edge_t, weight_t>;
@@ -24,11 +24,18 @@ void test_tc(int num_arguments, char** argument_array) {
   // --
   // IO
 
-  std::string filename = argument_array[1];
+  const std::string filename = argument_array[1];
+  const std::string reduce = argument_array[2];
+  const bool reduce_all_triangles = reduce.find("true") != std::string::npos;
 
   if (util::is_market(filename)) {
     io::matrix_market_t<vertex_t, edge_t, weight_t> mm;
-    csr.from_coo(mm.load(filename));
+    auto mmatrix = mm.load(filename);
+    if (!mm_is_symmetric(mm.code)) {
+      std::cerr << "Error: input matrix must be symmetric" << std::endl;
+      exit(1);
+    }
+    csr.from_coo(mmatrix);
   } else if (util::is_binary_csr(filename)) {
     csr.read_binary(filename);
   } else {
@@ -47,25 +54,28 @@ void test_tc(int num_arguments, char** argument_array) {
       csr.row_offsets.data().get(),     // row_offsets
       csr.column_indices.data().get(),  // column_indices
       csr.nonzero_values.data().get()   // values
-  );  // supports row_indices and column_offsets (default = nullptr)
+  );
 
   // --
   // Params and memory allocation
-  srand(time(NULL));
 
   vertex_t n_vertices = G.get_number_of_vertices();
   thrust::device_vector<count_t> triangles_count(n_vertices, 0);
-  std::cout << "TC for graph with n_vertices = " << n_vertices << std::endl;
 
   // --
   // GPU Run
 
-  float gpu_elapsed = tc::run(G, triangles_count.data().get());
+  std::size_t total_triangles = 0;
+  float gpu_elapsed = tc::run(G, reduce_all_triangles,
+                              triangles_count.data().get(), &total_triangles);
 
   // --
-  // Log + Validate
-  print::head(triangles_count, 40, "Per-vertex triangle count");
+  // Log
 
+  print::head(triangles_count, 40, "Per-vertex triangle count");
+  if (reduce_all_triangles) {
+    std::cout << "Total Graph Traingles : " << total_triangles << std::endl;
+  }
   std::cout << "GPU Elapsed Time : " << gpu_elapsed << " (ms)" << std::endl;
 }
 
--- include/gunrock/algorithms/tc.hxx
+++ include/gunrock/algorithms/tc.hxx
@@ -17,13 +17,18 @@ namespace tc {
 
 template <typename vertex_t>
 struct param_t {
-  // No parameters for this algorithm
+  bool reduce_all_triangles;
+  param_t(bool _reduce_all_triangles)
+      : reduce_all_triangles(_reduce_all_triangles) {}
 };
 
 template <typename vertex_t>
 struct result_t {
-  vertex_t* triangles_count;
-  result_t(vertex_t* _triangles_count) : triangles_count(_triangles_count) {}
+  vertex_t* vertex_triangles_count;
+  std::size_t* total_triangles_count;
+  result_t(vertex_t* _vertex_triangles_count, uint64_t* _total_triangles_count)
+      : vertex_triangles_count(_vertex_triangles_count),
+        total_triangles_count(_total_triangles_count) {}
 };
 
 template <typename graph_t, typename param_type, typename result_type>
@@ -72,18 +77,22 @@ struct enactor_t : gunrock::enactor_t<problem_t> {
     auto P = this->get_problem();
     auto G = P->get_graph();
 
-    auto triangles_count = P->result.triangles_count;
+    auto vertex_triangles_count = P->result.vertex_triangles_count;
     auto iteration = this->iteration;
 
-    auto intersect = [G, triangles_count] __host__ __device__(
+    auto intersect = [G, vertex_triangles_count] __host__ __device__(
                          vertex_t const& source,    // ... source
                          vertex_t const& neighbor,  // neighbor
                          edge_t const& edge,        // edge
                          weight_t const& weight     // weight (tuple).
                          ) -> bool {
-      if (source < neighbor) {
-        auto src_triangles_count = G.get_intersection_count(source, neighbor);
-        math::atomic::add(&(triangles_count[source]), src_triangles_count);
+      if (neighbor > source) {
+        auto src_vertex_triangles_count = G.get_intersection_count(
+            source, neighbor,
+            [vertex_triangles_count](auto intersection_vertex) {
+              math::atomic::add(&(vertex_triangles_count[intersection_vertex]),
+                                vertex_t{1});
+            });
       }
       return false;
     };
@@ -98,7 +107,9 @@ struct enactor_t : gunrock::enactor_t<problem_t> {
 
 template <typename graph_t>
 float run(graph_t& G,
-          typename graph_t::vertex_type* triangles_count,  // Output
+          bool reduce_all_triangles,
+          typename graph_t::vertex_type* vertex_triangles_count,  // Output
+          std::size_t* total_triangles_count,                     // Output
           std::shared_ptr<gcuda::multi_context_t> context =
               std::shared_ptr<gcuda::multi_context_t>(
                   new gcuda::multi_context_t(0))  // Context
@@ -110,8 +121,8 @@ float run(graph_t& G,
   using param_type = param_t<vertex_t>;
   using result_type = result_t<vertex_t>;
 
-  param_type param;
-  result_type result(triangles_count);
+  param_type param(reduce_all_triangles);
+  result_type result(vertex_triangles_count, total_triangles_count);
   // </user-defined>
 
   using problem_type = problem_t<graph_t, param_type, result_type>;
@@ -122,8 +133,21 @@ float run(graph_t& G,
   problem.reset();
 
   enactor_type enactor(&problem, context);
-  return enactor.enact();
+  auto time = enactor.enact();
+
+  if (param.reduce_all_triangles) {
+    auto policy = context->get_context(0)->execution_policy();
+    *result.total_triangles_count = thrust::transform_reduce(
+        policy, result.vertex_triangles_count,
+        result.vertex_triangles_count + G.get_number_of_vertices(),
+        [] __device__(const vertex_t& vertex_triangles) {
+          return static_cast<std::size_t>(vertex_triangles);
+        },
+        std::size_t{0}, thrust::plus<std::size_t>());
+  }
+
   // </boiler-plate>
+  return time;
 }
 
 }  // namespace tc
