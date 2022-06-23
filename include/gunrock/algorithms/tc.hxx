f514314df5b3bc92210a88913a611d0a67d55cbd
Muhammad Awad <mawad@ucdavis.edu>
2022-06-22 17:53:53 -0700

    [skip ci] Remove dead code

--- include/gunrock/algorithms/tc.hxx
+++ include/gunrock/algorithms/tc.hxx
@@ -65,14 +65,6 @@ struct enactor_t : gunrock::enactor_t<problem_t> {
   using weight_t = typename problem_t::weight_t;
   using frontier_t = typename enactor_t<problem_t>::frontier_t;
 
-  void prepare_frontier(frontier_t* f,
-                        gcuda::multi_context_t& context) override {
-    auto P = this->get_problem();
-    auto n_vertices = P->get_graph().get_number_of_vertices();
-
-    f->sequence((vertex_t)0, n_vertices, context.get_context(0)->stream());
-  }
-
   void loop(gcuda::multi_context_t& context) override {
     // Data slice
     auto E = this->get_enactor();
