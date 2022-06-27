181dedac2dbfe6500f693dec59d61a0c3b9c7c52
neoblizz <osama94@gmail.com>
2022-06-27 14:17:19 -0700

    Bump CUB/Thrust.

--- cmake/FetchThrustCUB.cmake
+++ cmake/FetchThrustCUB.cmake
@@ -9,7 +9,7 @@ set(FETCHCONTENT_BASE_DIR ${FC_BASE})
 FetchContent_Declare(
     thrust
     GIT_REPOSITORY https://github.com/thrust/thrust.git
-    GIT_TAG        1.16.0
+    GIT_TAG        1.17.0
 )
 
 FetchContent_GetProperties(thrust)
