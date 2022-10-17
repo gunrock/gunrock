f21584d25e4fe615dec1384dc549c90de07ad58c
Annie <annie@annielytical.com>
2022-10-16 20:56:01 -0700

    compiler def fixes

--- include/gunrock/util/compiler.hxx
+++ include/gunrock/util/compiler.hxx
@@ -8,8 +8,8 @@ std::string compiler_version = std::to_string(
     (__clang_major__ % 100) * 10000000 + (__clang_minor__ % 100) * 100000 +
     (__clang_patchlevel__ % 100000));
 #else
-std::string compiler = "Gnu GCC C/C++";
 #ifdef __GNUC_PATCHLEVEL__
+std::string compiler = "Gnu GCC C/C++";
 std::string compiler_version = std::to_string((__GNUC__ % 100) * 10000000 +
                                               (__GNUC_MINOR__ % 100) * 100000 +
                                               (__GNUC_PATCHLEVEL__ % 100000));
@@ -17,8 +17,10 @@ std::string compiler_version = std::to_string((__GNUC__ % 100) * 10000000 +
 #ifdef _MSC_VER
 std::string compiler = "MSVC";
 std::string compiler_version = std::to_string(_MSC_FULL_VER);
-else std::string compiler_version = std::to_string(
-    (__GNUC__ % 100) * 10000000 + (__GNUC_MINOR__ % 100) * 100000);
+#else
+std::string compiler = "Gnu GCC C/C++";
+std::string compiler_version = std::to_string((__GNUC__ % 100) * 10000000 +
+                                              (__GNUC_MINOR__ % 100) * 100000);
 #endif
 #endif
 #endif
