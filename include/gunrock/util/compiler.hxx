e440ff87e2db4f299cbb1229bb3929734d61d832
Annie <annie@annielytical.com>
2022-10-16 20:58:36 -0700

    formatting

--- include/gunrock/util/compiler.hxx
+++ include/gunrock/util/compiler.hxx
@@ -8,15 +8,15 @@ std::string compiler_version = std::to_string(
     (__clang_major__ % 100) * 10000000 + (__clang_minor__ % 100) * 100000 +
     (__clang_patchlevel__ % 100000));
 #else
+#ifdef _MSC_VER
+std::string compiler = "MSVC";
+std::string compiler_version = std::to_string(_MSC_FULL_VER);
+#else
 #ifdef __GNUC_PATCHLEVEL__
 std::string compiler = "Gnu GCC C/C++";
 std::string compiler_version = std::to_string((__GNUC__ % 100) * 10000000 +
                                               (__GNUC_MINOR__ % 100) * 100000 +
                                               (__GNUC_PATCHLEVEL__ % 100000));
-#else
-#ifdef _MSC_VER
-std::string compiler = "MSVC";
-std::string compiler_version = std::to_string(_MSC_FULL_VER);
 #else
 std::string compiler = "Gnu GCC C/C++";
 std::string compiler_version = std::to_string((__GNUC__ % 100) * 10000000 +
