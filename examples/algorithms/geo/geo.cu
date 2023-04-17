deb5ca6a120625cf44534ffa8055c4f139bf262f
Annie <annie@annielytical.com>
2023-04-16 19:03:30 -0700

    update geo (segfaults - need to fix)

--- examples/algorithms/geo/geo.cu
+++ examples/algorithms/geo/geo.cu
@@ -117,6 +117,8 @@ void test_geo(int num_arguments, char** argument_array) {
   using vertex_t = int;
   using edge_t = int;
   using weight_t = float;
+  using csr_t =
+      format::csr_t<memory_space_t::device, vertex_t, edge_t, weight_t>;
 
   // --
   // IO
@@ -124,23 +126,18 @@ void test_geo(int num_arguments, char** argument_array) {
   std::string filename = argument_array[1];
 
   io::matrix_market_t<vertex_t, edge_t, weight_t> mm;
+  gunrock::io::loader_struct<vertex_t, edge_t, weight_t> loader;
+  loader = mm.load(filename);
+  
+  format::csr_t<memory_space_t::device, vertex_t, edge_t, weight_t> csr;
 
-  using csr_t =
-      format::csr_t<memory_space_t::device, vertex_t, edge_t, weight_t>;
-  csr_t csr;
-  csr.from_coo(mm.load(filename));
+  csr.from_coo(loader.coo);
 
   // --
   // Build graph
 
-  auto G = graph::build::from_csr<memory_space_t::device, graph::view_t::csr>(
-      csr.number_of_rows,               // rows
-      csr.number_of_columns,            // columns
-      csr.number_of_nonzeros,           // nonzeros
-      csr.row_offsets.data().get(),     // row_offsets
-      csr.column_indices.data().get(),  // column_indices
-      csr.nonzero_values.data().get()   // values
-  );  // supports row_indices and column_offsets (default = nullptr)
+  auto G =
+      graph::build::build<memory_space_t::device>(loader.properties, csr);
 
   // --
   // Params and memory allocation
