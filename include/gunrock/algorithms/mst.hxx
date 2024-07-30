7965e86212ac91ada609635d5866de66d11d3ad2
Muhammad Osama <osama94@gmail.com>
2024-07-30 08:17:52 -0700

    Replace cuda API with context sync.

--- include/gunrock/algorithms/mst.hxx
+++ include/gunrock/algorithms/mst.hxx
@@ -237,7 +237,7 @@ struct enactor_t : gunrock::enactor_t<problem_t> {
     );
 
     // Need to sync before checking the value of not_decremented
-    cudaDeviceSynchronize();
+    context->get_context(0)->synchronize();
 
     // Throw an exception if the number of super vertices has not been
     // decremented
