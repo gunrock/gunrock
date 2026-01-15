006418f238ea306cd6fe38ed4cfd6f40021248a6
neoblizz <osama94@gmail.com>
2026-01-15 20:59:22 +0000

    Suppress MSVC warnings and linker fix.

--- unittests/framework/problem.cuh
+++ unittests/framework/problem.cuh
@@ -1,13 +1,14 @@
+#include <gunrock/compat/runtime_api.h>
 #include <gunrock/error.hxx>  // error checking
 #include <gunrock/framework/problem.hxx>
-#include <gunrock/compat/runtime_api.h>
 
 #include <gtest/gtest.h>
 
 using namespace gunrock;
 
 TEST(framework, problem) {
-  error::error_t status = hipSuccess;
+  // Use fully qualified type to avoid MSVC parsing issues
+  gunrock::error::error_t status = hipSuccess;
   // XXX ... write a test for [problem.hxx]
   (void)status;  // Suppress unused variable warning
 }
