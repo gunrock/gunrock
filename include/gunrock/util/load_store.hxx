b97bc0f5a19cd85ffdb50f4967523d47e5375467
neoblizz <osama94@gmail.com>
2026-01-15 04:31:04 +0000

    Fix load_store.hxx to use CUB for CUDA and hipCUB for HIP
    - Conditionally include CUB (for CUDA/NVCC) or hipCUB (for HIP)
    - Use appropriate namespace and types based on compiler
    - Resolves 'hipcub/hipcub.hpp: No such file' error when building with CUDA

--- include/gunrock/util/load_store.hxx
+++ include/gunrock/util/load_store.hxx
@@ -8,7 +8,25 @@
  *
  */
 #pragma once
-#include <hipcub/hipcub.hpp>
+
+// Include appropriate CUB library based on compiler
+#if defined(__CUDACC__) && !defined(__HIP__)
+  // Pure CUDA (NVCC without HIP): Use CUB
+  #include <cub/cub.cuh>
+  namespace cub_namespace = cub;
+  using CacheLoadModifierType = cub::CacheLoadModifier;
+  using CacheStoreModifierType = cub::CacheStoreModifier;
+  constexpr auto LOAD_DEFAULT = cub::CacheLoadModifier::LOAD_DEFAULT;
+  constexpr auto STORE_DEFAULT = cub::CacheStoreModifier::STORE_DEFAULT;
+#else
+  // HIP compiler (works for both NVIDIA and AMD platforms): Use hipCUB
+  #include <hipcub/hipcub.hpp>
+  namespace cub_namespace = hipcub;
+  using CacheLoadModifierType = hipcub::CacheLoadModifier;
+  using CacheStoreModifierType = hipcub::CacheStoreModifier;
+  constexpr auto LOAD_DEFAULT = hipcub::CacheLoadModifier::LOAD_DEFAULT;
+  constexpr auto STORE_DEFAULT = hipcub::CacheStoreModifier::STORE_DEFAULT;
+#endif
 
 namespace gunrock {
 namespace thread {
@@ -16,13 +34,11 @@ namespace thread {
 /**
  * @brief Uses a cached load to load a value from a given pointer.
  */
-template <hipcub::CacheLoadModifier MODIFIER =
-              hipcub::CacheLoadModifier::LOAD_DEFAULT,
+template <CacheLoadModifierType MODIFIER = LOAD_DEFAULT,
           typename type_t>
 __device__ __host__ __forceinline__ type_t load(type_t* ptr) {
-#ifdef __HIP_DEVICE_COMPILE__
-  // #ifdef __CUDA_ARCH__
-  return hipcub::ThreadLoad<MODIFIER>(ptr);
+#if defined(__HIP_DEVICE_COMPILE__) || defined(__CUDA_ARCH__)
+  return cub_namespace::ThreadLoad<MODIFIER>(ptr);
 #else
   return *ptr;
 #endif
@@ -31,13 +47,11 @@ __device__ __host__ __forceinline__ type_t load(type_t* ptr) {
 /**
  * @brief Uses a cached store to store a given value into a pointer.
  */
-template <hipcub::CacheStoreModifier MODIFIER =
-              hipcub::CacheStoreModifier::STORE_DEFAULT,
+template <CacheStoreModifierType MODIFIER = STORE_DEFAULT,
           typename type_t>
 __device__ __host__ __forceinline__ void store(type_t* ptr, const type_t& val) {
-#ifdef __HIP_DEVICE_COMPILE__
-  // #ifdef __CUDA_ARCH__
-  hipcub::ThreadStore<MODIFIER>(ptr, val);
+#if defined(__HIP_DEVICE_COMPILE__) || defined(__CUDA_ARCH__)
+  cub_namespace::ThreadStore<MODIFIER>(ptr, val);
 #else
   *ptr = val;
 #endif
